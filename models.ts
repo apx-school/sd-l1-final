375ee12ea7adca28d771a6e5a35fee2ea306b17e
Jae <jaeladu1@gmail.com>
2021-04-07 19:52:54 -0300

    falta index

--- models.ts
+++ models.ts
@@ -1,18 +1,54 @@
-import * as jsonfile from "jsonfile";
+import { readFile, writeFile } from "jsonfile";
 
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
-  id: number;
-  title: string;
-  tags: string[];
+   id: number;
+   title: string;
+   tags: string[];
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
-    });
-  }
+   moviesColection: Peli[];
+
+   getAll() {
+      return readFile("./pelis.json").then((res) => {
+         this.moviesColection = res;
+         return this.moviesColection;
+      });
+   }
+   getById(id: number) {
+      return this.getAll().then((res) => {
+         return res.find((p) => p.id == id);
+      });
+   }
+   search(options: any) {
+      return this.getAll().then((res) => {
+         var aux = res;
+
+         if (options.hasOwnProperty("title")) {
+            aux = res.filter((p) => p.title.includes(options.title));
+         }
+         if (options.hasOwnProperty("tag")) {
+            aux = aux.filter((p) => p.tags.includes(options.tag));
+         }
+         return aux;
+      });
+   }
+   add(Peli: Peli) {
+      return this.getAll().then((res) => {
+         if (
+            res.find((p) => {
+               return p.id == Peli.id;
+            })
+         ) {
+            return false;
+         } else {
+            res.push(Peli);
+            writeFile("./pelis.json", res).then(() => {
+               return true;
+            });
+         }
+      });
+   }
 }
 export { PelisCollection, Peli };
