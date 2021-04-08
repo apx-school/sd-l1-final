be1945311c7328d57f502e9a12a76749febfb272
Joustin Ortiz <ortizjoustin@gmail.com>
2021-04-08 11:42:45 -0500

    commit de seguridad por pull inesperado

--- controllers.ts
+++ controllers.ts
@@ -2,5 +2,11 @@ import { PelisCollection, Peli } from "./models";
 
 class PelisController {
   constructor() {}
+
+  get(options) {}
+
+  add() {}
 }
+
+var controllerINST = new PelisController();
 export { PelisController };
--- models.ts
+++ models.ts
@@ -1,4 +1,5 @@
 import * as jsonfile from "jsonfile";
+import { title } from "node:process";
 
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
@@ -8,11 +9,54 @@ class Peli {
 }
 
 class PelisCollection {
+  data = jsonfile.readFile("./pelis.json");
+
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
-    });
+    let all = this.data.then((x) => x);
+    return all;
+  }
+
+  getById(id: number) {
+    let encontrarId = this.data.then((x) => x.find((i) => i.id == id));
+    return encontrarId;
+  }
+
+  search(options: any) {
+    //Funciones correctamente, falta aplicar la libreria
+    if (options.includes("title")) {
+      let encontrarTitle = this.data.then((x) =>
+        x.filter((i) => i.title.includes(options))
+      );
+
+      return encontrarTitle;
+    }
+
+    //Tag
+    if (options.includes("tag")) {
+      let encontrarTag = this.data.then((x) =>
+        x.filter((i) => i.tags.includes(options))
+      );
+
+      return encontrarTag;
+    }
+  }
+
+  add(peli: Peli) {
+    let addData = jsonfile.writeFile("./pelis.json", peli);
+    addData.then((x) => x);
+    return addData;
   }
 }
+
+const nuevaPeli = {
+  id: 5699,
+  title: "Ted",
+  tags: ["accion", "comedia"],
+};
+
+var classINST = new PelisCollection();
+classINST.add(nuevaPeli).then((x) => {
+  console.log(x);
+});
+
 export { PelisCollection, Peli };
