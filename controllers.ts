b94069562af73d58db10e7d5a69eac91ab44dc73
Tobias <tobirastorza@gmail.com>
2021-04-09 18:19:57 -0300

    terminando index

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,24 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  movies: PelisCollection;
+  constructor() {
+    this.movies = new PelisCollection();  // Instancio pelisCollection();
+  }
+
+  get(options: any) {
+    if (options.hasOwnProperty("id")) {
+      return this.movies.getById(options.id)
+    } else if (options.hasOwnProperty("search")) {
+      return this.movies.search(options.search)
+    } else {
+      return this.movies.getAll();
+    }
+  }
+
+  add(pelicula: Peli) {
+    this.movies.add(pelicula)
+  }
 }
+
 export { PelisController };
--- models.ts
+++ models.ts
@@ -2,17 +2,66 @@ import * as jsonfile from "jsonfile";
 
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
-  id: number;
   title: string;
+  id: number;
   tags: string[];
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  pelis: Peli[] = []
+  getAll() {
+    return jsonfile.readFile("./pelis.json").then((json) => {
+      this.pelis = json;
+      return this.pelis;
+    })
+  }
+
+  getById(id: number) {
+    return this.getAll().then((item) => {
+      return (item.find((p) => p.id == id));
     });
   }
+  search(options: any) {
+
+    return this.getAll().then((res) => {
+      let movies = res;
+      if (options.hasOwnProperty("title")) {
+        movies = movies.filter((p) => p.title.includes(options.title));
+      }
+      if (options.hasOwnProperty("tags")) {
+        movies = movies.filter((p) => p.tags.includes(options.tags))
+      }
+      return movies;
+
+    });
+  }
+
+  add(movie: Peli) {
+    return this.getAll().then((json) => {
+      if (json.find(item => item.id == movie.id)){
+        return false;
+      } else {
+        let aux = json;
+        aux.push(movie)
+        jsonfile.writeFile("./pelis.json", aux).then(() =>{
+          return true;
+        });
+      }
+    })
+  }
+
+
 }
+
 export { PelisCollection, Peli };
+
+const test = new PelisCollection();
+const options = new Peli();
+options.id = 99;
+options.title = "Tobias peli";
+options.tags = ["accion", "comedia"]
+const promesa = new Promise((resolve, reject) => {
+  resolve(test.add(options));
+  reject("Error")
+})
+
