031060a09b587f1ed111b702e4fc6141411bbb4f
Gonzalo Cabrera <gdcabrera93@gmail.com>
2021-04-08 23:00:35 -0300

    commit prueba

--- index.ts
+++ index.ts
@@ -1,15 +1,55 @@
 import * as minimist from "minimist";
+import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
-
   return resultado;
 }
 
 function main() {
-  const params = parseaParams(process.argv.slice(2));
+  let params = parseaParams(process.argv.slice(2));
+  const peliController = new PelisController();
 
-  console.log(params);
+  if (params._[0] == "get") {
+    const idSearched = params._[1];
+    peliController.get({ id: idSearched }).then((res) => {
+      console.log(res);
+    });
+  }
+
+  if (params._.length < 1) {
+    peliController.get(null).then((res) => {
+      console.log(res);
+    });
+  }
+
+  if (params._[0] == "search") {
+    const buscado = {
+      search: {
+        title: params.title,
+        tags: params.tag,
+      },
+    };
+    peliController.get(buscado).then((res) => {
+      console.log(res);
+    });
+  }
+
+  if (params._[0] == "add") {
+    const added = {
+      id: params.id,
+      title: params.title,
+      tags: params.tags,
+    };
+    const respuesta = peliController.add(added);
+    respuesta.then((res) => {
+      res
+        ? console.table(res)
+        : console.error(
+            "Error, la pelicula que intenta agregar ya se encuentra en el listado"
+          );
+    });
+  }
 }
 
 main();
--- models.ts
+++ models.ts
@@ -8,10 +8,45 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  getAll(): Promise<any> {
+    return jsonfile.readFile("./pelis.json").then((json) => {
+      return json;
+    });
+  }
+
+  getById(id: number): Promise<Peli> {
+    return this.getAll().then((json) => {
+      const movieFound = json.find((item: Peli) => item.id === id);
+      return movieFound;
+    });
+  }
+
+  search(options: any): Promise<Peli[]> {
+    return this.getAll().then((json) => {
+      if (options.title && options.tag) {
+        return json.filter(
+          (item) =>
+            item.title.includes(options.title) && item.tags.includes(options.tag)
+        );
+      }
+
+      if (options.title && !options.tag) {
+        return json.filter((item) => item.title.includes(options.title));
+      }
+
+      if (options.tags && !options.title) {
+        return json.filter((item) => item.tags.includes(options.tag));
+      }
+    });
+  }
+
+  add(movie: Peli): Promise<Boolean> {
+    return this.getAll().then((json) => {
+      const moviesFiltered = json.find((item: Peli) => item.id === movie.id);
+      if (!moviesFiltered) {
+        json.push(movie);
+        return jsonfile.writeFile("./pelis.json", json).then(() => true);
+      }
     });
   }
 }
