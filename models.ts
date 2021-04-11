3d11cea55620fbd987d2c14a92fa813d79c8f7ec
Amadeo Isella <aisellamk@gmail.com>
2021-04-11 20:51:13 -0300

    Desafio-final-mod2

--- models.ts
+++ models.ts
@@ -1,18 +1,54 @@
 import * as jsonfile from "jsonfile";
-
+import * as filtrar from "lodash/filter"
+import * as find from "lodash/find"
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
-  id: number;
   title: string;
+  id: number;
   tags: string[];
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  pelis: Peli[] = []
+  getAll() {
+    return jsonfile.readFile("./pelis.json").then((json) => {
+      this.pelis = json;
+      return this.pelis;
+    })
+  }
+
+  getById(id: number) {
+    return this.getAll().then((item) => {
+      return (item.find((p) => p.id == id));
     });
   }
+  search(options: any) {
+    return this.getAll().then((res) => {
+      let aux = res
+      if(options.title){
+        aux = filtrar(aux, function(item) { return item.title.includes(options.title)})
+      }
+      if(options.tag){
+        aux = filtrar(aux, function(item) { return item.tags.includes(options.tag)})
+      }
+      return aux;
+    });
+  }
+
+  add(peli: Peli): Promise<any> {
+    return this.getAll().then((json) => {
+      let repeatedId = find(json,function(item) {return item.id === peli.id});
+      if(!repeatedId){
+        json.push(peli)
+        return jsonfile.writeFile("./pelis.json", json).then(() => true);
+      } else {
+        console.log("Error, pelicula ya agregada");
+        return false;
+      }
+    })
 }
+}
+
+
+
 export { PelisCollection, Peli };
