566646069d73311c0f1127b8d9bd456c8c59f9f0
ramirodoninodev <ramirodoninodev@gmail.com>
2021-04-13 20:26:37 -0300

    Commit con todos los test pasados

--- index.ts
+++ index.ts
@@ -7,30 +7,44 @@ function parseaParams(argv) {
   return resultado;
 }
 
+function opcionIngresada(objeto) {
+  const peliController = new PelisController();
+
+  if (objeto._[0] == "get") {
+    const idSearched = objeto._[1]
+    return peliController.get({ id: idSearched }).then((res) => {
+      return res;
+    });
+  }
+
+  if (objeto._[0] == "search") {
+    const buscado = {
+      search: {
+        title: objeto.title,
+        tags: objeto.tag,
+      },
+    };
+    return peliController.get(buscado).then((res) => {
+      return res
+    });
+  }
+
+  if (objeto._[0] == "add") {
+    const added = {
+      id: objeto.id,
+      title: objeto.title,
+      tags: objeto.tags,
+    };
+    return peliController.add(added).then((res) => res ? res : "Error, la pelicula que intenta agregar ya se encuentra en el listado")
+  }
+  return peliController.get(null).then((res) => {
+    return res
+  });
+}
 function main() {
-  
-  const params = parseaParams(process.argv.slice(2));
-  const controller = new PelisController()
-  if (params._[0] === "get") {
-    let consoleId = params._[1];
-    return controller.get({ id: consoleId })
-      .then(res => console.log(res))
-      .catch(err => console.log(err))
-  }
-  else if (params._[0] === "search") {
-    delete params._
-    return controller.pelis.search(params)
-      .then(res => console.log(res))
-      .catch(err => console.log(err))
-  } else if (params._[0] === "add") {
-    delete params._
-    return controller.pelis.add(params)
-    .then((res) => console.log(res))
-  } else {
-    return controller.get(null)
-    .then(res => console.log(res))
-    .catch(err => console.log(err)) 
-  }
+  const params = parseaParams(process.argv.slice(2))
+  let resp = opcionIngresada(params)
+  resp.then( r => console.log(r))
 }
 
 main();
