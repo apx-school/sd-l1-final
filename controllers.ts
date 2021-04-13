6065a9f5ee830ba0eaa13339ad193193d5943e8a
ramirodoninodev <ramirodoninodev@gmail.com>
2021-04-13 20:14:28 -0300

    Commit del desafio

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,25 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  pelis : PelisCollection
+  constructor() {
+    this.pelis = new PelisCollection()
+  }
+  get(options:any){
+    
+    if(!options){
+      return this.pelis.getAll()
+    }
+    if(options.hasOwnProperty("id")){
+      return this.pelis.getById(options.id)
+    }
+    if(options.hasOwnProperty("search")){
+      return this.pelis.search(options.search)
+    }
+  }
+  add(peli:Peli){
+    return this.pelis.add(peli)
+  }
 }
+
 export { PelisController };
--- models.ts
+++ models.ts
@@ -1,4 +1,6 @@
 import * as jsonfile from "jsonfile";
+import * as filtrar from "lodash/filter"
+
 
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
@@ -8,11 +10,55 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  pelis :  Peli[]
+  getAll() {
+    const promesa = jsonfile.readFile("./pelis.json")
+     promesa.then((res) => {
+      this.pelis = res
+      return this.pelis
     });
-  }
+     return promesa
+ }
+   getById(id:number){
+    return  this.getAll()
+    .then( respuesta =>
+      respuesta.find((p)=>{
+        if(p.id == id){
+          return p
+        }
+      })) 
+    } 
+
+    search(options: any) {
+      return this.getAll()
+      .then((res) => {
+         let respuesta = res
+
+         if (options.hasOwnProperty("title")) {
+          respuesta = res.filter((p) => p.title.includes(options.title))
+         }
+         if(options.hasOwnProperty("tag")){
+          respuesta = filtrar(respuesta, function(item){return item.tags.includes(options.tag)})
+         }
+         return respuesta
+      });
+   }
+   add(Peli: Peli) {
+      return this.getAll().then((res) => {
+         if (
+            res.find((p) => {
+               return p.id == Peli.id;
+            })
+         ) {
+            return false;
+         } else {
+            res.push(Peli);
+            jsonfile.writeFile("./pelis.json", res).then(() => {
+               return true;
+            });
+         }
+      });
+   }
 }
-export { PelisCollection, Peli };
+
+export { PelisCollection, Peli }
