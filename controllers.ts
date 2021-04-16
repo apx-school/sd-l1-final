ad1b145d6fa5935d44114c431c0c53aa912d83dd
fiba-dev <fibarrola06@gmail.com>
2021-04-15 22:48:49 -0300

    Commit Ibarrola Francisco

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,45 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  coleccionDePelis: PelisCollection;
+  promesaController: Promise<any>;
+  constructor() {
+    this.coleccionDePelis = new PelisCollection();
+    const promesa = this.coleccionDePelis.getAll();
+    this.promesaController = promesa;
+  }
+
+  get(option:any) {
+    let resultado: Promise<any>;
+    if (option.hasOwnProperty("add")) {
+      resultado = this.add(option.add);
+    } else if (option.hasOwnProperty("search")) {
+      resultado = this.coleccionDePelis.search(option.search).then((n) => {
+        return n;
+      });
+    } else if (option.hasOwnProperty("id")) {
+      resultado = this.coleccionDePelis.getById(option.id).then((n) => {
+        return n;
+      });
+    } else if (option.hasOwnProperty("get")) {
+      resultado = this.coleccionDePelis.getById(option.get).then((n) => {
+        return n;
+      });
+    } else {
+      resultado = this.coleccionDePelis.getAll().then((n) => {
+        return n;
+      });
+    }
+
+    return resultado;
+  }
+  add(objeto: Peli) {
+    let peli = new Peli();
+    peli.id = objeto.id;
+    peli.title = objeto.title;
+    peli.tags = objeto.tags;
+    let promesaAdd = this.coleccionDePelis.add(peli);
+    return promesaAdd;
+  }
 }
 export { PelisController };
--- index.ts
+++ index.ts
@@ -1,15 +1,41 @@
 import * as minimist from "minimist";
+import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
 
-  return resultado;
+  let aux: {} = {};
+  let propiedad: {} = {};
+
+  if (resultado._[0] == "search") {
+    for (const key in resultado) {
+      if (key != "_") {
+        aux[key] = resultado[key];
+      }
+    }
+    propiedad = { search: aux };
+  }
+  if (resultado._[0] == "get") {
+    propiedad = { id: resultado._[1] };
+  }
+  if (resultado._[0] == "add") {
+    for (const key in resultado) {
+      if (key != "_") aux[key] = resultado[key];
+    }
+    propiedad = { add: aux };
+  }
+
+  return propiedad;
 }
 
 function main() {
-  const params = parseaParams(process.argv.slice(2));
-
-  console.log(params);
+  const controller = new PelisController();
+  controller.promesaController.then(() => {
+    const params = parseaParams(process.argv.slice(2));
+    controller.get(params).then((n) => {
+      return n;
+    });
+  });
 }
 
 main();
