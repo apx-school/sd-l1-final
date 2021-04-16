12416983438b84c7f33797b111a237487bbbb89c
Fernando de Row <ferderow@gmail.com>
2021-04-15 23:29:25 -0300

    creación de metodos en models.ts

--- models.ts
+++ models.ts
@@ -8,11 +8,41 @@ class Peli {
 }
 
 class PelisCollection {
+  data: Promise<any>;
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
+    return jsonfile.readFile("./pelis.json").then((res) => {
+      this.data = res;
       return [];
     });
   }
+  getById(id: number) {
+    return this.data.then((res) => {
+      return res.find((item) => {
+        return item.id == id;
+      });
+    });
+  }
+  search(options: any) {
+    return this.data.then((res) => {
+      if (options.hasOwnProperty("title")) {
+        return res.filter((item) => {
+          return item.title.includes(options.title);
+        });
+      } else if (options.hasOwnProperty("title")) {
+        return res.filter((item) => {
+          return item.title.includes(options.title);
+        });
+      }
+    });
+  }
 }
+
+const main = () => {
+  const prueba = new PelisCollection();
+  console.log(prueba.getAll());
+  console.log(prueba.data);
+};
+
+main();
+
 export { PelisCollection, Peli };
