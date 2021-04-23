4b4d2206294f6b9313bb20768b807df282c3a5eb
Marcelo Zapaia <zapaiamarce@gmail.com>
2021-04-23 12:34:41 -0300

    prueba

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,25 @@
+import * as isEmpty from "lodash/isEmpty";
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  peliculas: PelisCollection;
+  constructor() {
+    this.peliculas = new PelisCollection();
+  }
+  get(options) {
+    if (options.id) {
+      return this.peliculas.getById(options.id);
+    }
+    if (options.search) {
+      return this.peliculas.search(options.search);
+    }
+    if (isEmpty(options)) {
+      return this.peliculas.getAll();
+    }
+  }
+  add(pelicula) {
+    return this.peliculas.add(pelicula);
+  }
 }
+
 export { PelisController };
