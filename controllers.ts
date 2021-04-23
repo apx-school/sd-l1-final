a4cfa419b66a4b6f75119613a376b95edc24d263
Shiananmiad Bermudez <bermudezdamian7@gmail.com>
2021-04-23 19:39:13 -0300

    desafio 2 terminado

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,28 @@
-import { PelisCollection, Peli } from "./models";
 
+import { PelisCollection, Peli } from "./models";
 class PelisController {
-  constructor() {}
+  peliculas:PelisCollection
+  promesa: Promise <any>
+  constructor() {
+    let pelis = new PelisCollection
+    this.peliculas = pelis
+    const laPreomesa = this.peliculas.getAll();
+    this.promesa = laPreomesa;
+  }
+  get(options): Promise<any> {
+    if (options.id) {
+      return this.peliculas.getById(options.id);
+    }
+    if (options.search) {
+      return this.peliculas.search(options.search);
+    } else {
+      return this.peliculas.getAll();
+    }
+  }
+
+  add(peli) {
+    return this.peliculas.add(peli);
+  }
+
 }
 export { PelisController };
--- models.ts
+++ models.ts
@@ -5,14 +5,63 @@ class Peli {
   id: number;
   title: string;
   tags: string[];
-}
+};
 
 class PelisCollection {
+  
+  pelis: Peli [] = [];
+
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    const promesa = jsonfile.readFile("./pelis.json")
+    promesa.then((json: Peli[]) => {
+    this.pelis = json
+    });    
+    return promesa;
+  };
+
+
+  getById(id: number) {
+    return this.getAll().then((json) => {
+      return json.find((item) => item.id == id);
     });
   }
+
+  search(options: any) {
+    return this.getAll().then((json) => {
+      let peliculas = json;
+
+      if (options.title) {
+        this.pelis = peliculas.filter((pelicula) => {
+          return pelicula.title.includes(options.title);
+        });
+      }
+      if (options.tag) {
+        this.pelis = peliculas.filter((pelicula) => {
+          return pelicula.tags.includes(options.tag);
+        });
+      }
+      return peliculas;
+    });
+  }
+
+
+  add(peli: Peli): Promise<boolean> {
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+      if (peliExistente) {
+        return false;
+      } else {
+        this.pelis.push(peli);
+        const promesaDos = jsonfile.writeFile("./pelis.json", this.pelis);
+
+        return promesaDos.then(() => {
+          return true;
+        });
+      }
+    });
+
+    return promesaUno;
+  }
 }
+
 export { PelisCollection, Peli };
+
