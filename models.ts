14ecc3d909c77d451ccf8fdcf5febacac7e78089
ghiglionemag <ghiglionemag@gmail.com>
2021-04-22 21:47:40 -0300

    pasa models no controllers

--- models.ts
+++ models.ts
@@ -8,46 +8,52 @@ class Peli {
 }
 
 class PelisCollection {
-  peliculas: Peli[];
-
-  getAll() {
+  data: Peli[];
+  getAll(): Promise<Peli[]> {
     return jsonfile.readFile("./pelis.json").then((json) => {
+      this.data = json;
       return json;
     });
   }
   getById(id: number) {
-    return this.getAll().then((json) => {
-      return json.find((item) => item.id == id);
+    return this.getAll().then((res) => {
+      return res.find((item) => item.id == id);
     });
   }
   search(options: any) {
-    return this.getAll().then((json) => {
+    return this.getAll().then((res) => {
       if (options.title && options.tag) {
-        return json.filter(
+        return res.filter(
           (item) =>
             item.title.includes(options.title) &&
             item.tags.includes(options.tag)
         );
       }
       if (options.title) {
-        return json.filter((item) => item.title.includes(options.title));
+        return res.filter((item) => item.title.includes(options.title));
       }
       if (options.tag) {
-        return json.filter((item) => item.tags.includes(options.tag));
+        return res.filter((item) => item.tags.includes(options.tag));
       }
     });
   }
 
-  add(Peli: Peli) {
-    return this.getAll().then((json) => {
-      const exist = json.find((p) => {
-        return p.id == Peli.id;
-      });
-      if (!exist) {
-        json.push(Peli);
-        return jsonfile.writeFile("./pelis.json", json);
+  add(peli: Peli): Promise<boolean> {
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+      if (peliExistente) {
+        return false;
+      } else {
+        // magia que agrega la pelicula a un objeto data
+        this.data.push(peli);
+        const promesaDos = jsonfile.writeFile("./pelis.json", this.data);
+
+        return promesaDos.then(() => {
+          return true;
+        });
       }
     });
+
+    return promesaUno;
   }
 }
 export { PelisCollection, Peli };
