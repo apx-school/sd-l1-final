5ef3df961e751a5543035cde24aaf9db5d4e3722
guido.colombo <colombo.guido@hotmail.com>
2021-04-25 12:19:05 -0300

    pasaron los test

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import * as minimist from "minimist";
+import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
@@ -6,10 +7,25 @@ function parseaParams(argv) {
   return resultado;
 }
 
+function parametros(controller, params) {
+  if (params._ == "add") {
+    controller.add(params);
+  } else if (params._.includes("get")) {
+    controller.get({ id: params._[1] }).then((x) => console.log(x));
+  } else if (params._ == "search") {
+    delete params._;
+    controller.get({ search: params }).then((x) => console.log(x));
+  } else {
+    controller.get().then((x) => console.log(x));
+  }
+}
+
 function main() {
   const params = parseaParams(process.argv.slice(2));
-
+  const controller = new PelisController();
+  parametros(controller, params);
   console.log(params);
+
 }
 
 main();
--- models.ts
+++ models.ts
@@ -1,4 +1,5 @@
 import * as jsonfile from "jsonfile";
+import * as _ from "lodash";
 
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
@@ -8,11 +9,53 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  peli: Peli[];
+  promise: Promise<any>;
+
+  getAll(): Promise<Peli[]> { 
+    this.promise = jsonfile.readFile("./pelis.json").then((obj) =>{
+    this.peli = obj;
+    return obj;
+    })
+    return this.promise;
+  }
+  getById(id:number): Promise<any>{
+    return this.promise.then((obj) =>{
+      const resultadoId = _.find(obj, ["id", id]);
+      return resultadoId
+    })
+  }
+
+  search(options: any): Promise<any> {
+    return this.promise.then((obj) => {
+      const filtrado = _.filter(obj, (x) => {
+        if (options.tag && options.title) {
+          return (
+            _.includes(x.tags, options.tag) &&
+            x.title.toLowerCase().includes(options.title.toLowerCase())
+          );
+        }
+        if (options.title && !options.tag) {
+          return x.title.toLowerCase().includes(options.title.toLowerCase());
+        }
+        if (!options.title && options.tag) {
+          return _.includes(x.tags, options.tag);
+        }
+      });
+      return filtrado;
     });
   }
+
+  add(peli:Peli): Promise<any> {
+    return this.getAll().then((obj) => {
+      if (!_.find(obj, ["id", peli.id])) {
+        obj.push(peli);
+        return jsonfile.writeFile("./pelis.json", obj);
+      } else {
+        return false;
+      }
+    });
+  }
+  
 }
 export { PelisCollection, Peli };
