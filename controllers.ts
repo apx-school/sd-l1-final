dc5b50174c78c5aaac3695adac2d6d165f58747f
Pablo Murillo <pablomurillo.sp@gmail.com>
2021-04-26 19:24:37 -0300

    hola

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,45 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  pelis: PelisCollection;
+  promesa: Promise<any>;
+
+  constructor() {
+    this.pelis = new PelisCollection();
+    let promesa = this.pelis.getAll();
+    this.promesa = promesa;
+  }
+
+  get(options: any) {
+    if (options.action == "esta vacio") {
+      return this.pelis.getAll();
+    }
+    if (options.action == "get") {
+      return this.pelis.getById(options.id);
+    }
+    if (options.action == "search" && options.params == "title") {
+      return this.pelis.getAll().then(() => {
+        return this.pelis.search(options.params, options.do);
+      });
+    }
+    if (options.action == "search" && options.params == "tags") {
+      return this.pelis.getAll().then(() => {
+        return this.pelis.search(options.params, options.do);
+      });
+    }
+  }
+
+  add(options: any) {
+    let peli = new Peli();
+
+    peli.id = options.id;
+    peli.title = options.title;
+    peli.tags = options.tags;
+
+    return this.pelis.getAll().then(() => {
+      return this.pelis.add(peli);
+    });
+  }
 }
+
 export { PelisController };
--- index.ts
+++ index.ts
@@ -1,15 +1,67 @@
+import { PelisController } from "./controllers";
 import * as minimist from "minimist";
+import * as vacio from "lodash/isEmpty";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
+  //el "_" de resultado representa el array que genera minimist dentro del objeto respuesta
+  let controller = new PelisController();
 
-  return resultado;
+  if (vacio(resultado._)) {
+    let options = {
+      action: "esta vacio",
+    };
+    return options;
+  }
+  if (resultado._.includes("get")) {
+    let options = {
+      action: "get",
+      id: resultado._[1],
+    };
+    return options;
+  }
+  if (resultado._.includes("search") && resultado.title) {
+    let options = {
+      action: "search",
+      params: "title",
+      do: resultado.title,
+    };
+    return options;
+  }
+  if (resultado._.includes("search") && resultado.tag) {
+    let options = {
+      action: "search",
+      params: "tags",
+      do: resultado.tag,
+    };
+    return options;
+  }
+
+  if (resultado._[0] == "add") {
+    let options = {
+      action: "add",
+
+      id: resultado.id,
+      title: resultado.title,
+      tags: resultado.tags,
+    };
+    return options;
+  }
 }
 
 function main() {
-  const params = parseaParams(process.argv.slice(2));
-
-  console.log(params);
+  const collection = new PelisController();
+  collection.promesa.then(() => {
+    const res = parseaParams(process.argv.slice(2));
+    if (res.action !== "add") {
+      collection.get(res).then((r) => {
+        console.log(r);
+      });
+    } else {
+      collection.add(res).then((r) => {
+        console.log(r);
+      });
+    }
+  });
 }
-
 main();
