15adedf834489279120911d3a373a46e5f76a7c2
felix <rodilfelix16@gmail.com>
2021-04-27 21:48:50 -0300

    segundo commit

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,22 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  pelis: PelisCollection;
+  constructor() {
+    this.pelis = new PelisCollection()
+  }
+  get(options):Promise<any>{
+    if(options.id){
+      return this.pelis.getById(options.id)
+    }
+    if(options.search){
+      return this.pelis.search(options.search)
+    }else {
+      return this.pelis.getAll()
+    }
+  }
+  add(peli:Peli) {
+    return this.pelis.add(peli)
+ }
 }
 export { PelisController };
