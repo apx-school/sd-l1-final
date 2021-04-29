1cd9c65db23f6f8d3d939e0e54dc362623197fa8
Marina Comotti <m.comotti@hotmail.com>
2021-04-29 17:20:49 -0300

    primer commit, todavía falla el test search tag

--- models.ts
+++ models.ts
@@ -1,6 +1,6 @@
 import * as jsonfile from "jsonfile";
 
-// no modificar estas propiedades, agregar todas las que quieras
+
 class Peli {
   id: number;
   title: string;
@@ -8,11 +8,55 @@ class Peli {
 }
 
 class PelisCollection {
+  pelis: Peli [];
+  
+
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json").then((pelis) => {
+      return this.pelis = pelis;
     });
+  }
+
+  getById(id:number){
+    return this.getAll().then((pelis)=>{
+      return pelis.find((peli)=>{
+        return peli.id == id;
+      });
+    });
+  }
+
+  search(options:any){
+    return this.getAll().then((pelis)=>{
+      if (options.title){
+        return pelis.filter((peli)=>{
+          return peli.title.includes(options.title);
+        });
+      };
+       if (options.tag){
+        return pelis.filter((peli)=>{
+          return peli.tags.includes(options.tag);
+        });
+      }
+    });
+
+  }
+  add(peli: Peli): Promise<boolean> {
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+      if (peliExistente) {
+        return false;
+      } else {
+        // magia que agrega la pelicula a un objeto data
+        this.pelis.push(peli);
+        const data = this.pelis;
+        const promesaDos = jsonfile.writeFile("./pelis.json", data);
+        return promesaDos.then(() => {
+          return true;
+        });
+      }
+    });
+
+    return promesaUno;
   }
 }
 export { PelisCollection, Peli };
+
