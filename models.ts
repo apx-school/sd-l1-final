cda129a1e5f18d8f3f12562b711b3083eda3e52e
Gian <marattagian15@gmail.com>
2021-04-28 23:34:20 -0300

    first commit and add() still not working

--- models.ts
+++ models.ts
@@ -8,11 +8,49 @@ class Peli {
 }
 
 class PelisCollection {
+  data: Peli[] = [];
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json").then((json) => {
+      this.data = json;
+      return json;
     });
   }
+
+  getById(id: number): Promise<Peli> {
+    return this.getAll().then((json) => {
+      return json.find((item) => item.id == id);
+    });
+  }
+
+  add(peli: Peli): Promise<boolean> {
+    const firstPromise = this.getById(peli.id).then((existantPeli) => {
+      if (existantPeli) {
+        return false;
+      } else {
+        this.data.push(peli);
+        const secondPromise = jsonfile.writeFile("./pelis.json", this.data);
+        return secondPromise.then(() => {
+          return true;
+        })
+      }
+    })
+
+    return firstPromise;
+  }
+  // search(options: any): Promise<any> {
+  //   return this.getAll().then(moviesArray => {
+  //     let movies = moviesArray;
+
+  //     if (options.title) {
+  //       movies = movies.filter(movie => movie.title.includes(options.title));
+  //     }
+  //     if (options.tag) {
+  //       movies = movies.filter(movie => movie.tags.includes(options.tag));
+  //     }
+
+  //     return movies;
+  //   })
+
+  // }
 }
 export { PelisCollection, Peli };
