0128144004651c8621d03f5a023be8d5a2754daf
Lucas Calvetti <lic.calvo099@gmail.com>
2021-05-01 16:41:08 -0300

    Todos los tests pasaron, programa terminado

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,26 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  pelis:PelisCollection
+  constructor() {
+    this.pelis = new PelisCollection;
+  }
+
+  get(options:any){
+    let resultado:Promise<any>
+    if(options.id){
+      resultado = this.pelis.getById(options.id);
+    }else if(options.search){
+      resultado = this.pelis.search(options.search);
+    }else{ resultado = this.pelis.getAll().then((peliculas)=>{
+      return peliculas
+    })}
+
+    return resultado;
+  }
+  
+  add(peli:Peli){
+    return this.pelis.add(peli);
+  }
 }
 export { PelisController };
--- index.ts
+++ index.ts
@@ -1,15 +1,59 @@
 import * as minimist from "minimist";
+import { PelisController } from "./controllers";
+import { Peli } from "./models";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
 
   return resultado;
 }
+function processInput(input) {
+
+  const controller = new PelisController;
+
+  if (input._[0] == "add"){
+    const peliToAdd:Peli = {
+      id:input.id,
+      title:input.title,
+      tags: input.tags
+    };
+    return controller.add(peliToAdd).then((r)=>{
+      return r;
+    });
+
+  } else if (input._[0] == "get"){
+
+    const idToGet = {id: input._[1]}
+    return controller.get(idToGet).then((r)=>{
+      return r;
+    });
+
+  }else if (input._[0] == "search"){
+    let param = {};
+    if (input.title){
+      param["title"] = input.title;
+    }
+    if (input.tag){
+      param["tag"] = input.tag;
+    }
+    return controller.get({search: param}).then((r)=>{
+      return r;
+    });
+  } else {
+    return controller.pelis.getAll().then((r)=>{
+    return r;}
+    )
+  }
+}
+
 
 function main() {
   const params = parseaParams(process.argv.slice(2));
 
-  console.log(params);
+  const resultado = processInput(params);
+  
+  resultado.then((r)=>{console.log(r)});
+  
 }
 
 main();
