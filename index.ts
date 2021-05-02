e3e3075b92b3db46b88675bbe0e6d4c24ac3741b
SIL_SILVA <silsilva.89@gmail.com>
2021-05-02 19:20:19 -0300

    corrección 1

--- index.ts
+++ index.ts
@@ -9,27 +9,27 @@ function parseaParams(argv) {
 
 function processOptions(argv) {
   const controller = new PelisController();
+  if (argv._[0] == "get") {
+    const id = argv._[1];
+    return controller.get({ id: id }).then((movie) => {
+      return movie;
+    });
 
-   if (argv._[0] == "search") {
+  }else if (argv._[0] == "search") {
     var params = {};
     if (argv.title) {
       params["title"] = argv.title;
     }
     if (argv.tag) {
       params["tag"] = argv.tag;
-    }
-    if (argv._[0] == "get") {
-      const id = argv._[1];
-      return controller.get({ id: id }).then((movie) => {
-        return movie;
-      });
+  
     }
     return controller.get({ search: params }).then((movie) => {
       return movie;
     });
     
-  }  if (argv._[0] == "add") {
-    var newMovie = {
+  } if (argv._[0] == "add") {
+    let newMovie = {
       id: argv.id,
       title: argv.title,
       tags: argv.tags,
@@ -37,7 +37,7 @@ function processOptions(argv) {
     return controller.add(newMovie).then((movie) => {
       return movie;
     });
-  }  if (argv._.length == 0) {
+  } else if (argv._.length == 0) {
     return controller.get({}).then((movie) => {
       return movie;
     });
@@ -47,7 +47,7 @@ function processOptions(argv) {
 function main() {
   const params = parseaParams(process.argv.slice(2));
   const result = processOptions(params);
-  result.then((r) => console.log(r));
+  result.then((r)=>console.log(r));
 }
 
 main();
