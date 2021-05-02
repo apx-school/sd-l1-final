6d562009284816e531a749dce430ddbc5044d09d
ArielGol <ariel00golubickas@gmail.com>
2021-05-02 20:09:02 -0300

    Paso los 8 tests

--- index.ts
+++ index.ts
@@ -1,15 +1,52 @@
 import * as minimist from "minimist";
+import {PelisController} from "./controllers"
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
-
   return resultado;
 }
 
-function main() {
-  const params = parseaParams(process.argv.slice(2));
+function processOptions(options){
+ const controller =new PelisController();
+ if(options._[0]=="add"){
+   var nuevaPeli={
+     id:options.id,
+     title:options.title,
+     tags:options.tags
+   };
+   return controller.add(nuevaPeli).then((respuesta)=>{
+     return respuesta;
+   })
+ } else if(options._[0]=="get"){
+   const idNumber=options._[1];
+   return controller.get({id:idNumber}).then((respuesta)=>{
+     return respuesta;
+   });
+ } else if (options._[0]=="search"){
+   let propiedad ={};
+   if(options.title){
+     propiedad["title"]=options.title;
+   }
+   if(options.tag){
+     propiedad["tag"]=options.tag;
+   }
+   return controller.get({search:propiedad}).then((respuesta)=>{
+     return respuesta;
+   })
+ } else if(options._.length==0){
+   return controller.get([]).then((respuesta)=>{
+     return respuesta;
+   });
+ }
+}
 
-  console.log(params);
+
+
+function main() {
+
+  const params = parseaParams(process.argv.slice(2));
+  const resultadoFinal = processOptions(params);
+  resultadoFinal.then((result)=>console.log(result))
 }
 
 main();
--- models.ts
+++ models.ts
@@ -1,6 +1,5 @@
 import * as jsonfile from "jsonfile";
 
-// no modificar estas propiedades, agregar todas las que quieras
 class Peli {
   id: number;
   title: string;
@@ -8,11 +7,50 @@ class Peli {
 }
 
 class PelisCollection {
+  peli:Peli[]=[];
+
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+      return jsonfile.readFile("./pelis.json").then((json) => {
+        return this.peli=json;
+        });
+    };
+  getById(id:number):Promise<Peli>{
+    const promesa= this.getAll().then(()=>{
+       return this.peli.find((item)=>item.id==id);
+      });
+      return promesa;
+    }
+
+  search(options:any):Promise<any>{
+    return this.getAll().then((resultado)=>{
+      let peliEncontrada=resultado;
+      if(options.title){
+        peliEncontrada=peliEncontrada.filter((item)=> {
+          return item.title.includes(options.title)||item.title.toLowerCase().includes(options.title);
+        });
+    }
+      if (options.tag){
+        peliEncontrada= peliEncontrada.filter((item)=>{
+        return item.tags.includes(options.tag);
+      });
+    }
+    return peliEncontrada;
     });
   }
-}
-export { PelisCollection, Peli };
+  add(pelicula: Peli): Promise<boolean> {
+    const promesaUno = this.getById(pelicula.id).then((peliExistente) => {
+      if (peliExistente) {
+        return false;
+      } else {
+        this.peli.push(pelicula);
+        const promesaDos = jsonfile.writeFile("./pelis.json", this.peli);
+        return promesaDos.then(() => {
+          return true;
+        });
+      }
+    });
+    return promesaUno;
+  }
+};
+
+    export { PelisCollection, Peli };
