0dcc56005a9b1eac1afe0df06237611ef7b53f58
Lion <leonardofontan14@gmail.com>
2021-05-03 22:37:12 -0300

    Complete los tests de Models y Controllers, Me falta el index.ts

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,37 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  data: PelisCollection;
+
+  constructor() {
+    this.data = new PelisCollection();
+  }
+  //Me quede aca #######################
+  get(options: any): Promise<any> {
+    if (options.id) {
+      const result = this.data.getById(options.id).then((r) => {
+        return r;
+      });
+      return result;
+    }
+    if (options.search) {
+      const result = this.data.search(options.search);
+      return result;
+    }
+    if (options) {
+      const result = this.data.getAll().then((r) => {
+        return r;
+      });
+      return result;
+    }
+  }
+
+  add(peli: Peli) {
+    return this.data.add(peli).then((result) => {
+      return result;
+    });
+  }
 }
 export { PelisController };
+
+const test = new PelisController();
--- models.ts
+++ models.ts
@@ -8,11 +8,55 @@ class Peli {
 }
 
 class PelisCollection {
+  data: Peli[] = [];
+
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json").then((json) => {
+      this.data = json;
+      return json;
     });
   }
+  getById(id: number) {
+    return this.getAll().then((pelis) => {
+      return pelis.find((peli) => {
+        return peli.id == id;
+      });
+    });
+  }
+
+  add(peli: Peli): Promise<boolean> {
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+      if (peliExistente) {
+        return false;
+      } else {
+        this.data.push(peli);
+
+        const promesaDos = jsonfile.writeFile("./pelis.json", this.data);
+
+        return promesaDos.then(() => {
+          return true;
+        });
+      }
+    });
+
+    return promesaUno;
+  }
+
+  search(options: any) {
+    const promesa = this.getAll().then((p) => {
+      if (options.title && options.tag) {
+        return p.filter(
+          (r) => r.title.includes(options.title) && r.tags.includes(options.tag)
+        );
+      }
+      if (options.title) {
+        return p.filter((r) => r.title.includes(options.title));
+      } else if (options.tag) {
+        return p.filter((r) => r.tags.includes(options.tag));
+      }
+    });
+    return promesa;
+  }
 }
+
 export { PelisCollection, Peli };
