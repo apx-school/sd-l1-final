088cfe17fdd4aaa1ef08dda24c75641d45c5f126
Rafa Lynch <rafa_lynch@hotmail.com>
2021-05-06 16:15:01 -0300

    models anda. controller armando

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,51 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  data: PelisCollection;
+  constructor() {
+    this.data = new PelisCollection;
+  }
+  get(options){
+    if (options.id){
+      return this.data.getById(options.id)
+    }
+    if (options.search.title && options.search.tag) {
+      return this.data.search(options.search)
+    }
+    if (options.search.title){
+      return this.data.search(options.search)
+    }
+    if (options.search.tag){
+      return this.data.search(options.search)
+    }
+    else {
+      return this.data.getAll();
+    }
+  }
+
+  add(peli: Peli){
+
+  }
 }
 export { PelisController };
+
+function pruebas (){
+  const controlador = new PelisController;
+  console.log(controlador.data)
+
+  //pruebas de get
+  const opt = {
+    search: {
+      // title: "Titanic",
+      // tag: "accion"
+    }
+  }
+  controlador.get(opt).then((p) => console.table(p))
+
+  //pruebas de add
+
+
+}
+
+
+pruebas();
--- models.ts
+++ models.ts
@@ -1,4 +1,5 @@
 import * as jsonfile from "jsonfile";
+import { includes } from "lodash";
 
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
@@ -9,10 +10,91 @@ class Peli {
 
 class PelisCollection {
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json")
+      .then((p) => {
+        return p;
+      });
+  }
+  getById(id: number): Promise<Peli>{
+    return jsonfile.readFile("./pelis.json")
+      .then((p) => {
+        return p.find((c) => c.id == id)
+      })
+  }
+  search(options: any): Promise<Peli[]>{
+    if (options.title){
+      return jsonfile.readFile("./pelis.json")
+        .then((p) => {
+          return p.filter((c) => includes(c.title, options.title))
+        });
+    }
+    if (options.tag){
+      return jsonfile.readFile("./pelis.json")
+        .then((p) => {
+          return p.filter((c) => includes(c.tags, options.tag))
+        });
+    }
+  }
+  add(peli: Peli): Promise<boolean>{
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+      if (peliExistente) {
+        return false;
+      } else {
+        // magia que agrega la pelicula a un objeto data
+        const data = jsonfile.readFile("./pelis.json");
+        data.then((d) => {
+          const data = d.concat(peli);
+          const promesaDos = jsonfile.writeFile("./pelis.json", data)
+            .then(() => {
+            return true;
+            });
+            return promesaDos;
+        });
+        return true;
+      }
     });
+
+    return promesaUno;
   }
 }
+
 export { PelisCollection, Peli };
+
+// function pruebas (){
+//   //nueva collection
+//   const col = new PelisCollection;
+
+//   //pruebas de .getAll
+//   const promesa = col.getAll();
+//   promesa.then((p)=>
+//     console.table(p)
+//   )
+
+//   //pruebas de .getById
+//   const getbyid = col.getById(1)
+//   getbyid.then((p)=>{
+//     if (p) {
+//       console.table(p)
+//     }
+//     else {
+//       console.log("No coincide ninguna id")
+//     }
+    
+//   })
+
+//   // pruebas de .search
+//   const opt = {
+//     title: "Harry"
+//   }
+//   const prom = col.search(opt)
+//   prom.then((p) => console.table(p))
+
+//   //pruebas de .add
+//   const peli = new Peli;
+//   peli.id = 4;
+//   peli.tags = ["accion"];
+//   peli.title = "La isla siniestra"
+//   col.add(peli).then((p) => console.log(p));
+// }
+
+// pruebas();
