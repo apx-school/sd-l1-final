77fdc10eeed27cfa194346b47c898d42cd7ccd0d
Gustavo Nievas <gustavonievas246@gmail.com>
2021-05-07 22:51:41 -0300

    nuevos cambios

--- index.ts
+++ index.ts
@@ -4,22 +4,22 @@ import { PelisCollection } from "./models"
 
 function parseaParams(argv): controllerOptions {
   const resultado = minimist(argv);
-  return resultado //{ action: resultado.action,
-    //params: JSON.parse(resultado.params),
-  //};
+  return { action: resultado.action,
+    params: JSON.parse(resultado.params),
+  };
 }
 
 function main() {
   const controller = new PelisController();
-    //controller.promesa.then(()=>{
+    controller.promesa.then(()=>{
     const params = parseaParams(process.argv.slice(2));
     const resultado = controller.processOptions(params);
-    resultado.then(()=>{
-
-      console.log(resultado);
-    })
-
+    console.log(resultado);
+  })
 }
-
-
+    
 main();
+
+
+
+
