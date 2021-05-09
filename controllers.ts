f8ed71ac0a7af98fe69954e664dbdb227069bcbd
Gustavo Nievas <gustavonievas246@gmail.com>
2021-05-09 17:24:56 -0300

    cambios definitivos

--- controllers.ts
+++ controllers.ts
@@ -1,33 +1,27 @@
 import { PelisCollection, Peli } from "./models";
 
-class controllerOptions{
-  action: "get" | "search"
-  params: any
-}
 
 class PelisController {
   pelis: PelisCollection;
   promesa: Promise<any>;
 
+
   constructor() {
     this.pelis = new PelisCollection();
   }
-  processOptions(options: controllerOptions): Promise<any> {
-    var resultado;
-    if (options.action == "get" && options.params.id) {
-      resultado = this.pelis.getById(options.params.id);
-      } else if (options.action == "get") {
-        resultado = this.pelis.getAll();
-      }else if (options.action == "search" && options.params.title){
-        this.pelis.search(options.params.title);
-      }else if (options.action == "search" && options.params.tag){
-        this.pelis.search(options.params.title);
-      }
-       
-      return resultado;
+  get(options:any): Promise<any> {
+    if (options.id) {
+      return this.pelis.getById(options.id);//then((r) => r);
+    } else if (options.search) {
+      return this.pelis.search(options.search);//then((r) => r);
+    } else {
+      return this.pelis.getAll().then((p)=>p);
+    }
   }
+
+
   add(peli:Peli){
-    return this.pelis.add(peli);
+    return this.pelis.add(peli).then((result)=>{return result});
   }
 }
-export { PelisController, controllerOptions };
+export { PelisController };
--- models.ts
+++ models.ts
@@ -1,4 +1,6 @@
 import * as jsonfile from "jsonfile";
+import { includes, find } from "lodash";
+
 
 class Peli {
 
@@ -10,40 +12,39 @@ class Peli {
 class PelisCollection {
   data: Peli[] = [];
   getAll(): Promise<Peli[]> {
-    return jsonfile.readfile("./pelis.json").then((pelisJson) => {
-      this.data = pelisJson;
-    return pelisJson;
+    return jsonfile.readFile("./pelis.json").then((data) => {
+      return this.data = data;
     });
   }
-  getById(id:number):Promise<any> {
+  getById(id:number): Promise<any>{
     return this.getAll().then((pelcoll) => {
-      const idPorParametro = pelcoll.find((item) => {
-        if(item.id == id) {
-          return true;}});
+      return pelcoll.find((item) => {
+       return item.id == id})
 
-          return idPorParametro;
+       
     })
   }
-  search(options:any):Promise<any>{
-    return this.getAll().then(()=>{
-      const resultadoTitle = options.filter(function (item) {
-        if (item.title.includes(options.title) || item.title.toLowerCase().includes(options.title))
-        return item;
-     });
-       const resultadoTag = options.filter(function (item) {
-         if (item.tag.includes()) return item;
-         });
-     return {resultadoTitle, resultadoTag};
+  search(options: any): Promise<Peli[]> {
+    return this.getAll().then((p) => {
+      let respuesta = p;
+      if (options.title) {
+        respuesta = respuesta.filter((c) => includes(c.title, options.title));
+      }
+      if (options.tag) {
+        respuesta = respuesta.filter((c) => includes(c.tags, options.tag));
+      }
 
+      return respuesta;
     });
+     
   }
   add(peli: Peli): Promise<boolean> {
     const promesaUno = this.getById(peli.id).then((peliExistente) => {
       if (peliExistente) {
         return false;
       } else {
-        const data = this.data.push(peli);
-        const promesaDos = jsonfile.writeFile("./pelis.json", data);
+        this.data.push(peli);
+        const promesaDos = jsonfile.writeFile("./pelis.json", this.data);
 
         return promesaDos.then(() => {
           return true;
