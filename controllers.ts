e475dea944a5e6bb82385b5c317472a857001af8
matisystem <maticarabajal7@gmail.com>
2021-05-11 02:43:25 -0300

    desafio m2 terminado

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,34 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  pelis: PelisCollection;
+  promesa: Promise<any>;
+  constructor() {
+    const pelis = new PelisCollection();
+    this.pelis = pelis;
+    var promesa = this.pelis.getAll();
+    this.promesa = promesa;
+  }
+
+  get(options: any) {
+    var resultado;
+    if (options.id) {
+      resultado = this.pelis.getById(options.id);
+      return resultado;
+    }
+    if (options.search) {
+      resultado = this.pelis.search(options.search);
+      return resultado;
+    } else {
+      resultado = this.pelis.getAll();
+      return resultado;
+    }
+  }
+  add(peli: Peli) {
+    return this.pelis.add(peli).then((resultado) => {
+      return resultado;
+    });
+  }
 }
+
 export { PelisController };
--- index.ts
+++ index.ts
@@ -1,15 +1,60 @@
 import * as minimist from "minimist";
+import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
+  return resultado;
+}
 
+function paramsOptions(controller, params) {
+  var resultado;
+  if (params._[0] == "add") {
+    let peli = {
+      id: params.id,
+      title: params.title,
+      tags: params.tags,
+    };
+    return (resultado = controller.add(peli).then((resultado) => {
+      return resultado;
+    }));
+  }
+  if (params._[0] == "get") {
+    return (resultado = controller
+      .get({ id: params._[1] })
+      .then((resultado) => {
+        return resultado;
+      }));
+  }
+  if (params._[0] == "search") {
+    let options = {};
+    if (params.title) {
+      options["title"] = params.title;
+    }
+    if (params.tag) {
+      options["tag"] = params.tag;
+    }
+    return (resultado = controller
+      .get({ search: options })
+      .then((resultado) => {
+        return resultado;
+      }));
+  }
+  if (params._.length == 0) {
+    return (resultado = controller.get({}).then((resultado) => {
+      return resultado;
+    }));
+  }
   return resultado;
 }
 
 function main() {
   const params = parseaParams(process.argv.slice(2));
-
-  console.log(params);
+  const controller = new PelisController();
+  paramsOptions(controller, params).then((resultado) => {
+    {
+      console.log(resultado);
+    }
+  });
 }
 
 main();
