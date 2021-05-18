435bf7c69a445994a83be563e4864e9853d98ad9
EzequielVilla <ezequiel.n.villa@gmail.com>
2021-05-18 01:22:57 -0300

    Desafio modulo2

--- index.ts
+++ index.ts
@@ -1,15 +1,54 @@
 import * as minimist from "minimist";
-
+import {PelisController} from "./controllers";
 function parseaParams(argv) {
   const resultado = minimist(argv);
-
   return resultado;
 }
 
-function main() {
-  const params = parseaParams(process.argv.slice(2));
+function doForInput(controller, params){
 
-  console.log(params);
+  if(params._[0] == "get" && typeof params._[1] == "number"){
+    return controller.get({id:params._[1]}).then((result)=>{
+      return result;
+    });
+  }
+  else if(params._[0] == "search" && params.tag && !params.title){
+    return controller.get({search:{tag:params.tag}}).then((result)=>{
+      return result;
+    });
+  }
+  else if(params._[0] == "search" && params.title && !params.tag){
+    return controller.get({search:{title:params.title}}).then((result)=>{
+      return result;
+    });
+  }
+  else if(params._[0] == "search" && params.title && params.tag){
+    return controller.get({search:{title:params.title, tag:params.tag}}).then((result)=>{
+      return result;
+    });
+  }
+  else if(params._[0] == "add"){
+    return controller.add(params).then((result)=>{
+      return result;
+    });
+  }
+  else{
+    return controller.get(params).then((result)=>{
+      return result
+    });
+  }
 }
+
+function main() {
+  const controller = new PelisController();
+  controller.promise.then(()=>{
+    const params = parseaParams(process.argv.slice(2));
+    doForInput(controller,params).then((procesado) =>{
+      console.log("resultado:",procesado)
+    });
+  });
+}
+
+
 
 main();
