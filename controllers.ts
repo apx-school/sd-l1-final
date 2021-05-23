bb6c532153e30a77e6f93579862f9e7657864344
Gianni Furci <giannifu93@gmail.com>
2021-05-23 20:22:31 -0300

    Desafío terminado

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,24 @@
 import { PelisCollection, Peli } from "./models";
+import * as isEmpty from "lodash/isEmpty";
 
 class PelisController {
-  constructor() {}
+  pelis: PelisCollection;
+  constructor() {
+    this.pelis = new PelisCollection();
+  }
+  get(options) {
+    if (options.id) {
+      return this.pelis.getById(options.id);
+    }
+    if (options.search) {
+      return this.pelis.search(options.search);
+    }
+    if (isEmpty(options)) {
+      return this.pelis.getAll();
+    }
+  }
+  add(pelicula) {
+    return this.pelis.add(pelicula);
+  }
 }
 export { PelisController };
--- index.ts
+++ index.ts
@@ -1,14 +1,28 @@
 import * as minimist from "minimist";
+import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
-
   return resultado;
+}
+
+function getController(controller, params) {
+  if (params._ == "add") {
+    controller.add(params);
+  } else if (params._.includes("get")) {
+    controller.get({ id: params._[1] }).then((res) => console.log(res));
+  } else if (params._ == "search") {
+    delete params._;
+    controller.get({ search: params }).then((res) => console.log(res));
+  } else {
+    controller.get().then((res) => console.log(res));
+  }
 }
 
 function main() {
   const params = parseaParams(process.argv.slice(2));
-
+  const controller = new PelisController();
+  getController(controller, params);
   console.log(params);
 }
 
