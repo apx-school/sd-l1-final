8c142f9b74b73f1f158504a8af6ef1e7187cedc1
Matias Toledo <toledo.nicolas.matias@gmail.com>
2021-05-24 17:08:29 -0300

    fallo en los tests

--- models.ts
+++ models.ts
@@ -1,6 +1,5 @@
 import * as jsonfile from "jsonfile";
 
-// no modificar estas propiedades, agregar todas las que quieras
 class Peli {
   id: number;
   title: string;
@@ -8,11 +7,50 @@ class Peli {
 }
 
 class PelisCollection {
+  data: Peli[] = [];
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json").then((json) => {
+      return (this.data = json);
     });
+  }
+  getById(id: number): Promise<any> {
+    return this.getAll().then((data) => {
+      const resultado = data.find((i) => {
+        i.id == id;
+      });
+      return resultado;
+    });
+  }
+  search(options: any): Promise<any> {
+    return this.getAll().then((data) => {
+      return data.filter((i) => {
+        if (options.title) {
+          return i.title.includes(options.title);
+        } else if (options.tag) {
+          return i.tags.includes(options.tag);
+        } else if (options.title && options.tag) {
+          return (
+            i.title.includes(options.title) && i.tags.includes(options.tag)
+          );
+        }
+      });
+    });
+  }
+  add(peli: Peli): Promise<boolean> {
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+      if (peliExistente) {
+        return false;
+      } else {
+        this.data.push(peli);
+        const promesaDos = jsonfile.writeFile("./pelis.json", this.data);
+
+        return promesaDos.then(() => {
+          return true;
+        });
+      }
+    });
+
+    return promesaUno;
   }
 }
 export { PelisCollection, Peli };
