a46dc6e4e486fb9eb549885d9c6ae8e9534f23f8
valentinrxk <valeen.n.vr@gmail.com>
2021-05-24 17:44:23 -0300

    funca todo

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,26 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  pelis: PelisCollection
+
+  constructor() {
+    this.pelis = new PelisCollection
+    
+  }
+  get(options:any):Promise<any> {
+    
+    if(options.id){
+      return this.pelis.getById(options.id)
+    } 
+    if(options.search){
+      return this.pelis.search(options.search)
+    }else {
+      return this.pelis.getAll()
+    }
+    
+  }
+  add(peli:Peli){
+      return this.pelis.add(peli)
+    }
 }
 export { PelisController };
--- models.ts
+++ models.ts
@@ -1,5 +1,6 @@
 import * as jsonfile from "jsonfile";
 
+
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
   id: number;
@@ -8,11 +9,55 @@ class Peli {
 }
 
 class PelisCollection {
+  pelis: Peli[];
+
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+   
+    return jsonfile.readFile("./pelis.json").then((pelis) => {
+      return this.pelis = pelis;
     });
+  };
+  getById(id:number){
+    return this.getAll().then((pelis)=>{
+      const resultado = pelis.find((peli)=>{
+        return peli.id == id;
+        });
+      return resultado;
+    });
+  };
+  search(options:any){
+    return this.getAll().then((pelis)=>{
+      var respuesta = pelis;
+
+      if (options.title){
+        respuesta = respuesta.filter((peli)=>{
+          return peli.title.includes(options.title);
+        });
+      };
+      if (options.tag){
+        respuesta = respuesta.filter((peli)=>{
+          return peli.tags.includes(options.tag);
+        });
+      }; 
+      return respuesta;
+    });
+
   }
-}
-export { PelisCollection, Peli };
+  add(peli: Peli):Promise<boolean> {
+    const promesaUno = this.getById(peli.id).then((peliExistente)=>{
+      if (peliExistente){
+        return false;
+      }else{
+        this.pelis.push(peli);
+        const data = this.pelis;
+        const promesaDos = jsonfile.writeFile("./pelis.json", data);
+        return promesaDos.then(()=>{
+          return true;
+        });
+      }
+    });
+    return promesaUno;
+
+  }
+};
+export{Peli,PelisCollection}
