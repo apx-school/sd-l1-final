2ba9b02403600975a0c2d08df5129aa8c2018481
ramifurci <ramifurci1@gmail.com>
2021-05-23 21:59:15 -0300

    desafio terminado

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,24 @@
 import { PelisCollection, Peli } from "./models";
+import * as isEmpty from "lodash/isEmpty"
 
 class PelisController {
-  constructor() {}
+  peliculas: PelisCollection
+  constructor() {
+    this.peliculas = new PelisCollection()
+  }
+  get(options) {
+    if (options.id) {
+      return this.peliculas.getById(options.id);
+    }
+    if (options.search) {
+      return this.peliculas.search(options.search);
+    }
+    if (isEmpty(options)) {
+      return this.peliculas.getAll();
+    }
+  }
+  add(pelicula) {
+    return this.peliculas.add(pelicula);
+}
 }
 export { PelisController };
