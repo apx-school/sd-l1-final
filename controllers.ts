51e44d194b42bcd75656fa21214513f4da1fee75
JGiacusa <giacusajoaquin@gmail.com>
2021-05-26 22:41:17 -0300

    commit con error en funcion search y controller.ts

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,26 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+    pelisCollection: PelisCollection;
+    constructor() {
+        this.pelisCollection = new PelisCollection();
+    }
+
+    get(options: any) {
+        if (options.id) {
+            return this.pelisCollection.getById(options.id);
+        }
+
+        if (options.search) {
+            return this.pelisCollection.search(options.search);
+        } else {
+            return this.pelisCollection.getAll();
+        }
+    }
+
+    add(peli: Peli) {
+        return this.pelisCollection.add(peli);
+    }
 }
+
 export { PelisController };
--- models.ts
+++ models.ts
@@ -2,17 +2,62 @@ import * as jsonfile from "jsonfile";
 
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
-  id: number;
-  title: string;
-  tags: string[];
+    id: number;
+    title: string;
+    tags: string[];
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
-    });
-  }
+    peliculas: Peli[];
+
+    getAll(): Promise<Peli[]> {
+        const todasLasPelis = jsonfile
+            .readFile("./pelis.json")
+            .then((pelis) => {
+                return (this.peliculas = pelis);
+            });
+        return todasLasPelis;
+    }
+
+    getById(id: number) {
+        const buscarPorId = this.getAll().then((pelis) => {
+            return pelis.find((item) => {
+                return item.id == id;
+            });
+        });
+        return buscarPorId;
+    }
+    search(options: any) {
+        const search = this.getAll().then((pelis) => {
+            return pelis.filter((item) => {
+                if (options.title) {
+                    return item.title
+                        .toLocaleLowerCase()
+                        .includes(options.title);
+                }
+                if (options.tags) {
+                    return item.tags.includes(options.tags);
+                }
+            });
+        });
+        return search;
+    }
+
+    add(peli: Peli) {
+        const promesaUno = this.getById(peli.id).then((peliExistente) => {
+            if (peliExistente) {
+                return false;
+            } else {
+                this.peliculas.push(peli);
+                const data = this.peliculas;
+                const promesaDos = jsonfile.writeFile("./pelis.json", data);
+
+                return promesaDos.then(() => {
+                    return true;
+                });
+            }
+        });
+        return promesaUno;
+    }
 }
 export { PelisCollection, Peli };
