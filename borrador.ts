d112656c506d765ab2057cc04087a13afae3ed13
Martin <martoxxx100@gmail.com>
2021-05-31 13:21:13 -0300

    trabajando

---
+++ borrador.ts
@@ -0,0 +1,5 @@
+import * as jsonfile from "jsonfile"
+import * as lodash from "lodash"
+const pelis = jsonfile.readFileSync("./pelis.json")
+const listaFiltrada = lodash.filter(pelis,function(o){o.title.includes("a")});
+console.log(listaFiltrada)
--- models.ts
+++ models.ts
@@ -1,4 +1,6 @@
 import * as jsonfile from "jsonfile";
+import * as lodash from "lodash"
+import { result } from "lodash";
 
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
@@ -9,10 +11,43 @@ class Peli {
 
 class PelisCollection {
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
+    return jsonfile.readFile("./pelis.json").then((pelis) => {
       // la respuesta de la promesa
-      return [];
+      return pelis;
     });
   }
+  getById(id:number){
+    return this.getAll().then((pelis)=>{
+      const lista = pelis.find((peli)=>{
+        return peli.id == id;
+      })
+      return lista
+    })
+  }
+  search(option){
+
+if (option.search){
+   return this.getAll().then((pelis)=>{
+   return  pelis.filter((item)=>{
+     return item.title.includes(option.search)
+   })
+     })
+  }else if(option.tag){
+    return this.getAll().then((pelis)=>{
+      return pelis.filter((item)=>{
+     return item.tags.includes(option.tag)
+      })
+    })
+  }
+
+ }
 }
 export { PelisCollection, Peli };
+
+const prueba = new PelisCollection
+
+prueba.search({search:"u"}).then((item)=>{
+  console.log(item)
+})
+
+
