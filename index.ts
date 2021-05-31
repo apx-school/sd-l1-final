8c8a9afdf8bb73bbe6fe035674030f0d5e965181
Benjamin Henley <benjaminhenley@Benjamins-MacBook-Pro.local>
2021-05-31 18:59:18 -0300

    primer commit

--- index.ts
+++ index.ts
@@ -1,15 +1,45 @@
 import * as minimist from "minimist";
+import { PelisController } from "./controllers";
 
+function processOptions(controller, argumentos) {
+  if (argumentos.search) {
+    return controller.get(argumentos);
+  } else if (argumentos.add) {
+    return controller.add(argumentos.add);
+  } else if (argumentos.get) {
+    return controller.get(argumentos.get);
+  } else {
+    return controller.pelis;
+  }
+}
 function parseaParams(argv) {
-  const resultado = minimist(argv);
-
-  return resultado;
+  const resultadoMinimist = minimist(argv);
+  if (resultadoMinimist._[0] == "search") {
+    return {
+      search: { title: resultadoMinimist.title, tags: resultadoMinimist.tag },
+    };
+  } else if (resultadoMinimist._[0] == "add") {
+    return {
+      add: {
+        id: resultadoMinimist.id,
+        title: resultadoMinimist.title,
+        tags: resultadoMinimist.tags,
+      },
+    };
+  } else if (resultadoMinimist._[0] == "get") {
+    return { get: { id: resultadoMinimist._[1] } };
+  } else {
+    return {};
+  }
 }
 
 function main() {
-  const params = parseaParams(process.argv.slice(2));
+  const controlador = new PelisController();
+  const argumentosParseados = parseaParams(process.argv.slice(2));
 
-  console.log(params);
+  processOptions(controlador, argumentosParseados).then((i) => {
+    console.log(i);
+  });
 }
 
 main();
