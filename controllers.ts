a2103559d1a56e82ef217a839d2f41b64ab4c56f
Martiniano <mortvillalba@gmail.com>
2021-06-01 15:17:35 -0300

    Desafío del módulo 2 terminado

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,21 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  films: PelisCollection;
+  constructor() {
+    this.films = new PelisCollection();
+  }
+  get(options: any) {
+    if (options.id) {
+      return this.films.getById(options.id);
+    } else if (options.search) {
+      return this.films.search(options.search);
+    } else {
+      return this.films.getAll();
+    }
+  }
+  add(film: Peli) {
+    return this.films.add(film);
+  }
 }
 export { PelisController };
