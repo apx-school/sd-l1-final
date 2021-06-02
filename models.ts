2def6b8a7d333527a58d1d2aa0cb63ed795bf3bd
Junior Vitteri <vitteridev@gmail.com>
2021-06-02 14:26:42 -0300

    Agregando metodos asyncronos en models.ts

--- models.ts
+++ models.ts
@@ -1,4 +1,5 @@
 import * as jsonfile from "jsonfile";
+import * as find from "lodash/find";
 
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
@@ -8,11 +9,54 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
+  data: Peli[];
+  getAll() {
+    const pelis = jsonfile.readFile("./pelis.json").then((json) => {
       // la respuesta de la promesa
-      return [];
+      this.data = json;
+      return json;
+    });
+    return pelis;
+  }
+  getById(id: number) {
+    const pelisId = this.getAll().then((json) => {
+      return find(json, function (obj) {
+        return obj.id == id;
+      });
+    });
+    return pelisId;
+  }
+  search(options: any) {
+    return this.getAll().then((json) => {
+      if (options.title) {
+        return json.filter((obj) =>
+          obj.title.toLowerCase().includes(options.title)
+        );
+      }
+
+      if (options.tag) {
+        return json.filter((obj) => obj.tags.includes(options.tag));
+      }
     });
   }
+  add(peli: Peli): Promise<boolean> {
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+      if (peliExistente) {
+        return false;
+      } else {
+        //magia
+        this.data.push(peli);
+        const promesaDos = jsonfile.writeFile("./pelis.json", this.data);
+        return promesaDos.then(() => {
+          return true;
+        });
+      }
+    });
+    return promesaUno;
+  }
 }
-export { PelisCollection, Peli };
+/* const objeto = new PelisCollection();
+objeto.getById(3).then((obj) => {
+  console.log(obj);
+}); */
+export { Peli, PelisCollection };
