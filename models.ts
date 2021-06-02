fc520188ebd84ff73c9790f5057f451c617bf8aa
Lucas Pascual <pascual_lucas117@hotmail.com>
2021-06-02 19:40:45 -0300

    commit de prueba para revision de codigo

--- models.ts
+++ models.ts
@@ -9,10 +9,91 @@ class Peli {
 
 class PelisCollection {
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
+    return jsonfile.readFile("./pelis.json").then((json) => {
       // la respuesta de la promesa
-      return [];
+      return json;
     });
+  }
+  getById(id: number) {
+    return this.getAll().then((pelis) => {
+      const encontrada = pelis.find((peli) => {
+        return peli.id == id;
+      });
+      return encontrada;
+    });
+  }
+  search(option: any) {
+    if (option.title) {
+      return this.getAll().then((pelis) => {
+        const pelisPorLetra = pelis.filter(function (peli) {
+          return peli.title.toLowerCase().includes(option.title.toLowerCase());
+        });
+        return pelisPorLetra;
+      });
+    }
+
+    if (option.tag) {
+      return this.getAll().then((pelis) => {
+        const pelisPorTag = pelis.filter(function (peli) {
+          const buscadorTags = peli.tags.find(function (tags) {
+            return tags.toLocaleLowerCase() == option.tag.toLowerCase();
+          });
+          return buscadorTags;
+        });
+        return pelisPorTag;
+      });
+    }
+  }
+
+  add(peli: Peli): Promise<boolean> {
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+      if (peliExistente) {
+        return false;
+      } else {
+        // magia que agrega la pelicula a un objeto data
+
+        const data = this.getAll().then((data) => {
+          data.push(peli);
+          return data;
+        });
+
+        return data.then((todaLaData) => {
+          const promesaDos = jsonfile.writeFile("./pelis.json", todaLaData);
+
+          return promesaDos.then(() => {
+            return true;
+          });
+        });
+      }
+    });
+
+    return promesaUno;
   }
 }
 export { PelisCollection, Peli };
+
+// MOCK
+
+const probadorClase = new PelisCollection();
+
+// probadorClase.getAll().then((resultado) => {
+//   console.log(resultado);
+// });
+
+// probadorClase.getById(2).then((resultado) => {
+//   console.log(resultado);
+// });
+
+// probadorClase.search({ title: "d" }).then((resultado) => {
+//   console.log(resultado);
+// });
+
+// probadorClase.search({ tag: "clasica" }).then((resultado) => {
+//   console.log(resultado);
+// });
+
+// probadorClase
+//   .add({ id: 9, title: "nueva peli", tags: ["accion", "clasica"] })
+//   .then((resultado) => {
+//     console.log(resultado);
+//   });
