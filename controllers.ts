73338eadb65612b5bafc0910753d15417cc0c995
martin <romero7682@gmail.com>
2021-06-02 11:49:01 -0300

    Agregué la funcionalidad de que acepte tanto mayúsculas como minúsculas en las búsquedas con --search

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,27 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  peliculas:PelisCollection
+  promesa: Promise<any>
+  constructor() {
+    this.peliculas = new PelisCollection()
+    const promise = this.peliculas.getAll()
+    this.promesa = promise
+  };
+  get(options: any): Promise<any>{
+    if(options.id){
+      //console.log(options.id)
+      return this.peliculas.getById(options.id)
+    }
+    if(options.search){
+      return this.peliculas.search(options.search)
+    }
+    else{
+      return this.peliculas.getAll()
+    }
+  }
+  add(peli:Peli){
+    return this.peliculas.add(peli)
+  }
 }
-export { PelisController };
+export {PelisController};
--- models.ts
+++ models.ts
@@ -1,4 +1,6 @@
 import * as jsonfile from "jsonfile";
+import { OPENSSL_VERSION_NUMBER } from "node:constants";
+//import { PelisController } from "./controllers";
 
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
@@ -8,11 +10,57 @@ class Peli {
 }
 
 class PelisCollection {
+  peli:Peli[]=[]
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
+    return jsonfile.readFile("./pelis.json").then((jsonPelis) => {
       // la respuesta de la promesa
-      return [];
+      return this.peli =  jsonPelis;
     });
   }
+  getById(id:number):Promise<Peli>{
+    //console.log(id)
+    return this.getAll().then((peliculas)=>{
+      const respuesta = peliculas.find((item)=>{
+        return item.id == id
+    })
+    return respuesta
+    })   
+  }
+  search(options:any):Promise<any>{
+    return this.getAll().then((peliculas)=>{
+      let peliEcontrada =  peliculas
+      if(options.title){
+        peliEcontrada = peliEcontrada.filter((item)=>{
+          return item.title.toLowerCase().includes(options.title.toLowerCase())
+        })
+      }
+      if(options.tag){
+        peliEcontrada = peliEcontrada.filter((item)=>{
+          return item.tags.toString().toLocaleLowerCase().includes(options.tag.toString().toLowerCase())
+        })        
+      }
+      if(options.title && options.tag){
+        peliEcontrada = peliEcontrada.filter((item)=>{
+          return item.title.toLowerCase().includes(options.title.toLowerCase()) &&
+          item.tags.toString().toLocaleLowerCase().includes(options.tag.toString().toLowerCase())
+        })
+      }
+      return peliEcontrada
+    })
+  }
+  add(pelicula: Peli):Promise<boolean>{
+    const promesaUno = this.getById(pelicula.id)
+      .then((peliExistente)=>{
+        if(peliExistente){
+          return false
+        }else{
+          this.peli.push(pelicula)
+          const segundaPromesa = jsonfile.writeFile("./pelis.json", this.peli)
+          return segundaPromesa.then(()=>{
+            return true})
+        }
+      })
+    return promesaUno
+  };
 }
 export { PelisCollection, Peli };
