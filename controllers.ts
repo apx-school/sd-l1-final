835824bb6dd892075043b904b3e71d9e126f8f53
Giuliana Erro <giulierro07@hotmail.com>
2021-06-01 21:32:09 -0300

    desafio final 2 completado

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,25 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  peliColl: PelisCollection;
+
+  constructor() {
+    this.peliColl = new PelisCollection();
+  }
+
+  get(options: any) {
+    if (options.id) {
+      return this.peliColl.getById(options.id);
+    } else if (options.search) {
+      return this.peliColl.search(options.search);
+    } else {
+      return this.peliColl.getAll();
+    }
+  }
+
+  add(peli: Peli) {
+    return this.peliColl.add(peli);
+  }
 }
+
 export { PelisController };
