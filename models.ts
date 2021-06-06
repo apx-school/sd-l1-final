d4cb80b6063ff12bc772ae055fdf90deaeeef39e
GonzaloRythmic <gonzalocortez1991@hotmail.com>
2021-06-06 15:27:11 -0300

    primer commit

--- models.ts
+++ models.ts
@@ -8,11 +8,58 @@ class Peli {
 }
 
 class PelisCollection {
+  peliculas: Peli[];
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json").then((pelis) => {
+      // console.log(pelis);
+      return pelis;
     });
   }
+
+  getById(id: number) {
+    return this.getAll().then((peliculas) => {
+      return peliculas.find((i) => {
+        return i.id == id;
+      });
+    });
+  }
+
+  search(options: any) {
+    return this.getAll().then((lasPelis) => {
+      if (options.title && options.tag) {
+        return lasPelis.filter((i) => {
+          return (
+            i.title.includes(options.title) && i.tags.includes(options.tag)
+          );
+        });
+      }
+      if (options.title) {
+        return lasPelis.filter((i) => i.title.includes(options.title));
+      }
+      if (options.tag) {
+        return lasPelis.filter((i) => i.tags.includes(options.tag));
+      }
+    });
+  }
+
+  add(peli:Peli): Promise<boolean>{
+    const promesaUno = this.getById(peli.id).then((peliExistente)=>{
+     console.log(peli)
+     if(peliExistente){
+       return false;
+     }else{
+       this.peliculas.push(peli);
+       const data = this.peliculas;
+       const promesaDos = jsonfile.writeFile("./pelis.json", data);
+       return promesaDos.then(()=>{
+         return true;
+       });
+     }
+   });
+   return promesaUno;
+  }
 }
+
+
+
 export { PelisCollection, Peli };
