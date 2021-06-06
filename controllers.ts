ceb09a0c318b64b748ab7d0581a6873a72854918
fefe <fedescalada17@hotmail.com>
2021-06-06 20:32:42 -0300

    controller a revisar

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,27 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}  
+  data: Peli[] = [];
+  collectionPelis: PelisCollection;
+  constructor() {
+    this.collectionPelis = new PelisCollection();
+    this.collectionPelis.getAll();
+  }
+  get(options){
+    if (options.id){
+      return this.collectionPelis.getById(options.id);
+    }
+    if (options.search.title){
+      return this.collectionPelis.search(options.search.title);
+    }
+    if (options.search.tags){
+      return this.collectionPelis.search(options.search.tags);
+    } else{
+      return this.collectionPelis.getAll();
+    }
+  }
+  add(peli:Peli){
+    this.data.push(peli);
+  }
 }
 export { PelisController };
