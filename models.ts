b9ce1eecb4433066feadef296a95ee464edc3c4e
GonzaloRythmic <gonzalocortez1991@hotmail.com>
2021-06-06 21:26:45 -0300

    Tercer Commit

--- models.ts
+++ models.ts
@@ -2,18 +2,17 @@ import * as jsonfile from "jsonfile";
 
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
-  id: number;
   title: string;
+  id: number;
   tags: string[];
 }
 
 class PelisCollection {
   peliculas: Peli[];
   getAll(): Promise<Peli[]> {
-    return jsonfile.readFile("./pelis.json").then((pelis) => {
-      // console.log(pelis);
+      const todasLasPelis = jsonfile.readFile("./pelis.json").then((pelis) => {
       return pelis;
-    });
+    });return todasLasPelis;
   }
 
   getById(id: number) {
@@ -24,41 +23,66 @@ class PelisCollection {
     });
   }
 
-  search(options: any) {
-    return this.getAll().then((lasPelis) => {
-      if (options.title && options.tag) {
-        return lasPelis.filter((i) => {
-          return (
-            i.title.includes(options.title) && i.tags.includes(options.tag)
-          );
+  search(options: any): Promise<any> {
+      if (options.title && options.tag){
+        return this.getAll().then((listaPelis) => {
+        const peliFiltrada = listaPelis.filter((item)=>{
+          return item.title.includes(options.title) && item.tags.includes(options.tag);
+        }); return peliEncontrada;
+        })
+      }
+      else if (options.title) {
+        return this.getAll().then((listaPelis) => {
+          const peliTituloFiltrado = listaPelis.filter((item)=>{
+            return item.title.includes(options.title);
+          });return peliTituloFiltrado;
+          }) 
+      } else if (options.tags) {
+        return this.getAll().then((listaPelis) => {
+          const peliTagsFiltrado = listaPelis.filter((item)=>{
+            return item.tags.includes(options.tags);
+          });return peliTagsFiltrado;
+          }) 
+      }
+  
+  }
+     
+
+  add(peli: Peli): Promise<boolean> {
+    return this.getById(peli.id).then((idRepetido) => {
+      if (idRepetido) {
+        return false;
+      } else {
+        return this.getAll().then((lista) => {
+          lista.push(peli);
+          return jsonfile.writeFile("./pelis.json", lista).then(() => {
+            return true;
+          });
         });
-      }
-      if (options.title) {
-        return lasPelis.filter((i) => i.title.includes(options.title));
-      }
-      if (options.tag) {
-        return lasPelis.filter((i) => i.tags.includes(options.tag));
       }
     });
   }
-
-  add(peli:Peli): Promise<boolean>{
-    const promesaUno = this.getById(peli.id).then((peliExistente)=>{
-     console.log(peli)
-     if(peliExistente){
-       return false;
-     }else{
-       this.peliculas.push(peli);
-       const data = this.peliculas;
-       const promesaDos = jsonfile.writeFile("./pelis.json", data);
-       return promesaDos.then(()=>{
-         return true;
-       });
-     }
-   });
-   return promesaUno;
-  }
 }
+
+
+const peliNueva = new PelisCollection;
+// const peliEncontrada = peliNueva.getById(11).then((p)=> {
+//   console.log(p);
+//   return p;
+// })
+// console.log (peliEncontrada);
+
+// peliNueva.add({title: "ET",id: 13,
+// tags: ["Humor", "Niños", "Ciencia Ficción"]
+// });
+// const todasLasPelis = peliNueva.getAll().then((p)=>{
+//   console.table(p);
+//   return p;
+
+
+const peliEncontrada = peliNueva.search({tags:"accion" })
+console.table(peliEncontrada);
+
 
 
 
