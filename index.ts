9e4c9af3237d45e11fb42733a82adceb3f1c8bf6
gonzidel <gonzidel@gmail.com>
2021-06-07 20:02:11 -0300

    terminando

--- index.ts
+++ index.ts
@@ -1,15 +1,53 @@
 import * as minimist from "minimist";
+import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
-  const resultado = minimist(argv);
-
-  return resultado;
+  const result = minimist(argv);
+  const ne = new PelisController();
+  if(result._[0] == "search"){
+    if(result.title || result.tag){
+      var peli = {
+        search:{
+          title: result.title,
+          tag:result.tag,
+        },
+      };
+      return ne.get(peli).then((d)=>{
+        return d
+      });
+    }
+  }
+  if(result._[0] == "add"){
+    var peliAd = {
+      id: result.id,
+      title: result.title,
+      tags: result.tags
+    };
+    return ne.add(peliAd).then((d)=>{
+      return d;
+    });
+  }
+  if(result._[0] == "get"){
+    var peliGet ={
+      id: result._[1],
+    };
+    return ne.get(peliGet).then((d)=>{
+      return d;
+    });
+  }
+  if(result._[0] == null){
+    return ne.get(result).then((d)=>{
+      return d;
+    })
+  }
+  return result;
 }
 
 function main() {
   const params = parseaParams(process.argv.slice(2));
-
-  console.log(params);
+  params.then(()=>console.log(params)
+  )
+  
 }
 
 main();
--- models.ts
+++ models.ts
@@ -1,5 +1,6 @@
 import * as jsonfile from "jsonfile";
 
+
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
   id: number;
@@ -7,12 +8,64 @@ class Peli {
   tags: string[];
 }
 
+
 class PelisCollection {
+  data: Peli[] = [];
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json").then((dat) => {
+      this.data = dat
+      return dat
+      
     });
+    
+  }
+  getById(id: number) {
+    return this.getAll().then((pelis) => {
+      return pelis.find((pel)=>{
+        return pel.id == id
+      }); 
+    });
+     
+  }
+  search(options: any) {
+  const ser = this.getAll().then((d) => {
+    if(options.title && options.tag){
+      return d.filter((dd)=> dd.title.includes(options.title) && dd.tags.includes(options.tag));
+
+    } else if (options.title){
+      return d.filter((dd)=> dd.title.includes(options.title));
+    }if (options.tag) {
+      return d.filter((dd) => dd.tags.includes(options.tag)) 
+  }
+  });
+  return ser;
+  
+  }
+  add(peli: Peli): Promise<boolean> {
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+      if (peliExistente) {
+        return false;
+      } else {
+        this.data.push(peli);
+        const promesaDos = jsonfile.writeFile("./pelis.json", this.data);
+        return promesaDos.then(() => {
+          return true;
+        });
+      }
+    });
+    return promesaUno;
   }
 }
 export { PelisCollection, Peli };
+
+
+
+
+
+
+
+
+
+
+
+
