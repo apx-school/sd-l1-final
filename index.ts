b6016eccd075459580af9ca71127091001e497db
fefe <fedescalada17@hotmail.com>
2021-06-08 13:42:42 -0300

    todo parseado

--- index.ts
+++ index.ts
@@ -1,25 +1,40 @@
 import * as minimist from "minimist";
 import {PelisController} from "./controllers";
-import {PelisCollection} from "./models";
+import {Peli} from "./models";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
-  if (resultado.search){
-    return {search: resultado.search};
-  }else{
-    
-    console.log(resultado);
-    return {};
-  }
+  console.log(resultado);
+  if (resultado._[0] == "get"){
+    const objt = {get: {id: resultado._[1]}};
+    return objt;
+  }else if(resultado._[0] == "add"){
+    const peli = new Peli();
+    peli.id = resultado.id;
+    peli.title = resultado.title;
+    peli.tags = resultado.tags;
+
+    const objt = {add: peli};
+    return objt;    
+
+  } else if (resultado._[0] == "search"){
+    const objt = {
+      search: {
+        id: resultado.id,
+        title: resultado.title,
+        tags: resultado.tags,
+      }
+    }
+    return objt;
+  }else return {};
 }
 
 function main() {
-  const params = process.argv.slice(2);
-  const parsedArguments = parseaParams(params);
+  const params = parseaParams(process.argv.slice(2));
   const parsedController = new PelisController();
-  parsedController.get(parsedArguments).then((r)=>{
+  parsedController.get(params).then((r)=>{
 
-  //  console.log(r);  
+    console.log(r);  
   })
 
 
