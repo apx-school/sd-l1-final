a38058a7d5413e240b1ca566f1e2706278470aa9
geerflores <germannahuelflores@gmail.com>
2021-06-12 18:44:21 -0300

    no pasa el test controller

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,26 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  peliculas:PelisCollection;
+  constructor() {
+    this.peliculas = new PelisCollection();
+  }
+  get(options){
+    if(options.id){
+      return this.peliculas.getById(options.id)
+    }else if(options.search){
+      if(options.search){//listo
+        return this.peliculas.search(options.search);
+        //debe buscar las pelis que tengan ese string en el título. (ej: { search:{ title:"ju" } });(metodo llamado desde models)
+      }
+    }else{
+      return this.peliculas.getAll();
+    }
+    return
+  }
+  add(peli:Peli){
+    return this.peliculas.add(peli)
+    //recibe un objeto y crea una peli en base a él. (Ej.: { id:4421, title:"Una peli", tags:["classic","action"] })
+  }
 }
 export { PelisController };
--- models.ts
+++ models.ts
@@ -1,4 +1,6 @@
+import { Console } from "console";
 import * as jsonfile from "jsonfile";
+import { PelisController } from "./controllers";
 
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
@@ -8,11 +10,73 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  pelis:Peli[] = [];
+  getAll(): Promise<Peli[]>{//listo
+    return jsonfile.readFile("./pelis.json").then((lista) => {
+      this.pelis = lista;
+      return lista
     });
   }
+  getById(id:number){//listo
+    return this.getAll().then((lista)=>{
+      const encontrado = lista.find((item)=>{
+        return item.id == id;
+      })
+      return encontrado;
+    })
+  }
+  search(options:any){//listo
+    if(options.tag && options.title){
+      return this.getAll().then((lista)=>{
+        const resultado = lista.filter((item)=>{
+          return item.title.includes(options.title);
+        })
+        const listaFinal = resultado.filter((i)=>{
+          return i.tags.includes(options.tag)
+        })
+        return listaFinal;
+      })
+    }else if(options.title){
+      return this.getAll().then((lista)=>{//listo
+        const listaAMostrar = lista.filter((item)=>{
+          return item.title.includes(options.title)
+        });
+        return  listaAMostrar;
+        
+      });
+    }else if(options.tag){
+      return this.getAll().then((lista)=>{//listo
+        const tagAMostrar = lista.filter((item)=>{
+          return item.tags.includes(options.tag)
+        })
+        return tagAMostrar
+      })
+    }
+    
+  }
+  add(peli: Peli): Promise<boolean> {
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+      if (peliExistente) {
+        return false;
+      }else{
+        this.pelis.push(peli);
+        const lista = this.pelis;
+        const promesaDos = jsonfile.writeFile("./pelis.json", lista).then(()=>{
+          return true;
+        });
+        return promesaDos;
+        
+      }
+      
+    });
+
+    return promesaUno;
+  }
+
 }
 export { PelisCollection, Peli };
+/*const objeto = new PelisCollection();
+objeto.search({"tag":"accion"}).then((resultado)=>{
+  console.log(resultado)
+})*/
+
