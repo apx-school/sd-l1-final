20b9bf283b77db2156039851ff62e28173f0f99e
Maxi <maxijofre.c@gmail.com>
2021-06-17 22:45:06 -0300

    Desafío Final Módulo 2 Terminado

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,54 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  constructor() {
+    this.modelo = new PelisCollection();
+  }
+  modelo: PelisCollection;
+
+  get(options){
+    if(options.id){
+      return this.modelo.getById(options.id);
+    } else if(options.search){
+      return this.modelo.search(options.search);
+    } else{
+      return this.modelo.getAll();
+    }
+  }
+  add(peli:Peli){
+    return this.modelo.add(peli);
+  }
 }
+
 export { PelisController };
+
+// const prueba = new PelisController();
+
+//PRUEBA GET(ID)
+
+// prueba.get({"id": 5}).then((prueba) => {
+//   console.log(prueba)
+// })
+
+//PRUEBA GET(SEARCH)
+
+// prueba.get({"search": {"title" : "El"}}).then((prueba)=> {
+//   console.log(prueba)
+// })
+
+//PRUEBA GET(VACIO)
+
+// prueba.get({}).then((prueba)=> {
+//   console.log(prueba)
+// })
+
+//PRUEBA GET(ADD)
+
+// prueba.add({
+//   id: 6,
+//   title: 'El Hocker',
+//   tags: [ 'Drama', 'Acción', 'Fantasía' ]
+// }).then((add) => {
+//   console.log(add)
+// })
+
--- models.ts
+++ models.ts
@@ -1,5 +1,6 @@
 import * as jsonfile from "jsonfile";
 
+
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
   id: number;
@@ -8,11 +9,86 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
+  data : Peli[] = [];
+  getAll(): Promise<any[]> {
+    return jsonfile.readFile("./pelis.json").then((pelis) => {
       // la respuesta de la promesa
-      return [];
+      this.data = pelis;
+      return pelis;
     });
   }
+  getById(id: number){
+    return this.getAll().then((peliculas) => {
+      return peliculas.find(pelis => pelis.id == id);
+    });
+  }
+  search(options:any){
+    if(options.title && options.tag) {
+      return this.getAll().then((peliculas) => {
+        return peliculas.filter((item) => {
+          return item.title.includes(options.title) && item.tags.includes(options.tag);
+        });
+      });
+    } else if(options.title){
+      return this.getAll().then((peliculas) => {
+        return peliculas.filter((item) => {
+          return item.title.includes(options.title);
+        });
+      });
+    } else if(options.tag){
+        return this.getAll().then((peliculas) => {
+          return peliculas.filter((item) => {
+            return item.tags.includes(options.tag);
+          });
+        });
+    }
+  }
+  add(peli:Peli): Promise<boolean>{
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+      if(peliExistente){
+        return false;
+      } else {
+        //magia que agrega la peli a un obj data
+        this.data.push(peli);
+        const data = this.data;
+
+        const promesaDos = jsonfile.writeFile("./pelis.json", data);
+
+        return promesaDos.then(() => {
+          return true;
+        });
+      }
+    });
+    return promesaUno;
+  }
 }
+
 export { PelisCollection, Peli };
+
+// PRUEBAS
+
+// const peliculas = new PelisCollection();
+
+// peliculas.getAll().then((pelis) => {
+//   console.log(pelis);
+// });
+
+// peliculas.getById(5).then((producto) => {
+//   console.log(producto);
+// });
+
+// peliculas.search({"title": "El", "tag": "Acción"}).then((prod) => {
+//   console.log(prod);
+// });
+
+// peliculas.search({"tag": "Drama"}).then((prod) => {
+//   console.log(prod);
+// });
+
+// peliculas.add({
+//   "id": 6,
+//   "title": "El Hocker",
+//   "tags": ["Drama", "Acción", "Fantasía"]
+// }).then((peli) => {
+//   console.log(peli);
+// })
