035249bb9718ebbf7c3bc7427f87212e556cd9a2
Alvaroalvaro <64299010+Alvax10@users.noreply.github.com>
2021-06-18 22:09:05 -0300

    primer cambio en controller

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,33 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
-}
+  coleccion: PelisCollection;
+  constructor() {
+    this.coleccion = new PelisCollection;
+  }
+
+  get(options?: any): Promise<any> {
+    if (options.search) {
+      return this.coleccion.search(options.search).then((p)=>{
+        return p;
+      })
+    } else if (options._ == "id") {
+      return this.coleccion.getById(options.id).then((p)=>{
+        return p;
+      })
+    } else if (options.id) {
+      return this.coleccion.getById(options.id).then((p)=>{
+        return p;
+      })
+    } else 
+    return this.coleccion.getAll().then((p)=>{
+      return p;
+    }) 
+  }  
+
+  add(peli: Peli) {
+      this.coleccion.add(peli);
+    }
+  }
+
 export { PelisController };
