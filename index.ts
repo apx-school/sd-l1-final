196b63b83d529f722d12d0241329a3fafb74d0ec
Alvaroalvaro <64299010+Alvax10@users.noreply.github.com>
2021-06-19 09:35:20 -0300

    segundo commit del index

--- index.ts
+++ index.ts
@@ -1,33 +1,39 @@
-import { PelisCollection, Peli } from "./models";
+import * as minimist from "minimist";
+import { PelisController } from "./controllers";
 
-class PelisController {
-  coleccion: PelisCollection;
-  constructor() {
-    this.coleccion = new PelisCollection;
+function parseaParams(argv) {
+  const resultado = minimist(argv);
+  return resultado;
+}
+
+function ejecutador (params) {
+  const pelisController = new PelisController;
+
+  if (params._ == "get") {
+    pelisController.get({id: params._[1]}).then((res) => {
+      console.log(res)
+    });
+    
+    if (params._ == 'search') {
+      pelisController.get({ search: {title: params.title, tag: params.tag},
+    }).then((res)=> { 
+      console.log(res);
+    });
+  } 
+  else if ( params._ == "add") {
+    console.log ("Se agregará la siguiente película:", params.title);
+    pelisController.add(params); 
+    } else return pelisController.get(params).then((p)=>{
+      console.log(p);
+    })
   }
+}
 
-  get(options?: any): Promise<any> {
-    if (options.search) {
-      return this.coleccion.search(options.search).then((p)=>{
-        return p;
-      })
-    } else if (options._ == "id") {
-      return this.coleccion.getById(options.id).then((p)=>{
-        return p;
-      })
-    } else if (options.id) {
-      return this.coleccion.getById(options.id).then((p)=>{
-        return p;
-      })
-    } else 
-    return this.coleccion.getAll().then((p)=>{
-      return p;
-    }) 
-  }  
+function main() {
+  const params = parseaParams(process.argv.slice(2));
+  console.log ("los parametros llegan asi:", params);
+  ejecutador(params);
+}
 
-  add(peli: Peli) {
-      this.coleccion.add(peli);
-    }
-  }
+main();
 
-export { PelisController };
