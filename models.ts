4666289e3ce1dfd94017d5b1466e31e524f729dc
Alvaroalvaro <64299010+Alvax10@users.noreply.github.com>
2021-06-20 13:15:24 -0300

    segundo commit

--- models.ts
+++ models.ts
@@ -9,57 +9,59 @@ class Peli {
 
 class PelisCollection {
    peliculas: Peli[];
-  
+   
    getAll(): Promise<Peli[]> {
-    const todasLasPelis = jsonfile.readFile("./pelis.json").then((pelis) => {
-    this.peliculas = pelis
-    return pelis;
-  });
-  return todasLasPelis;
-}
-
-  getById(id:number): Promise<any> {
-    return this.getAll().then((peliculas) => {
-      return peliculas.find((peli) => {
-        return peli.id == id;
-      }) 
-    })
-  }
-
-  search(options:any){
-    if(options.title && options.tag) {
-      return this.getAll().then((peliculas) => {
-        return peliculas.filter((item) => {
-          return item.title.includes(options.title) && item.tags.includes(options.tag);
-        });
+     const todasLasPelis = jsonfile.readFile("./pelis.json").then((pelis) => {
+       this.peliculas = pelis
+       return pelis;
       });
-    } else if(options.title){
+      return todasLasPelis;
+    }
+    
+    getById(id:number): Promise<any> {
       return this.getAll().then((peliculas) => {
-        return peliculas.filter((item) => {
-          return item.title.includes(options.title);
+        const resultado = peliculas.find((peli) => {
+          return peli.id == id;
         });
+        return resultado;
       });
-    } else if(options.tag){
+    }
+    
+    search(options:any){
+      if(options.title && options.tag) {
+        return this.getAll().then((peliculas) => {
+          return peliculas.filter((item) => {
+            return item.title.includes(options.title) && item.tags.includes(options.tag);
+          });
+        });
+      } else if(options.title){
+        return this.getAll().then((peliculas) => {
+          return peliculas.filter((item) => {
+            return item.title.includes(options.title);
+          });
+        });
+      } else if(options.tag) {
         return this.getAll().then((peliculas) => {
           return peliculas.filter((item) => {
             return item.tags.includes(options.tag);
           });
         });
+      }
+    }
+    
+    add(peli: Peli): any{
+      return this.getById(peli.id).then((idRepetido) => {
+        if (idRepetido) {
+          return false;
+        } else {
+          return this.getAll().then((lista) => {
+            lista.push(peli);
+            console.log("Peli agregada!");
+            return jsonfile.writeFile("./pelis.json", lista).then(() => {})
+          });
+        } 
+      });
     }
   }
-
-  add(peli: Peli): any{
-    return this.getById(peli.id).then((idRepetido) => {
-      if (idRepetido) {
-        return false;
-      } else {
-        return this.getAll().then((lista) => {
-          lista.push(peli);
-          return jsonfile.writeFile("./pelis.json", lista).then(() => {});
-        });
-      }
-    });
-  }
-}
-
+  
 export { Peli, PelisCollection };
