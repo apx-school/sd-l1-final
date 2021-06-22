4fbf5c625962fbe8004ea986644357691ad82f99
Alvaroalvaro <64299010+Alvax10@users.noreply.github.com>
2021-06-22 12:18:12 -0300

    cuarto commit

--- index.ts
+++ index.ts
@@ -6,33 +6,35 @@ function parseaParams(argv) {
   return resultado;
 }
 
-function ejecutaArgumentos (params) {
-  const pelisController = new PelisController;
-  
-  if(params._[0] == 'get') {
-      pelisController.get({id: params._[1]}).then((res) => {
-        console.log(res);
-      });
-  }
+const pelisController = new PelisController();
 
-  if (params._[0] == 'search') {
-    pelisController.get({search: {title: params.title, tag: params.tag},
-    }).then((res)=> { 
+function ejecutador (params) {
+
+  if (params._ == 'search') {
+    pelisController.get({search:{title: params.title, tag: params.tag}})
+    .then((res)=>{console.log(res);
+    });
+
+  } else if (params._[0] == "add") {
+      console.log ("Se agregará la siguiente película:", params.title);
+      delete params["_"];
+      pelisController.add(params); 
+
+  } else if(params._[0] == "get") {
+      return pelisController.get({id: params._[1]}).then((res) => {
+      console.log(res);
+    });
+
+  } else {
+      return pelisController.coleccion.getAll().then((res) => {
       console.log(res);
     });
   }
-
-  if (params._[0] == 'add') {
-    console.log ("Se agregará la siguiente película:", params.title);
-    pelisController.add(params); 
-    } else return pelisController.get(params).then(()=>{
-    });
 }
 
 function main() {
-  const parametros = parseaParams(process.argv.slice(2));
-  ejecutaArgumentos(parametros);
-  console.log ("los parametros llegan asi:", parametros);
+  const params = parseaParams(process.argv.slice(2));
+  ejecutador(params);
 }
 
 main();
