034bd33888d761d4246f2cb3cd41a603c89abce2
unknown <zabit-009.ramiro_gomez243@outlook.es>
2021-06-24 14:57:23 -0300

    commit

--- index.ts
+++ index.ts
@@ -8,11 +8,11 @@ function parseaParams(argv) {
   if(resultado._ == "add") {
     return controller.add({id:resultado.id, title:resultado.title, tags:resultado.tags});
   } else if(resultado._[0] == "search" && resultado.title && resultado.tag) {
-    return {search: {title:resultado.title, tags:resultado.tag}};
+    return {search: {title:resultado.title, tag:resultado.tag}};
   } else if(resultado._[0] == "search" && resultado.title) {
     return {search: {title:resultado.title}};
   } else if(resultado._[0] == "search" && resultado.tag) {
-    return {search: {tags:resultado.tag}};
+    return {search: {tag:resultado.tag}};
   } else if(resultado._[0] == "get") {
     return {id:resultado._[1]};
   } else {
