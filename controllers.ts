75b2f253446c3e3af3a97ca3c0acb822e6fe39fc
Hernig23 <herniag.99@gmail.com>
2021-06-23 22:20:08 -0300

    ULTIMO COMMIT

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,23 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  coleccion: PelisCollection;
+  constructor() {
+    this.coleccion = new PelisCollection();
+  }
+
+  get(options?: any): Promise<any> {
+    if (options.id) {
+      return this.coleccion.getById(options.id);
+    } else if (options.search) {
+      return this.coleccion.search(options.search);
+    } else {
+      return this.coleccion.getAll();
+    }
+  }
+  add(peli: Peli) {
+    return this.coleccion.add(peli);
+  }
 }
+
 export { PelisController };
