212b4154d662abc4f7bb2059b6bfab229fbfc94f
unknown <zabit-009.ramiro_gomez243@outlook.es>
2021-06-24 12:47:53 -0300

    index terminado

--- index.ts
+++ index.ts
@@ -1,15 +1,32 @@
 import * as minimist from "minimist";
+import {PelisController} from "./controllers";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
+  const controller = new PelisController();
 
-  return resultado;
-}
+  if(resultado._ == "add") {
+    return controller.add({id:resultado.id, title:resultado.title, tags:resultado.tags});
+  } else if(resultado._[0] == "search" && resultado.title && resultado.tag) {
+    return {search: {title:resultado.title, tags:resultado.tag}};
+  } else if(resultado._[0] == "search" && resultado.title) {
+    return {search: {title:resultado.title}};
+  } else if(resultado._[0] == "search" && resultado.tag) {
+    return {search: {tags:resultado.tag}};
+  } else if(resultado._[0] == "get") {
+    return {id:resultado._[1]};
+  } else {
+    return controller.peliculas.getAll();
+  }
+};
 
 function main() {
-  const params = parseaParams(process.argv.slice(2));
-
-  console.log(params);
+const params = (process.argv.slice(2));
+const parseado = parseaParams(params);
+const pelisController = new PelisController();
+pelisController.get(parseado).then((resultado) => {
+  console.log(resultado);
+})
 }
 
 main();
