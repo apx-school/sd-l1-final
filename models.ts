ab51a8f17684f819aea672b13c26b4245744076c
unknown <zabit-009.ramiro_gomez243@outlook.es>
2021-06-24 12:24:39 -0300

    models terminado

--- models.ts
+++ models.ts
@@ -1,6 +1,6 @@
 import * as jsonfile from "jsonfile";
 
-// no modificar estas propiedades, agregar todas las que quieras
+
 class Peli {
   id: number;
   title: string;
@@ -8,11 +8,58 @@ class Peli {
 }
 
 class PelisCollection {
+data: Peli[] = [];
+
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json").then((peliculas) => {
+      this.data = peliculas;
+      return peliculas;
     });
+  };
+
+  getById(id:number):Promise<Peli> {
+    return this.getAll().then((peliculas) => {
+      const resultado = peliculas.find((item) => {
+        return item.id == id;
+      })
+      return resultado;
+    })
+  }
+
+  search(options:any) {
+    return this.getAll().then((peliculas) => {
+      if(options.title && options.tag) {
+        return peliculas.filter((item) => {
+          return item.title.includes(options.title) && item.tags.includes(options.tag);
+        })
+      } else if(options.title) {
+        return peliculas.filter((item) => {
+          return item.title.includes(options.includes);
+        })
+      } else if(options.tag) {
+        return peliculas.filter((item) => {
+          return item.tags.includes(options.tag);
+        })
+      }
+    })
+  }
+
+  add(peli:Peli):Promise<boolean> {
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+      if(peliExistente) {
+        return false;
+      } else {
+        this.data.push(peli);
+        const data = this.data;
+        const promesaDos = jsonfile.writeFile("./pelis.json", data);
+
+        return promesaDos.then(() => {
+          return true;
+        })
+      }
+    })
+    return promesaUno;
   }
 }
+
 export { PelisCollection, Peli };
