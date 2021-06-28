92d211b084ee96d0bc07523a723903267d6f7597
Misael <dmisael2000@gmail.com>
2021-06-28 15:47:12 -0300

    metodos de models

--- models.ts
+++ models.ts
@@ -9,10 +9,43 @@ class Peli {
 
 class PelisCollection {
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json").then((pelis) => {
+      return pelis;
+    });
+  }
+  getById(id: number) {
+    return this.getAll().then((pelis) => {
+      const result = pelis.find((peli) => {
+        return peli.id == id;
+      });
+      return result;
+    });
+  }
+  search(options: any) {
+    return this.getAll().then((peliculas) => {
+      if (options.title && options.tags) {
+        return peliculas.filter((objPelis) => {
+          const result =  objPelis.title.includes(options.title) && objPelis.tags.includes(options.tag)
+           return  result
+        });
+      } else if (options.title) {
+        return peliculas.filter((pelis) => {
+          return pelis.title.includes(options);
+          
+         
+        });
+      } else if (options.tags) {
+        return peliculas.filter((pelis) => {
+          return pelis.tags.includes(options.tags);
+        });
+      }
     });
   }
 }
+
+const peli = new PelisCollection();
+peli.search({title:"ce"}).then((resultado) => {
+  console.log(resultado);
+});
+
 export { PelisCollection, Peli };
