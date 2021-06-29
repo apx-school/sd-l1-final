663519d2fed88e5adff9fab03377e58dbebe1ab7
Matias Ferreira <ferre.matias@hotmail.com>
2021-06-29 00:02:40 -0300

    models hasta el search

--- models.ts
+++ models.ts
@@ -8,11 +8,53 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  getAll() {
+    return jsonfile.readFile("./pelis.json").then((pelis) => {
+      return pelis;
     });
+  }
+  getById(id:number){
+    const buscado = this.getAll().then((json)=>
+    json.find((peli)=>peli.id == id))
+    return buscado
+  }
+  search(options:any){
+    const filtrado = this.getAll().then((pelis=>{
+      if(options.tittle && options.tag){
+        let resultado = pelis
+        
+        pelis.forEach(peli=>{
+          if(peli.tags.includes(options.tag.toLowerCase())
+          )resultado.push(peli)
+        })
+
+        return resultado.filter((peli)=>{
+          let nombreEnMinus = peli.title.toLowerCase()
+          return nombreEnMinus.includes(options.title.toLowerCase())
+        })
+      }
+      if(options.title){
+        return pelis.filter((peli)=>{
+          let nombreEnMinus = peli.title.toLowerCase()
+          return nombreEnMinus.includes(options.title.toLowerCase())
+        })
+      }
+      if(options.tag){
+        let resultado =[]
+        pelis.forEach(peli=>{
+          if(peli.tags.includes(options.tag.toLowerCase())
+          )resultado.push(peli)
+        })
+        return resultado
+      }
+    }))
+    return filtrado
   }
 }
 export { PelisCollection, Peli };
+const pelis = new PelisCollection
+
+
+// pelis.getAll().then(pelis=>console.log(pelis))
+// pelis.getById(2).then(peli=>console.log(peli))
+// pelis.search({title:"true",tag:"drama"}).then(a=>console.log(a))
