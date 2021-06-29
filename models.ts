f094d24cfe60e85a9d44686a1029029f8238e688
Matias Ferreira <ferre.matias@hotmail.com>
2021-06-29 13:37:51 -0300

    models no pasa test con toLowerCase

--- models.ts
+++ models.ts
@@ -20,36 +20,29 @@ class PelisCollection {
     return buscado;
   }
   search(options: any) {
-    const filtrado = this.getAll().then((pelis) => {
+    return this.getAll().then((pelis) => {
       if (options.title && options.tag) {
-        let resultado = pelis;
-
+        let resultado = [];
         pelis.forEach((peli) => {
-          if (peli.tags.includes(options.tag.toLowerCase()))
+          if (peli.tags.includes(options.tag.toLowerCase())) {
             resultado.push(peli);
+          }
         });
-
         return resultado.filter((peli) => {
           let nombreEnMinus = peli.title.toLowerCase();
           return nombreEnMinus.includes(options.title.toLowerCase());
         });
-      }
-      if (options.title) {
+      } else if (options.title) {
         return pelis.filter((peli) => {
           let nombreEnMinus = peli.title.toLowerCase();
           return nombreEnMinus.includes(options.title.toLowerCase());
         });
-      }
-      if (options.tag) {
-        let resultado = [];
-        pelis.forEach((peli) => {
-          if (peli.tags.includes(options.tag.toLowerCase()))
-            resultado.push(peli);
+      } else if (options.tag) {
+        return pelis.filter((peli) => {
+          return peli.tags.includes(options.tag);
         });
-        return resultado;
       }
     });
-    return filtrado;
   }
   add(peli: Peli): Promise<boolean> {
     const promesaUno = this.getById(peli.id).then((peliExistente) => {
@@ -71,15 +64,13 @@ class PelisCollection {
   }
 }
 export { PelisCollection, Peli };
-const pelis = new PelisCollection();
+// const pelis = new PelisCollection();
 
 // pelis.getAll().then(pelis=>console.log(pelis))
 // pelis.getById(2).then(peli=>console.log(peli))
-// pelis.search({title:"true",tag:"drama"}).then(a=>console.log(a))
+// pelis.search({ title: "el" }).then((a) => console.log(a));
 
-const peli = new Peli();
-peli.id = 17;
-peli.title = "Para test";
-peli.tags = ["accion"];
-
-pelis.add(peli).then((res) => console.log(res));
+// const peli = new Peli();
+// peli.id = 17;
+// peli.title = "Para test";
+// peli.tags = ["accion"];
