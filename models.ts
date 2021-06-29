140272fb80bf6c30bb400306499133fe8f949ace
Matias Ferreira <ferre.matias@hotmail.com>
2021-06-29 10:55:39 -0300

    models 'completado'

--- models.ts
+++ models.ts
@@ -13,73 +13,73 @@ class PelisCollection {
       return pelis;
     });
   }
-  getById(id:number){
-    const buscado = this.getAll().then((json)=>
-    json.find((peli)=>peli.id == id))
-    return buscado
+  getById(id: number) {
+    const buscado = this.getAll().then((json) =>
+      json.find((peli) => peli.id == id)
+    );
+    return buscado;
   }
-  search(options:any){
-    const filtrado = this.getAll().then((pelis=>{
-      if(options.tittle && options.tag){
-        let resultado = pelis
-        
-        pelis.forEach(peli=>{
-          if(peli.tags.includes(options.tag.toLowerCase())
-          )resultado.push(peli)
-        })
+  search(options: any) {
+    const filtrado = this.getAll().then((pelis) => {
+      if (options.tittle && options.tag) {
+        let resultado = pelis;
 
-        return resultado.filter((peli)=>{
-          let nombreEnMinus = peli.title.toLowerCase()
-          return nombreEnMinus.includes(options.title.toLowerCase())
-        })
+        pelis.forEach((peli) => {
+          if (peli.tags.includes(options.tag.toLowerCase()))
+            resultado.push(peli);
+        });
+
+        return resultado.filter((peli) => {
+          let nombreEnMinus = peli.title.toLowerCase();
+          return nombreEnMinus.includes(options.title.toLowerCase());
+        });
       }
-      if(options.title){
-        return pelis.filter((peli)=>{
-          let nombreEnMinus = peli.title.toLowerCase()
-          return nombreEnMinus.includes(options.title.toLowerCase())
-        })
+      if (options.title) {
+        return pelis.filter((peli) => {
+          let nombreEnMinus = peli.title.toLowerCase();
+          return nombreEnMinus.includes(options.title.toLowerCase());
+        });
       }
-      if(options.tag){
-        let resultado =[]
-        pelis.forEach(peli=>{
-          if(peli.tags.includes(options.tag.toLowerCase())
-          )resultado.push(peli)
-        })
-        return resultado
+      if (options.tag) {
+        let resultado = [];
+        pelis.forEach((peli) => {
+          if (peli.tags.includes(options.tag.toLowerCase()))
+            resultado.push(peli);
+        });
+        return resultado;
       }
-    }))
-    return filtrado
+    });
+    return filtrado;
   }
   add(peli: Peli): Promise<boolean> {
     const promesaUno = this.getById(peli.id).then((peliExistente) => {
       if (peliExistente) {
         return false;
-      } 
-      // else {
-      //   // magia que agrega la pelicula a un objeto data
-      //   const data = {...};
-      //   const promesaDos = jsonfile.writeFile("./pelis.json", data);
+      } else {
+        const promesaDos = this.getAll().then((todas) => {
+          todas.push(peli);
+          return jsonfile.writeFile("./pelis.json", todas);
+        });
 
-      //   return promesaDos.then(() => {
-      //     return true;
-      //   });
-      // }
+        return promesaDos.then(() => {
+          return true;
+        });
+      }
     });
 
     return promesaUno;
   }
 }
 export { PelisCollection, Peli };
-const pelis = new PelisCollection
-
+const pelis = new PelisCollection();
 
 // pelis.getAll().then(pelis=>console.log(pelis))
 // pelis.getById(2).then(peli=>console.log(peli))
 // pelis.search({title:"true",tag:"drama"}).then(a=>console.log(a))
 
-const peli = new Peli
-peli.id = 14
-peli.title = "Para test"
-peli.tags = ["accion"]
+const peli = new Peli();
+peli.id = 17;
+peli.title = "Para test";
+peli.tags = ["accion"];
 
-pelis.add(peli).then(res=>console.log(res))
+pelis.add(peli).then((res) => console.log(res));
