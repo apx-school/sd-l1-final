fcd474b5b57329ccc825651072f647605015295e
matias.ferreira <ferre.matias@hotmail.com>
2021-06-30 15:34:16 -0300

    index ok, falta 1 test

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
 import * as minimist from "minimist";
 import { PelisController } from "./controllers";
 
+function ejecutarComandos(params){
+  const controller = new PelisController
+  if(params.search || params.id){return controller.get(params).then(respuesta=>console.log(respuesta))}
+  else if(params.add){return controller.add(params).then(respuesta=>console.log(respuesta))}
+  else if (params == 0){return controller.get({}).then(respuesta=>console.log(respuesta))}
+}
+
+
 function parseaParams(argv) {
   const parseo = minimist(argv);
   if (parseo._[0] == "get") {
@@ -13,14 +21,12 @@ function parseaParams(argv) {
     return { search: { tag: parseo.tag } };
   } else if (parseo._[0] == "add") {
     return { title: parseo.title, id: parseo.id, tags: parseo.tags };
-  }
+  }else{return 0}
 }
 
 function main() {
-  const controller = new PelisController();
   const params = parseaParams(process.argv.slice(2));
-  const resultado = controller.get(params);
-  resultado.then((resultado) => console.log(resultado));
+  ejecutarComandos(params)
 }
 
 main();
