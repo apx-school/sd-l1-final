2ab5f233cfbe1563a2443f4e28946077bfa0632c
pampero-iot <pampero.iot@gmail.com>
2021-07-03 16:21:20 -0300

    completo desafio modulo 2

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,21 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  pelis: PelisCollection;
+  constructor() {
+    this.pelis = new PelisCollection();
+  }
+  get(options: { id?: any; search?: any }): Promise<any> {
+    if (options.id) {
+      return this.pelis.getById(options.id);
+    } else if (options.search) {
+      return this.pelis.search(options.search);
+    } else {
+      return this.pelis.getAll();
+    }
+  }
+  add(peli: Peli): Promise<boolean> {
+    return this.pelis.add(peli);
+  }
 }
 export { PelisController };
--- index.ts
+++ index.ts
@@ -1,15 +1,59 @@
 import * as minimist from "minimist";
+import { PelisController } from "./controllers";
+import { Peli, SearchOptions } from "./models";
 
-function parseaParams(argv) {
+class Params {
+  action: "add" | "get" | "search";
+  options?: { id?: number; search?: SearchOptions };
+  peli?: Peli;
+}
+
+function parseaParams(argv: string[]): Params {
   const resultado = minimist(argv);
-
-  return resultado;
+  const action = resultado._;
+  if (action.includes("add")) {
+    return {
+      action: "add",
+      peli: {
+        id: resultado.id,
+        title: resultado.title,
+        tags: resultado.tags,
+      },
+    };
+  } else if (action.includes("get")) {
+    return {
+      action: "get",
+      options: {
+        id: action[1],
+      },
+    };
+  } else if (action.includes("search")) {
+    return {
+      action: "search",
+      options: {
+        search: {
+          title: resultado.title,
+          tag: resultado.tag,
+        },
+      },
+    };
+  } else {
+    return { action: "get", options: {} };
+  }
 }
 
 function main() {
+  const controller = new PelisController();
   const params = parseaParams(process.argv.slice(2));
-
-  console.log(params);
+  if (params.action === "add") {
+    controller.add(params.peli).then((response) => {
+      console.log(response);
+    });
+  } else {
+    controller.get(params.options).then((response) => {
+      console.log(response);
+    });
+  }
 }
 
 main();
