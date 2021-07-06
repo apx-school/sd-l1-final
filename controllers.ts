20f6a3b98eba26cdc88ddb4dc16a0bdd00b9a07f
alayn saez <saezalayn@gmail.com>
2021-07-06 19:27:22 -0300

    controllers.ts fixed

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,25 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  pelisColl: PelisCollection;
+
+  constructor() {
+    this.pelisColl = new PelisCollection();
+  }
+
+  get(options: any): Promise<any> {
+    if (options.id) {
+      return this.pelisColl.getById(options.id);
+    } else if (options.search) {
+      return this.pelisColl.search(options.search);
+    } else {
+      return this.pelisColl.getAll();
+    }
+  }
+
+  add(peli: Peli) {
+    return this.pelisColl.add(peli);
+  }
 }
+
 export { PelisController };
--- index.ts
+++ index.ts
@@ -1,15 +1,40 @@
 import * as minimist from "minimist";
+import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
+  const controller = new PelisController();
 
-  return resultado;
+  // cada una de las opciones de lo que se ingresa por la terminal, ya parseado con minismist, para derivarlo correspondientemente a get() en la funcion main.
+  if (resultado._ === "add") {
+    return controller.add({
+      id: resultado.id,
+      title: resultado.title,
+      tags: resultado.tags,
+    });
+  }
+
+  if (resultado._[0] === "get") {
+    return { id: resultado._[1] };
+  } else if (resultado._[0] === "search" && resultado.title) {
+    return { search: { title: resultado.title } };
+  } else if (resultado._[0] === "search" && resultado.tag) {
+    return { search: { tag: resultado.tag } };
+  } else if (resultado._[0] === "search" && resultado.title && resultado.tag) {
+    return { search: { title: resultado.title, tag: resultado.tag } };
+  } else {
+    return controller.pelisColl.getAll();
+  }
 }
 
 function main() {
+  const controller = new PelisController();
+
   const params = parseaParams(process.argv.slice(2));
 
-  console.log(params);
+  controller.get(params).then((result) => {
+    return console.log(result);
+  });
 }
 
 main();
