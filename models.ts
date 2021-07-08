51c7b8553f696d8566b0f6bd5cf3fd7c7b279f88
lautaro427 <85697105+lautaro427@users.noreply.github.com>
2021-07-08 14:05:23 -0300

    Update models.ts

--- models.ts
+++ models.ts
@@ -8,11 +8,52 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
+  getAll() {
+    return jsonfile.readFile("./pelis.json").then((pelis) => {
       // la respuesta de la promesa
-      return [];
+      return pelis;
     });
   }
+  getById(id:number){
+    const peliBuscada = this.getAll().then((pelis) =>
+      pelis.find((item) => item.id == id)
+    );
+    return peliBuscada;
+  }
+  search(options:any){
+    return this.getAll().then((pelis) =>{
+      if (options.title && options.tag){
+        return pelis.filter((item)=> {
+          return item.title.includes(options.title) && item.tags.includes(options.tag);
+        });
+      }else if(options.title){
+        return pelis.filter((item) =>{
+          return item.title.includes(options.title);
+        });
+      }else if(options.tag){
+        return pelis.filter((item) =>{
+          return item.tags.includes(options.tag);
+        });
+      }
+    });
+  }
+  
+  add(peli:Peli): Promise <boolean> {
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+      if(peliExistente){
+        return false;
+      }else{
+        const promesaDos = this.getAll().then((data) => {
+          data.push(peli);
+          return jsonfile.writeFile("./pelis.json", data);
+        });
+        return promesaDos.then(() => {
+          return true;
+        });
+      }
+    });
+    return promesaUno;
+  }
 }
+
 export { PelisCollection, Peli };
