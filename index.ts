bf0d433f1150243b890357d3315ca92db8e5dd94
lautaro427 <85697105+lautaro427@users.noreply.github.com>
2021-07-09 12:35:34 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,15 +1,32 @@
 import * as minimist from "minimist";
+import {PelisController} from "./controllers";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
-
   return resultado;
+}
+
+function options (params){
+  const controller = new PelisController();
+  if(params._[0] == "add"){
+    delete params._;
+    return controller.add(params);
+  } else if(params._[0] == "search"){
+    delete params._;
+    return (controller.get({"search" : params }));
+  } else if(params._[0] == "get"){
+    return controller.get({"id": params._[1]});
+  } else {
+    return controller.get({});
+  }
 }
 
 function main() {
   const params = parseaParams(process.argv.slice(2));
-
-  console.log(params);
+  const optionPelis = options(params)
+  optionPelis.then((result) => {
+  console.log(result);
+  });
 }
 
 main();
