861429bb90af82a86e1a511788d8084364bf9f9f
Camila Vazquez <camilavazquezvazquez2014@outlook.com>
2021-07-13 17:25:59 -0300

    nueva modificacion

--- index.ts
+++ index.ts
@@ -1,26 +1,6 @@
 import * as minimist from "minimist";
 import {PelisController} from "./controllers";
-import {PelisCollection} from "./models";
-import {argv} from "node:process";
 
-
-function ejecutarComandos(params){
-  const controllerPelis = new PelisController(); 
-  var resultado;
-  if(params.add){
-  return resultado = controllerPelis.add(params);
-  console.log(resultado); 
-  } else if(params.title && params.id && params.tags){
-  return resultado = controllerPelis.add(params);
-    console.log(resultado));
-  } else if(params.search || params.id){                             
-      return resultado = controllerPelis.get(params);
-      console.log(resultado));  
-  } else if(params == 0){
-    return resultado = controllerPelis.get({});
-      console.log(resultado));
-  }
-  }
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
@@ -38,6 +18,25 @@ function parseaParams(argv) {
     return 0;
   }
 }
+
+
+function ejecutarComandos(params){
+const controllerPelis = new PelisController;
+var resultado;
+if(params.add){
+  return resultado = controllerPelis.add(params);
+}else if(params.title && params.id && params.tags){
+  return resultado = controllerPelis.add(params);
+}else if(params.search || params.id){
+  return resultado = controllerPelis.get(params);
+}else if(params == 0){
+  return resultado = controllerPelis.get({});
+}
+return resultado;
+}
+
+
+
 
 function main() {
   const params = parseaParams(process.argv.slice(2));
