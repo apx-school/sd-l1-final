3be0becd67c9fe051ba1ae2a8b0f9932171f5aa7
Camila Vazquez <camilavazquezvazquez2014@outlook.com>
2021-07-15 19:22:07 -0300

    modifiqué algunas cosas

--- controllers.ts
+++ controllers.ts
@@ -7,23 +7,24 @@ class PelisController {
   }
 get(options:any){
     var resultado;
+    console.log(this.peliculas);
 console.log(options);
  if(options.search){
   return resultado = this.peliculas.search(options.search);
 } else if(options.search){
-  return resultado = this.peliculas.search(options.search);
+  return resultado = this.peliculas.search(options.search.title);
 } else if(options.search) {
-  return resultado = this.peliculas.search(options.search.tag);
-} else {    
-  return this.peliculas.getAll();
+  return resultado = (this.peliculas.search(options.search.tag));
+} else if(options.get) {
+  return resultado = this.peliculas.getById(options.get); 
+  } else {
+    return this.peliculas.getAll();
   }
-
 }
 add(peli:Peli){
     return this.peliculas.add(peli);
   }
 }
-  
 export { PelisController };
  
 const control = new PelisController();
