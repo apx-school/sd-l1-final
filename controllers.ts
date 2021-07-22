5f36d9bedf006633beb10d0c2a40e02b30ae8864
matias <matias3282@hotmail.com>
2021-07-22 20:16:04 -0300

    prueba primer commit

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,36 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  peliculas: PelisCollection;
+  constructor() {
+    this.peliculas = new PelisCollection();
+  }
+  get(options: any): Promise<any> {
+    if (options.id) {
+      return this.peliculas.getById(options.id).then((pelicula) => {
+        return pelicula;
+      });
+    } else if (options.search) {
+      const peliBuscada = this.peliculas
+        .search(options.search)
+        .then((pelicula) => {
+          return pelicula;
+        });
+      return peliBuscada;
+    } else if (options.tag) {
+      const peliEncontrada = this.peliculas
+        .search(options.tag)
+        .then((pelicula) => {
+          return pelicula;
+        });
+      return peliEncontrada;
+    } else {
+      return this.peliculas.getAll();
+    }
+  }
+  add(peli: Peli) {
+    return this.peliculas.add(peli);
+  }
 }
+
 export { PelisController };
--- index.ts
+++ index.ts
@@ -1,15 +1,50 @@
 import * as minimist from "minimist";
+import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
+  if (resultado[0] == "get") {
+    return { id: resultado._[0] };
+  } else if (resultado[0] == "search" && resultado.title && resultado.tag) {
+    return { search: { title: resultado.title, tag: resultado.tag } };
+  } else if (resultado[0] == "search" && resultado.title) {
+    return { search: { title: resultado.title } };
+  } else if (resultado[0] == "search" && resultado.tag) {
+    return { search: { title: resultado.tag } };
+  } else if (resultado[0] == "add") {
+    return { title: resultado.title, id: resultado.id, tags: resultado.tag };
+  } else {
+    return 0;
+  }
+}
 
-  return resultado;
+function ejcutarComandos(params) {
+  const pelisController = new PelisController();
+  if (params.add) {
+    return pelisController.add(params).then((resultado) => {
+      console.log(resultado);
+    });
+  } else if (params.title && params.id && params.tags) {
+    return pelisController.add(params).then((resultado) => {
+      console.log(resultado);
+    });
+  } else if (params.search || params.id) {
+    return pelisController.get(params).then((resultado) => {
+      console.log(resultado);
+    });
+  } else if (params == 0) {
+    return pelisController.get({}).then((resultado) => {
+      console.log(resultado);
+    });
+  }
 }
 
 function main() {
   const params = parseaParams(process.argv.slice(2));
-
-  console.log(params);
+  const optionPelis = ejcutarComandos(params);
+  optionPelis.then((resultado) => {
+    console.log(resultado);
+  });
 }
 
 main();
