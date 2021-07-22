3dce56112e6b2c34a944bf456e5a1b66c5fc213a
Maxi <maxiorieta.-@hotmail.com>
2021-07-22 11:20:29 -0300

    models.ts: getById me devuelve undefined

--- models.ts
+++ models.ts
@@ -9,10 +9,28 @@ class Peli {
 
 class PelisCollection {
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
+    return jsonfile.readFile("./pelis.json").then((pelis) => {
       // la respuesta de la promesa
-      return [];
+      return [pelis];
     });
+  };
+  getById(id:number){
+    return this.getAll().then((peli)=>{
+      const resultado = peli.find((movie)=>{
+        return movie.id == id
+      })
+      return resultado
+    })
   }
 }
 export { PelisCollection, Peli };
+
+
+//instancias de prueba
+const peliDePrueba = new PelisCollection;
+// peliDePrueba.getAll().then((peli)=>{
+//   console.log(peli)
+// });
+peliDePrueba.getById(1).then((peli)=>{
+  console.log(peli)
+})
