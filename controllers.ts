0ee20ccee15c72ebc004bd5e5eb5ef0f4a721a0e
Gonzalo Gregorutti <gonza.gregorutti@hotmail.com>
2021-07-25 13:46:12 -0300

    desafio 2 finalizado

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,28 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  peliculas: PelisCollection
+  constructor() {
+    this.peliculas = new PelisCollection()
+  }
+  get(options:any) {
+    if(options.id){
+      return this.peliculas.getById(options.id).then((pelis) => {
+        return pelis
+      }) 
+    } else if(options.search) {
+      return this.peliculas.search(options.search)
+    } else if (options.search){
+      return this.peliculas.search(options.search.tags)
+    } else {
+      return this.peliculas.getAll()
+    }
+  }
+  add(peli:Peli){
+   return this.peliculas.add(peli)
+  }
 }
+
+
+
 export { PelisController };
