06d21e9ff562e20c832b32adf77975b35500b90c
Florencia <maluf.florencia@gmail.com>
2021-07-25 21:03:37 -0300

    primer commit-controllers y models

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,29 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
-}
+  peliculas: PelisCollection
+  constructor() {
+    this.peliculas = new PelisCollection()
+  }
+
+  get(options){
+    if (options.id){
+      return this.peliculas.getById(options.id)
+    } else if (options.search){
+      return this.peliculas.search(options.search.title)
+    } else if (options.search){
+      return this.peliculas.search(options.search.tags)
+    } else if (options.search){
+      return this.peliculas.search(options.search.title) && this.peliculas.search(options.search.tags)
+    } else {
+      return this.peliculas.getAll()
+    };
+
+  };
+
+  add(peli:Peli){
+    return this.peliculas.add(peli)
+  }
+};
+
 export { PelisController };
--- models.ts
+++ models.ts
@@ -1,6 +1,5 @@
 import * as jsonfile from "jsonfile";
 
-// no modificar estas propiedades, agregar todas las que quieras
 class Peli {
   id: number;
   title: string;
@@ -9,10 +8,58 @@ class Peli {
 
 class PelisCollection {
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json").then((peliculas) => {
+      return peliculas;
     });
-  }
-}
+  };
+
+  getById(id:number){
+    return this.getAll().then((peliculas) => {
+    const movies = peliculas.find((p) => {
+      return p.id == id
+    });
+    return movies
+  })};
+
+  search (options: any){
+    return this.getAll().then((peliculas) =>{
+      if (options.title && options.tags){
+        return peliculas.filter((p) =>{
+          p.title.includes(options.title)
+          p.tags.includes(options.tags)
+
+        });
+      } 
+      else if (options.title){
+        return peliculas.filter((p) => {
+          return p.title.includes(options.title)
+        });
+      }
+      else if (options.tags){
+        return peliculas.filter((p) => {
+          return p.tags.includes(options.tags)
+        });
+      };
+    });
+  };
+
+  add(peli: Peli) : Promise<boolean>{
+    const promesaUno = this.getById(peli.id).then((exist) =>{
+      if (exist){
+        return false
+      } else {
+           const promesaDos = this.getAll().then((peliculas) =>{
+           peliculas.push(peli)
+           return jsonfile.writeFile("./pelis.json", peliculas);
+         });
+         return promesaDos.then(() =>{
+           return true
+         });
+      };
+    });
+
+    return promesaUno
+  };
+};
+
 export { PelisCollection, Peli };
