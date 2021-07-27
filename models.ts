fe5c8de0c946b86c9af7a0cf680ea1bf88924d8d
Facundo Aguilar <facundoaguilar96@hotmail.com>
2021-07-27 15:07:04 -0300

    Funcionan casi todas las funciones de models

--- models.ts
+++ models.ts
@@ -8,86 +8,96 @@ class Peli {
 }
 
 class PelisCollection {
-  arrayPelis:Peli[];
+  arrayPelis: Peli[];
   getAll(): Promise<Peli[]> {
-    const todasLasPelis = jsonfile.readFile("./pelis.json").then((resultado) => {
-      return this.arrayPelis = resultado;
-    });
+    const todasLasPelis = jsonfile
+      .readFile("./pelis.json")
+      .then((resultado) => {
+        return (this.arrayPelis = resultado);
+      });
     return todasLasPelis;
   }
-  getById(id:number){
-    return this.getAll().then((peliId)=>{
-     const peliculaEncontrada = peliId.find((peliculaId)=>{
+  getById(id: number) {
+    return this.getAll().then((peliId) => {
+      const peliculaEncontrada = peliId.find((peliculaId) => {
         return peliculaId.id == id;
-      })
+      });
       return peliculaEncontrada;
-    })
+    });
   }
-  search(options:any){
-    
-    if (options.search && options.tag){
-      this.getAll().then((peliTagYSerch)=>{
-        const parametrosEncontrados = peliTagYSerch.find((peliEncontrada)=>{
-          return peliEncontrada.title.includes(options.search) && peliEncontrada.tags.includes(options.tag);
-        })
-        
-      return parametrosEncontrados;
-      })
-    }else if (options.search){
-      this.getAll().then((peliSerch)=>{
-        const parametroEncontrado = peliSerch.find((peliEncontrada)=>{
-          console.log("estoy entrando aca")
-          return peliEncontrada.title.includes(options.search);})
-      return parametroEncontrado;
-    })
-    }else if(options.tag){
-      this.getAll().then((peliSerch)=>{
-        const parametroTagEncontrado = peliSerch.find((peliEncontrada)=>{
-          return peliEncontrada.tags.includes(options.tag);})
-      return parametroTagEncontrado;
-     })}
+  search(options: any) {
+    if (options.search && options.tag) {
+      console.log("ahora estoy entrando aca");
+      return this.getAll().then((peliTagYSerch) => {
+        const parametrosEncontrados = peliTagYSerch.filter((peliEncontrada) => {
+          return (
+            peliEncontrada.title.includes(options.search) &&
+            peliEncontrada.tags.includes(options.tag)
+          );
+        });
 
+        return parametrosEncontrados;
+      });
+    } else if (options.search) {
+      return this.getAll().then((peliSerch) => {
+        const parametroEncontrado = peliSerch.filter((peliEncontrada) => {
+          console.log("estoy entrando aca");
+          return peliEncontrada.title.includes(options.search);
+        });
+        return parametroEncontrado;
+      });
+    } else if (options.tag) {
+      return this.getAll().then((peliTag) => {
+        console.log("ultimo aca");
+        const parametroTagEncontrado = peliTag.filter((peliEncontrada) => {
+          return peliEncontrada.tags.includes(options.tag);
+        });
+        return parametroTagEncontrado;
+      });
+    }
   }
-  //add(peli:Peli){
+  add(peli: Peli): Promise<boolean> {
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+      if (peliExistente) {
+        return false;
+      } else {
+        const data = peli;
+        const promesaDos = jsonfile.writeFile("./pelis.json", data);
 
-  //}
+        return promesaDos.then((peli) => {
+          console.log("la pelicula se agrego correctamente");
+          return true;
+        });
+      }
+    });
+
+    return promesaUno;
+  }
 }
 export { PelisCollection, Peli };
 
+const pelis = new PelisCollection();
+const resultado1 = pelis
+  .add({
+    id: 42,
+    title: "Piratas del cari",
+    tags: ["Accion", "Aventura", "Drama"],
+  })
+  .then((i) => {
+    return console.log(i);
+  });
 
+//const pelis = new PelisCollection();
+//const resultado1 = pelis.search({ search: "L", tag: "Drama" }).then((i) => {
+//  return console.log(i);
+//});
 
+//const pelis = new PelisCollection();
+//const resultado1 = pelis.search({ search: "El" }).then((i) => {
+//  return console.log(i);
+//});
 
-
-
-
-
-
-
-
-
-const peli = new PelisCollection();
-const resultado = peli.getAll().then((i)=>{
-  return lodash.find(i,{id:1});
-  
-});
-console.log(resultado)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//const peli2 = new PelisCollection();
-//peli2.getById(4).then((resultado)=>{
-//  console.log(resultado)})
-   
+//const peli = new PelisCollection();
+//const resultado = peli.search({ tag: "Romance" }).then((i) => {
+//  return console.log(i);
+//});
