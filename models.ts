5b835810c26b36829f425e8a90fb92029223a128
catriel <catriel_ramirez@hotmail.com>
2021-07-31 18:05:48 -0300

    Pasaron los test

--- models.ts
+++ models.ts
@@ -1,18 +1,75 @@
 import * as jsonfile from "jsonfile";
 
-// no modificar estas propiedades, agregar todas las que quieras
 class Peli {
   id: number;
   title: string;
   tags: string[];
-}
+};
+
 
 class PelisCollection {
+
+  pelis:Peli[];
+
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json").then((pelis) => {
+      return this.pelis = pelis
     });
+  };
+
+
+  getById(id:number) {
+      return this.getAll().then((pelis)=>{
+      return pelis.find((peli)=>{
+        return peli.id == id;
+      });
+    }) ;
+  };
+
+
+search(options:any) {
+return this.getAll().then((peliculas)=>{
+  if(options.title && options.tag){
+    return peliculas.filter((p)=>{
+      return p.title.includes(options.title) &&
+      p.tags.includes(options.tag)
+        });
+
+  } else if(options.title){
+    return peliculas.filter((p)=>{
+      return p.title.includes(options.title)
+    })
+
+
+  } else if(options.tag){
+    return peliculas.filter((p)=>{
+      return p.tags.includes(options.tag);
+    })
   }
+})
+};
+
+
+add(peli:Peli): Promise<boolean> {
+  return this.getById(peli.id).then((peliExistente) => {
+    if (peliExistente) {
+      return false;
+      console.log("La pelicula que intenta agregar ya existe");
+    } else {
+    
+    return this.getAll().then((peliculas)=> {
+      peliculas.push(peli);
+    
+    return jsonfile.writeFile("./pelis.json", peliculas);
+    });
+    }
+  });
+
 }
+
+};
+
+
+
+
 export { PelisCollection, Peli };
