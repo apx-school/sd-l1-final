82808c6f6a3c9890404c0b221e73120935390a05
ariel <arieldaich@gmail.com>
2021-08-01 11:01:34 -0300

    first commit, 2 tests failed

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,20 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  collection: PelisCollection;
+  constructor() {
+    this.collection = new PelisCollection;
+  }
+  get(options): Promise<any>{
+    if (options.id) {
+      return this.collection.getById(options.id);
+    } else if (options.search) {
+      return this.collection.search(options.search);
+    }
+    return this.collection.getAll();
+  }
+  add(peli:Peli){
+    return this.collection.add(peli);
+  }
 }
 export { PelisController };
--- models.ts
+++ models.ts
@@ -9,9 +9,41 @@ class Peli {
 
 class PelisCollection {
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json").then((res) => {
+      return res;
+    });
+  }
+  getById(id:number): Promise<Peli> {
+    return this.getAll().then((res) => {
+      return res.find(p => p.id == id);
+    });
+  }
+  search(options:any): Promise<Peli[]> {
+    return this.getAll().then((res) => {
+      var result = res;
+      if (options.title) {
+        result = result.filter(p => p.title.includes(options.title));
+      } 
+      if (options.tag) {
+        result = result.filter(p => p.tags.includes(options.tag));
+      }
+      return result;
+    });
+  }
+  add(peli:Peli): Promise<boolean> {
+    return this.getById(peli.id).then((p) => {
+      if (p) {
+        return false;
+      } else {
+        this.getAll().then((res) => {
+          const moviesList = res;
+          moviesList.push(peli);
+          jsonfile.writeFile("./pelis.json", moviesList).then(() => {
+            return true;
+          })
+          .catch(() => {return false});
+        });
+      }
     });
   }
 }
