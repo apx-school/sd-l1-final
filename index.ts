6f73957d92b0fba7f90b49cee160a77798394711
Julieta <agosx_@hotmai.com>
2021-08-16 21:23:00 -0300

    tercer commit: Index.ts-correcion de la jerarquia de los if en parseaParams()

--- index.ts
+++ index.ts
@@ -20,14 +20,14 @@ function comandos(params) {
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
-  if (resultado._[0] == "get") {
-    return {id: resultado._[1]};
+  if (resultado._[0] == "search" && resultado.title && resultado.tag) {
+    return {search: {title: resultado.title, tag: resultado.tag}};
   } else if (resultado._[0] == "search" && resultado.title) {
     return {search: {title: resultado.title}};
   } else if (resultado._[0] == "search" && resultado.tag) {
     return {search: {tag: resultado.tag}};
-  } else if (resultado._[0] == "search" && resultado.title && resultado.tag) {
-    return {search: {title: resultado.title, tag: resultado.tag}};
+  } else if (resultado._[0] == "get") {
+    return {id: resultado._[1]};
   } else if (resultado._[0] == "add") {
     return {
       id: resultado.id,
