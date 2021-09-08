73e302168a17715b02ff056c0dc14e4666bed438
StradaLucas <fxloopsl@hotmail.com>
2021-09-08 19:24:06 -0300

    First commit

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,56 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+
+    pelis: PelisCollection;
+    constructor() {
+      this.pelis = new PelisCollection();
+    }
+    get(options) {
+      
+      if (options.id) {
+        return this.pelis.getById(options.id).then((peliPorId) => {
+          return peliPorId;
+        });
+
+      } else if (options.search) {
+        return this.pelis.search(options.search).then((pelisBuscadas) => {
+          return pelisBuscadas;
+        });
+
+      } else {
+        return this.pelis.getAll().then((todasLasPelis) => {
+          return todasLasPelis;
+        });
+      }
+    }
+    add(peli: Peli) {
+        return this.pelis.add(peli);
+    }
 }
+
+
+// const objeto = new PelisController();
+// objeto.add({"id": 12, "title": "Guardianes de la galaxia", "tags": ["comedia"]}).then((resultado) => {
+//     console.log(resultado);
+// })
+// objeto.get({ id: 10 }).then((resultado) => {
+//   console.log(resultado);
+// })
+// objeto.get({ search:{ tags:"romance", title:"land"}}).then((resultado) => {
+//   console.log(resultado);
+// })
+// objeto.get({ search:{ tags:"animación"}}).then((resultado) => {
+//   console.log(resultado);
+// })
+// objeto.get({ search:{ title: 'and'}}).then((resultado) => {
+//   console.log(resultado);
+// })
+// objeto.get({ search:{ title:"unica"}}).then((resultado) => {
+//   console.log(resultado);
+// })
+// objeto.get({}).then((resultado) => {
+//   console.log(resultado);
+// })
+
 export { PelisController };
--- models.ts
+++ models.ts
@@ -1,6 +1,7 @@
 import * as jsonfile from "jsonfile";
 
 // no modificar estas propiedades, agregar todas las que quieras
+
 class Peli {
   id: number;
   title: string;
@@ -8,11 +9,92 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+
+  getAll(): Promise<any[]> {
+    return jsonfile.readFile("./pelis.json").then((pelis) => {
+      return pelis;
     });
   }
+  
+
+  getById(id: number) {
+    return this.getAll().then((pelis) => {
+      const peliById = pelis.find((obtenerUnaPeli) => {
+        return obtenerUnaPeli.id == id;
+      });
+      return peliById;
+    });
+  }
+
+  search(options) {
+    return this.getAll().then((pelis) => {
+
+      if (options.title && options.tag) {
+        return pelis.filter((peli) => {
+          return(
+            peli.title.includes(options.title) &&
+            peli.tags.includes(options.tag)
+            )
+        });
+
+      } else if (options.title) {
+        return pelis.filter((peli) => {
+          return peli.title.includes(options.title);
+        });
+
+      } else if (options.tag) {
+        return pelis.filter((peli) => {
+          return peli.tags.includes(options.tag);
+        });
+      }
+    });
+  }
+
+  add(peli: Peli): Promise<boolean> {
+    
+    // console.log(peli);
+    
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+
+      if (peliExistente) {
+        return false;
+
+      } else {
+          const promesaDos = this.getAll().then((pelis) => {
+            pelis.push(peli);
+          return jsonfile.writeFile("./pelis.json", pelis);
+        });
+        return promesaDos.then(() => {
+          return true;
+        });
+      }
+    });
+    return promesaUno;
+  }
 }
+
+// const objeto = new PelisCollection();
+// objeto.getAll().then((resultado) => {
+//   console.log(resultado);
+// })
+
+// objeto.getById(1).then((resultado) => {
+//   console.log(resultado);
+// })
+
+// objeto.search({title: 'a'}).then((resultado) => {
+  // console.log(resultado);
+// })
+// objeto.search({"tag": "finanzas"}).then((resultado) => {
+//   console.log(resultado);
+// })
+// objeto.search({"title": "espanta","tag": "animación"}).then((resultado) => {
+//   console.log(resultado);
+// })
+
+// objeto.add({"id": 10, "title": "Guardianes de la galaxia", "tags": ["comedia"]}).then((resultado) => {
+//     console.log(resultado);
+// })
+
+
 export { PelisCollection, Peli };
