1acff8402c71db2e33dd49873978b87beb93e452
Luca <lupattin@gmail.comgit config --global user.email lupattin@gmail.comgit config --global user.email lupattin@gmail.com>
2021-09-08 21:16:04 -0300

    terminado

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,23 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
-}
+  peliculas: PelisCollection
+  constructor() {
+    this.peliculas = new PelisCollection
+  }
+  get(options){
+    if(options.id){
+      return this.peliculas.getById(options.id)
+    }
+    else if(options.search){
+      return this.peliculas.search(options.search)
+      }
+      else{
+        return this.peliculas.getAll()
+      }
+    }
+  add(peli:Peli){
+    return this.peliculas.add(peli)
+  }
+ } 
 export { PelisController };
