187cde28d43e0427b99a66feb703d7e5ca2cf3e2
Caro <carofattore@gmail.com>
2021-09-23 18:23:48 -0300

    programa terminado pero falla un test del controllers
    a
    a

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import * as minimist from "minimist";
+import { PelisController } from "./controllers";
+import { Peli } from "./models";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
@@ -6,10 +8,43 @@ function parseaParams(argv) {
   return resultado;
 }
 
+function paramsAdd(res) {
+  let resultado: Peli;
+  if (res._.includes("add")) {
+    resultado = {
+      id: res.id,
+      title: res.title,
+      tags: res.tags,
+    };
+    return resultado;
+  }
+}
+
+function paramsGet(res) {
+  if (res._.includes("get")) {
+    return { id: res.id };
+  } else if (res._.includes("search")) {
+    return { search: { title: res.title, tag: res.tag } };
+  } else {
+    return {};
+  }
+}
+
 function main() {
   const params = parseaParams(process.argv.slice(2));
+  const controller = new PelisController();
 
-  console.log(params);
+  if (params._[0] == "add") {
+    const funcionAdd = paramsAdd(params);
+    controller.add(funcionAdd).then(() => {
+      console.log("Pelicula agregada");
+    });
+  } else {
+    const funcionGet = paramsGet(params);
+    controller.get(funcionGet).then((res) => {
+      console.log(res);
+    });
+  }
 }
 
 main();
