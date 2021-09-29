b1f48770fd53e2e67c0f355f289d3846296d33a4
Micaela777 <88109114+Micaela777@users.noreply.github.com>
2021-09-29 16:52:35 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
 import * as minimist from "minimist";
+import { PelisController } from "./controllers"
 
 function parseaParams(argv) {
-  const resultado = minimist(argv);
-
-  return resultado;
-}
+  const resultadoMinimist = minimist(argv)
+  return resultadoMinimist
+};
 
 function main() {
   const params = parseaParams(process.argv.slice(2));
-
-  console.log(params);
-}
-
+  console.log(params)
+  const instancia = new PelisController()
+};
 main();
+
