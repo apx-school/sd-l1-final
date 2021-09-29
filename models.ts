e938309acb55acb204a520439d3f672bf96d3ebe
Micaela777 <88109114+Micaela777@users.noreply.github.com>
2021-09-29 15:52:00 -0300

    Update models.ts

--- models.ts
+++ models.ts
@@ -1,6 +1,5 @@
 import * as jsonfile from "jsonfile";
 
-// no modificar estas propiedades, agregar todas las que quieras
 class Peli {
   id: number;
   title: string;
@@ -8,11 +7,55 @@ class Peli {
 }
 
 class PelisCollection {
+
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json").then((peliculas) => {
+      return peliculas
+    })
+  };
+
+  getById(id: number) {
+    return this.getAll().then((pelicula) => {
+      const encontrado = pelicula.find((p) => {
+        return p.id == id
+      })
+      return encontrado
     });
+  };
+
+  search(options: any) {
+    if (options.title) {
+      return this.getAll().then((x) => {
+        return x.filter((y) => {
+          return y.title.includes(options.title)
+        })
+      });
+    } else if (options.tag) {
+      return this.getAll().then((w) => {
+        return w.filter((z) => {
+          return z.tags.includes(options.tag)
+        })
+      });
+    };
+  };
+
+  add(peli: Peli): Promise<boolean> {
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+      if (peliExistente) {
+        return false;
+      } else {
+        const promesaDos = this.getAll().then((x) => {
+          x.push(peli)
+          return jsonfile.writeFile("./pelis.json", x);
+        })
+
+        return promesaDos.then(() => {
+          return true;
+        });
+      }
+    });
+    return promesaUno;
   }
-}
+};
+
 export { PelisCollection, Peli };
