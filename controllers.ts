f1a3b7416bc887dc8ce1068543cf6f3aca960608
Micaela777 <88109114+Micaela777@users.noreply.github.com>
2021-09-29 15:52:54 -0300

    Update controllers.ts

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,29 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
-}
+  peliculas: PelisCollection
+  constructor() {
+    this.peliculas = new PelisCollection()
+  };
+
+  get(options) {
+
+    var resultado
+
+    if (options.id) {
+      resultado = this.peliculas.getById(options.id)
+
+    } else if (options.search) {
+      resultado = this.peliculas.search(options.search)
+
+    } else {
+      resultado = this.peliculas.getAll()
+    }
+  };
+
+  addPeli(peli: Peli) {
+    return this.peliculas.add(peli)
+  };
+
+};
 export { PelisController };
