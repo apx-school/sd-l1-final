bb9baea95e3ec92c81d6ff6139c957fc26406c62
Fernando Areosa <fernando_areosa@hotmail.com.ar>
2021-10-07 15:46:35 -0300

    Fix: Desafio resuelto, se prueban todas las funciones declaradas en el desafio y fincionan bien.-

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,23 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  coll:PelisCollection
+  constructor(){
+    this.coll = new PelisCollection
+  }
+  get(options){
+    if(options.id){
+      return this.coll.getById(options.id)
+    }else if(options.search){
+      return this.coll.search(options.search)
+    }else if(options[0]=="all") {
+      return this.coll.getAll()
+    }
+  }
+  add(peli:Peli){
+    return this.coll.add(peli)
+  }
+
+
 }
 export { PelisController };
--- models.ts
+++ models.ts
@@ -9,10 +9,35 @@ class Peli {
 
 class PelisCollection {
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
-    });
+    return jsonfile.readFile("./pelis.json");
+  };
+  getById(id:number){
+    this.getAll().catch(err=>console.log(err));
+    return this.getAll().then(all=>{return all.find(peli=>peli.id == id)});
+  }; 
+  search(options:any){
+    this.getAll().catch(err=>console.log(err))
+    return this.getAll().then(all=>{
+      if(options.title){
+        return all.filter(peli=>peli.title.toUpperCase().includes(options.title.toUpperCase()))
+      }else if (options.tag){
+        return all.filter(peli=>{return peli.tags.map(tag=>tag.toUpperCase()).includes(options.tag.toUpperCase())})
+      };
+    })
+  };
+  add(peli:Peli):Promise<boolean>{
+     const promesaUno = this.getById(peli.id).then(peliExistente=>{
+      if(peliExistente){
+        return false
+      } else {
+        const added = this.getAll().then(all=>{let rtta=all; rtta.push(peli);return rtta});
+        const promesaDos = added.then (rtta=>jsonfile.writeFile("./pelis.json",rtta))
+        return promesaDos.then(()=>true)
+      };
+    })
+    return promesaUno
+
   }
-}
+
+};
 export { PelisCollection, Peli };
