2abd706689276d550e561c79a72fcff558e3324f
fsanchezvallejo <76439642+fsanchezvallejo@users.noreply.github.com>
2021-10-10 17:17:08 -0300

    Primer commit

--- index.ts
+++ index.ts
@@ -1,15 +1,52 @@
+import { PelisController } from "./controllers";
 import * as minimist from "minimist";
 
 function parseaParams(argv) {
-  const resultado = minimist(argv);
+  const resMinimist = minimist(argv);
 
-  return resultado;
+  if(resMinimist._[0] == "add") {
+    return {
+      title: resMinimist.title,
+      id: resMinimist.id,
+      tags: resMinimist.tags
+    };
+  } else if(resMinimist._[0] == "get"){
+    return {id: resMinimist._[1]};
+  }else if(resMinimist._[0] == "search") {
+    return {search: resMinimist.title};
+  }else if(resMinimist._[0] == "search") {
+    return {search: resMinimist.tag};
+  }else if(resMinimist._[0] == "search" && resMinimist.title && resMinimist.tag) {
+    return {search: {title:resMinimist.title, tag: resMinimist.tag}};
+  }else{
+    return {};
+  }
+}
+
+function comandos(parametros) {
+  const controller = new PelisController();
+  if(parametros.id && parametros.title && parametros.tags) {
+    return controller.add(parametros).then((respuesta)=> {
+      console.log(respuesta);
+    });
+  }else if(parametros.search || parametros.tags) {
+    return controller.get(parametros).then((resultado)=> {
+      console.log(resultado);
+    })
+  }else {
+    return controller.get({}).then((resultado) => {
+      resultado;
+    })
+  }
 }
 
 function main() {
   const params = parseaParams(process.argv.slice(2));
-
-  console.log(params);
-}
+  comandos(params);
+  //const parametrosTreminal = parsearParamsDeTerminal(peliculas, params)
+  //console.log(parametrosTreminal);
+  
+ 
+};
 
 main();
--- models.ts
+++ models.ts
@@ -1,4 +1,5 @@
 import * as jsonfile from "jsonfile";
+import { formatWithOptions } from "util";
 
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
@@ -9,10 +10,70 @@ class Peli {
 
 class PelisCollection {
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
+    return jsonfile.readFile("./pelis.json").then((peli) => {
       // la respuesta de la promesa
-      return [];
+      return peli;
     });
   }
+  getById(id:number) {
+    return this.getAll().then((pelis) => {
+      const resultado = pelis.find((title)=> {
+       return title.id == id;
+      });
+      return resultado;
+    });
+  }
+  search(options:any)  {
+    if (options.title && options.tag){
+      return this.getAll().then((i) => {
+        return i.filter((t) => {
+          return t.title.includes(options.title) && t.tags.includes(options.tag);
+        });
+      });
+    } else if (options.title){
+      return this.getAll().then((i) => {
+        return i.filter((t) => {
+          return t.title.includes(options.title);
+        });
+      });
+    } else if (options.tag){
+      return this.getAll().then((i) => {
+        return i.filter((t) => {
+          return t.tags.includes(options.tag);
+        });
+      });
+    }
+  }
+  add(peli: Peli): Promise<boolean> {
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+      if (peliExistente) {
+        return false;
+      } else {
+        // magia que agrega la pelicula a un objeto data
+        //const data = {}; 
+        const promesaDos = this.getAll().then((peliculas) => {
+          peliculas.push(peli);
+          return jsonfile.writeFile("./pelis.json", peliculas);
+        })
+        return promesaDos.then(() => {
+          
+          return true;
+        });
+      
+      }
+      
+    });
+
+    return promesaUno;
+  }
 }
+
+/*const objeto = new PelisCollection();
+objeto.search("t").then((resultado) => {
+  console.log(resultado);
+})*/
+
+
+
+
 export { PelisCollection, Peli };
