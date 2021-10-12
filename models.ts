067a5210af7f3806d10b4c44a16114eb17fe7a37
unknown <barboriniignacio@gmail.com>
2021-10-12 17:40:51 -0300

    primer entrega del desafio

--- models.ts
+++ models.ts
@@ -9,10 +9,60 @@ class Peli {
 
 class PelisCollection {
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json")
+    .then((json) => {
+      return json;
     });
   }
+
+  getById(id:number){
+    return this.getAll().then((json) => {
+      const respuesta = json.find((obj) => {
+        return obj.id == id 
+      })
+      return respuesta;    
+    })
+  }
+
+  search(options:any): Promise<Peli[]>{  
+    return this.getAll().then((json) =>{ 
+      
+      if(options.title && options.tags){
+        return json.filter((obj) => {
+          return obj.title.includes(options.title) && 
+          obj.tags.includes(options.tags)
+        })
+      }
+      else if(options.title){
+        return json.filter((obj) => {
+          return obj.title.includes(options.title)
+        })
+      }
+      else if(options.tags){
+        return json.filter((obj) => {
+          return obj.tags.includes(options.tags)
+        })
+      }
+    })
+  }
+
+  add(peli: Peli): Promise<boolean> {
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+      if (peliExistente) {
+        return false;
+      } else {
+          const promesaDos = this.getAll().then((pelis) => {
+           pelis.push(peli)
+          return jsonfile.writeFile("./pelis.json", pelis)
+        });
+
+        return promesaDos.then(() => {
+          return true;
+        });
+      }
+    });
+    return promesaUno;
+  }
 }
+
 export { PelisCollection, Peli };
