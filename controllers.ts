e0af9bb93a93b20e70c6da6fed86268fff7c99c8
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-10-13 02:23:38 -0300

    Desafío Finalizado

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,31 @@
-import { PelisCollection, Peli } from "./models";
+import { PelisCollection, Peli } from './models';
 
 class PelisController {
-  constructor() {}
+  pelis: PelisCollection;
+  constructor() {
+    this.pelis = new PelisCollection();
+  }
+  get(options: any) {
+    let resultado;
+    if (options.id) {
+      resultado = this.pelis.getById(options.id);
+    }
+    if (options.search) {
+      if (options.search.title && options.search.tag) {
+        resultado = this.pelis.search({
+          title: options.search.title,
+          tag: options.search.tag,
+        });
+      } else if (options.search.title) {
+        resultado = this.pelis.search({ title: options.search.title });
+      } else if (options.search.tag) {
+        resultado = this.pelis.search({ tag: options.search.tag });
+      }
+    }
+    return resultado;
+  }
+  add(peli: Peli) {
+    return this.pelis.add(peli);
+  }
 }
 export { PelisController };
--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import * as minimist from "minimist";
+import * as minimist from 'minimist';
+import { PelisController } from './controllers';
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
@@ -8,8 +9,38 @@ function parseaParams(argv) {
 
 function main() {
   const params = parseaParams(process.argv.slice(2));
+  const pelisController = new PelisController();
 
-  console.log(params);
+  if (params._.toString().includes('get')) {
+    const id = params._.slice()[1];
+    pelisController.get({ id }).then((res) => console.log(res));
+  }
+  if (params._.toString() === 'search') {
+    let options;
+    if (params.title && params.tag) {
+      options = {
+        search: {
+          title: params.title,
+          tag: params.tag,
+        },
+      };
+      pelisController.get(options).then((res) => console.log(res));
+      // console.log(pelisController.get(options));
+    } else if (params.title) {
+      pelisController
+        .get({ search: { title: params.title } })
+        .then((res) => console.log(res));
+    } else if (params.tag) {
+      pelisController
+        .get({ search: { tag: params.tag } })
+        .then((res) => console.log(res));
+    }
+  }
+  if (params._.toString() === 'add') {
+    pelisController
+      .add({ id: params.id, title: params.title, tags: params.tags })
+      .then((res) => console.log(res));
+  }
 }
 
 main();
