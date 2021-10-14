964e4e67df3db0dfef1f650fd628962b6b630795
ferchamo <fernandochamorro0507@gmail.com>
2021-10-14 15:23:38 -0300

    commit

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,24 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  pelicula = new PelisCollection();
+  constructor() {
+    this.pelicula = new PelisCollection();
+  }
+  get(options): Promise<any> {
+    var resultado;
+    if (options.id) {
+      resultado = this.pelicula.getById(options.id);
+    } else if (options.search) {
+      resultado = this.pelicula.search(options.search);
+    } else {
+      resultado = this.pelicula.getAll();
+    }
+    return resultado;
+  }
+  add(peli: Peli): Promise<boolean> {
+    return this.pelicula.add(peli);
+  }
 }
+
 export { PelisController };
