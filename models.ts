5bf9fc4c11d730bd252d0dca4e160947d9ceaf0d
Pedro Jaime <pedrojaime12pb@gmail.com>
2021-10-14 13:08:40 -0300

    primer commit con models.ts y pelis.json

--- models.ts
+++ models.ts
@@ -1,4 +1,5 @@
 import * as jsonfile from "jsonfile";
+import { title } from "process";
 
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
@@ -9,10 +10,61 @@ class Peli {
 
 class PelisCollection {
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json").then((a) => {
+      return (a);
     });
   }
+  getById(id:number){
+   return this.getAll().then((a) => {
+       const peliFind = a.find(resultado => {
+       return resultado.id == id
+      })
+      return peliFind
+    })
+  } 
+  search(options:any){
+    return this.getAll().then((a) => {
+      if ( options.title && options.tags){
+        return a.filter((objeto) => {
+         return objeto.title.includes(options.title) 
+         && objeto.tags.includes(options.tags)
+        })
+      } else if (options.tags){
+        return a.filter((objeto) => {
+          objeto.tags.includes(options.tags)
+        })
+      } else if (options.title){
+        return a.filter((objeto) => {
+          objeto.title.includes(options.title)
+        })
+      }
+    }) 
+  }
+  addPeli(peli:Peli): Promise<boolean> {
+    const primerPromesa = this.getById(peli.id).then((idPeliRepetido) => {
+      if (idPeliRepetido){
+        return false;
+      } else {
+        const segundaPromesa = this.getAll().then((nuevaPeli) => {
+          nuevaPeli.push(peli)
+          return jsonfile.writeFile("./pelis.json", nuevaPeli)
+        })
+        return segundaPromesa.then((resultado) => {
+          return resultado = true
+        })
+      }
+    })
+    return primerPromesa;
+  }  
 }
+
+/*  
+const obj = new PelisCollection()
+obj.search([{tags: "acción"}]).then((it) => {
+  return console.log(it);
+})
+
+*/
+
+
 export { PelisCollection, Peli };
