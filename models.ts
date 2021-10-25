9364d89eb87ea566448c7a805b0c203eb27e9d7d
Florencia Luz Duarte <florluzduarte@gmail.com>
2021-10-24 21:36:17 -0300

    resuelvo los metrodos getAll y getById del models

--- models.ts
+++ models.ts
@@ -8,11 +8,56 @@ class Peli {
 }
 
 class PelisCollection {
+
+  //getAll() devuelve un array del tipo Peli con todas las pelis que se encuentren guardadas
+  //en el archivo JSON de pelis.
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json").then((peliculas) => {
+      return peliculas;
     });
   }
-}
+
+  //getById(id:number) devuelve la peli que tenga el id que se le pase por parámetro.
+  getById(id:number) {
+    return this.getAll().then(peliculas => {
+       const peliculaBuscada = peliculas.find(pelis =>{
+         return pelis.id == id;
+       })
+    return peliculaBuscada;   
+    });
+
+    
+  }
+  
+  //search(options:any) recibe un objeto y, según cuales sean sus propiedades, hay dos opciones:
+  search(options:any) {
+
+    //si el objeto tiene la propiedad title, el método tiene que devolver todas
+    //las películas que tengan ese string en su title.
+    if(options.title){
+      return this.getAll().then(peliculas => {
+        const resultadoTitle = peliculas.filter(pelis => {
+          return pelis.title.includes(options.title);
+        })
+        return resultadoTitle;
+        })
+      }
+
+      //si el objeto tiene la propiedad tag, el método tiene
+      //que devolver todas las películas que tengan ese string en sus tags.
+      // if(options.tag){
+      //   return this.getAll().then(peliculas => {
+      //     const resultadoTags = peliculas.
+      //   })
+      // }
+    }
+
+    
+  }
+
+const prueba = new PelisCollection();
+prueba.getById(3).then(resultado => {
+  console.log(resultado);
+})
+
 export { PelisCollection, Peli };
