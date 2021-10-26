5e5822afae34d109b07050ee03724c5d4c28f6a7
Fypoolday <Fypoolday@users.noreply.github.com>
2021-10-26 18:26:57 -0300

    segundo commit

--- models.ts
+++ models.ts
@@ -1,5 +1,5 @@
 import * as jsonfile from "jsonfile";
-
+import * as lodash from "lodash";
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
   id: number;
@@ -9,10 +9,36 @@ class Peli {
 
 class PelisCollection {
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
-    });
+    return jsonfile.readFile("./pelis.json");
+  };
+  getById(id:number){
+    this.getAll().catch(err=>console.log(err));
+    return this.getAll().then(all=>{return all.find(peli=>peli.id == id)});
+  }; 
+  search(options:any){
+    this.getAll().catch(err=>console.log(err))
+    return this.getAll().then(all=>{
+      if(options.title && options.tag){
+        return lodash.filter(all,i=>i.title.includes(options.title)&&i.tags.includes(options.tag))
+      }if(options.title){
+        return lodash.filter(all,i=>i.title.includes(options.title))
+      }else if (options.tag){
+        return lodash.filter(all,i=>i.tags.includes(options.tag))
+      }
+    })
+  };
+  add(peli:Peli):Promise<boolean>{
+     const promesaUno = this.getById(peli.id).then(peliExistente=>{
+      if(peliExistente){
+        return false
+      } else {
+        const added = this.getAll().then(all=>{let rtta=all; rtta.push(peli);return rtta});
+        const promesaDos = added.then (rtta=>jsonfile.writeFile("./pelis.json",rtta))
+        return promesaDos.then(()=>true)
+      };
+    })
+    return promesaUno
   }
-}
+
+};
 export { PelisCollection, Peli };
