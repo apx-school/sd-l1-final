b13aef90d7948cd79842ea0b68239a818f0ec70c
Luis Trujillo <lu.a.tru.sul@gmail.com>
2021-11-02 19:28:43 -0300

    módulo controllers.ts terminado, eliminar prueba en la parte inferior!

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,34 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  pelis: PelisCollection;
+  constructor() {
+    this.pelis = new PelisCollection();
+  }
+  get(options) {
+    if (options.id) {
+      return this.pelis.getById(options.id);
+    } else if (options.search.title && options.search.tag) {
+      return this.pelis.search({
+        title: options.search.title,
+        tag: options.search.tag,
+      });
+    } else if (options.search.title) {
+      return this.pelis.search(options.search.title);
+    } else if (options.search.tag) {
+      return this.pelis.search(options.search.tag);
+    } else {
+      return this.pelis.getAll();
+    }
+  }
+  add(peli: Peli) {
+    return this.pelis.add(peli);
+  }
 }
+
 export { PelisController };
+
+// const nuevaPelisController = new PelisController();
+
+// const getId = nuevaPelisController.get({ id: 2 });
+// console.log(getId);
