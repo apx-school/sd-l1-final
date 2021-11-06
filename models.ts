038789b9eeabe3170ff651bb5b11c26c5abdda6a
Luis Trujillo <lu.a.tru.sul@gmail.com>
2021-11-06 12:10:28 -0300

    antes del estallido!

--- models.ts
+++ models.ts
@@ -23,27 +23,47 @@ class PelisCollection {
     });
   }
   search(options: any) {
-    return this.getAll().then((collection) => {
+    return this.getAll().then((json: any) => {
       if (options.title && options.tag) {
-        const resultadoTitle = collection.filter((films) =>
-          films.title.includes(options.title.toLowerCase())
-        );
-        return resultadoTitle.filter((film) =>
-          film.tags.find((a) => a == options.tag.toLowerCase())
-        );
+        return json.filter((obj) => {
+          return (
+            obj.title.includes(options.title) && obj.tags.includes(options.tag)
+          );
+        });
       } else if (options.title) {
-        const resultadoTitle = collection.filter((films) =>
-          films.title.includes(options.title.toLowerCase())
-        );
-        return resultadoTitle;
+        return json.filter((obj) => {
+          return obj.title.includes(options.title);
+        });
       } else if (options.tag) {
-        const resultadoTags = collection.filter((films) =>
-          films.tags.find((b) => b == options.tag.toLowerCase())
-        );
-        return resultadoTags;
+        return json.filter((obj) => {
+          return obj.tags.includes(options.tag);
+        });
       }
     });
   }
+
+  // search(options: any) {
+  //   return this.getAll().then((collection) => {
+  //     if (options.title && options.tag) {
+  //       const resultadoTitle = collection.filter((films) =>
+  //         films.title.toLowerCase().includes(options.title.toLowerCase())
+  //       );
+  //       return resultadoTitle.filter((film) =>
+  //         film.tags.find((a) => a == options.tag.toLowerCase())
+  //       );
+  //     } else if (options.title) {
+  //       const resultadoTitle = collection.filter((films) =>
+  //         films.title.toLowerCase().includes(options.title.toLowerCase())
+  //       );
+  //       return resultadoTitle;
+  //     } else if (options.tag) {
+  //       const resultadoTags = collection.filter((films) =>
+  //         films.tags.find((b) => b == options.tag.toLowerCase())
+  //       );
+  //       return resultadoTags;
+  //     }
+  //   });
+  // }
   add(peli: Peli): Promise<boolean> {
     const promesaUno = this.getById(peli.id).then((peliExistente) => {
       if (peliExistente) {
@@ -51,9 +71,9 @@ class PelisCollection {
       } else {
         // magia que agrega la pelicula a un objeto data
         return this.getAll().then((collection) => {
-          var data = collection;
+          // var data = collection;
           collection.push(peli);
-          const promesaDos = jsonfile.writeFile("./pelis.json", data);
+          const promesaDos = jsonfile.writeFile("./pelis.json", collection);
           return promesaDos.then(() => {
             return true;
           });
@@ -67,3 +87,37 @@ class PelisCollection {
 export { PelisCollection, Peli };
 
 const nuevaPelisCollection = new PelisCollection();
+
+//getAll
+// nuevaPelisCollection.getAll().then((getAll) => {
+//   console.log(getAll);
+// });
+
+//getById
+// nuevaPelisCollection.getById(1).then((getById) => {
+//   console.log(getById);
+// });
+
+//search
+//title
+// nuevaPelisCollection.search({ title: "S" }).then((search) => {
+//   console.log(search);
+// });
+//tag
+// nuevaPelisCollection.search({ tag: "cOMeDia" }).then((search) => {
+//   console.log(search);
+// });
+//title & tag
+// nuevaPelisCollection.search({ title: "T", tag: "drama" }).then((titleTag) => {
+//   console.log(titleTag);
+// });
+
+//add
+// const nuevaPeli = new Peli();
+// nuevaPeli.id = 10;
+// nuevaPeli.title = "Nueva peli";
+// nuevaPeli.tags = ["prueba 1", "prueba 2"];
+
+// nuevaPelisCollection.add(nuevaPeli).then((add) => {
+//   console.log(add);
+// });
