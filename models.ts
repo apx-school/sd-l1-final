5de712b773f925a56d5e5563cbc00a17ff978f95
Luis Trujillo <lu.a.tru.sul@gmail.com>
2021-11-06 12:12:05 -0300

    listo para enviar

--- models.ts
+++ models.ts
@@ -42,28 +42,6 @@ class PelisCollection {
     });
   }
 
-  // search(options: any) {
-  //   return this.getAll().then((collection) => {
-  //     if (options.title && options.tag) {
-  //       const resultadoTitle = collection.filter((films) =>
-  //         films.title.toLowerCase().includes(options.title.toLowerCase())
-  //       );
-  //       return resultadoTitle.filter((film) =>
-  //         film.tags.find((a) => a == options.tag.toLowerCase())
-  //       );
-  //     } else if (options.title) {
-  //       const resultadoTitle = collection.filter((films) =>
-  //         films.title.toLowerCase().includes(options.title.toLowerCase())
-  //       );
-  //       return resultadoTitle;
-  //     } else if (options.tag) {
-  //       const resultadoTags = collection.filter((films) =>
-  //         films.tags.find((b) => b == options.tag.toLowerCase())
-  //       );
-  //       return resultadoTags;
-  //     }
-  //   });
-  // }
   add(peli: Peli): Promise<boolean> {
     const promesaUno = this.getById(peli.id).then((peliExistente) => {
       if (peliExistente) {
@@ -87,37 +65,3 @@ class PelisCollection {
 export { PelisCollection, Peli };
 
 const nuevaPelisCollection = new PelisCollection();
-
-//getAll
-// nuevaPelisCollection.getAll().then((getAll) => {
-//   console.log(getAll);
-// });
-
-//getById
-// nuevaPelisCollection.getById(1).then((getById) => {
-//   console.log(getById);
-// });
-
-//search
-//title
-// nuevaPelisCollection.search({ title: "S" }).then((search) => {
-//   console.log(search);
-// });
-//tag
-// nuevaPelisCollection.search({ tag: "cOMeDia" }).then((search) => {
-//   console.log(search);
-// });
-//title & tag
-// nuevaPelisCollection.search({ title: "T", tag: "drama" }).then((titleTag) => {
-//   console.log(titleTag);
-// });
-
-//add
-// const nuevaPeli = new Peli();
-// nuevaPeli.id = 10;
-// nuevaPeli.title = "Nueva peli";
-// nuevaPeli.tags = ["prueba 1", "prueba 2"];
-
-// nuevaPelisCollection.add(nuevaPeli).then((add) => {
-//   console.log(add);
-// });
