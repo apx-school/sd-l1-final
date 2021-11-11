3477a99bd28f210de037b5dea9a3bc9dbd2554d6
Your Name <you@example.com>
2021-11-11 18:54:43 -0300

    models funciona

--- models.ts
+++ models.ts
@@ -8,25 +8,63 @@ class Peli {
 }
 
 class PelisCollection {
-  data = [];
   getAll(): Promise<Peli[]> {
-    return jsonfile.readFile('./pelis.json').then((collection) => {
-      this.data = collection;
-      return collection;
+    return jsonfile.readFile('./pelis.json').then((peliculas) => {
+      return peliculas;
     });
   }
 
-  getById(id: number) {
-    return this.getAll().then((collection) => {
-      var peliBuscada = collection.find((peli) => peli.id == id);
+  getById(id: number): Promise<Peli> {
+    return this.getAll().then((peliculas) => {
+      var peliBuscada = peliculas.find((peli) => peli.id == id);
       return peliBuscada;
     });
   }
+
+  search(options: any): Promise<any> {
+    return this.getAll().then((peliculas) => {
+      if (options.title && options.tag) {
+        const resultadoTitle = peliculas.filter((pelis) =>
+          pelis.title.includes(options.title)
+        );
+        return resultadoTitle.filter((peli) =>
+          peli.tags.find((t) => t == options.tag)
+        );
+      } else if (options.title) {
+        const resultadoTitle = peliculas.filter((pelis) =>
+          pelis.title.includes(options.title)
+        );
+        return resultadoTitle;
+      } else if (options.tag) {
+        const resultadoTags = peliculas.filter((pelis) =>
+          pelis.tags.find((t) => t == options.tag)
+        );
+        return resultadoTags;
+      }
+    });
+  }
+  add(peli: Peli): Promise<boolean> {
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+      if (peliExistente) {
+        return false;
+      } else {
+        return this.getAll().then((peliculas) => {
+          const data = peliculas;
+          peliculas.push(peli);
+          const promesaDos = jsonfile.writeFile('./pelis.json', data);
+          return promesaDos.then(() => {
+            return true;
+          });
+        });
+      }
+    });
+
+    return promesaUno;
+  }
 }
 
-const peliculas = new PelisCollection();
-peliculas.getById(2).then((e) => {
-  console.log(e);
-});
+const x = new PelisCollection();
+var options = { title: 'mpire' };
+x.search(options).then((p) => console.log(p));
 console.log('a');
 export { PelisCollection, Peli };
