4f875e2a37bda7e141ac3c057f2a871edcafee5e
Dylan <dylanfp14@gmail.com>
2021-11-27 19:09:49 -0300

    desafío finalizado

--- models.ts
+++ models.ts
@@ -9,10 +9,52 @@ class Peli {
 
 class PelisCollection {
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json").then((res) => {
+      return res;
+    });
+    // la respuesta de la promesa
+  }
+
+  getById(id: number) {
+    return this.getAll().then((res) => {
+      return res.find((i) => {
+        return i.id == id;
+      });
     });
   }
+
+  search(options: any): Promise<any> {
+    return this.getAll().then((res) => {
+      if (options.title && options.tag) {
+        return res.filter((i) => {
+          return (
+            i.title.includes(options.title) && i.tags.includes(options.tag)
+          );
+        });
+      } else if (options.title) {
+        return res.filter((i) => i.title.includes(options.title));
+      } else if (options.tag) {
+        return res.filter((i) => i.tags.includes(options.tag));
+      }
+    });
+  }
+
+  add(peli: Peli): Promise<boolean> {
+    const promesaUno = this.getById(peli.id).then((peliExiste) => {
+      if (peliExiste) {
+        return false;
+      } else {
+        const promesaDos = this.getAll().then((res) => {
+          res.push(peli);
+          return jsonfile.writeFileSync("./pelis.json", res);
+        });
+        return promesaDos.then((res) => {
+          return true;
+        });
+      }
+    });
+    return promesaUno;
+  }
 }
+
 export { PelisCollection, Peli };
