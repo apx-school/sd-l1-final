335862374059502ccf16453616072c469ab4c67d
Lucas Bastidas <lucaasbastidas@hotmail.com>
2021-11-27 15:27:45 -0300

    desafio terminado

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import * as minimist from "minimist";
+import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
@@ -6,10 +7,43 @@ function parseaParams(argv) {
   return resultado;
 }
 
-function main() {
-  const params = parseaParams(process.argv.slice(2));
+async function parametrosYControlador ( controlador, parametros ){
+  if(parametros._[0]==="add"){
+    const resultado = controlador.add({id: parametros.id, title: parametros.title, tags: parametros.tags})
+    return resultado
+  }
 
-  console.log(params);
+  else if(parametros._[0] === "get"){
+      const resultado = controlador.get({id: parametros._[1]})
+      return resultado
+    }
+    else if(parametros._[0] === "search" && parametros.title && parametros.tag){
+      const resultado= await controlador.get({search:{title: parametros.title, tag: parametros.tag}})
+      return resultado;
+    }
+  else if(parametros._[0] === "search" && parametros.title ){
+    const resultado = await controlador.get({search:{title: parametros.title}})
+    return resultado;
+  }
+  else if(parametros._[0] === "search" && parametros.tag ){
+    const resultado = await controlador.get({search:{tag: parametros.tag}})
+    return resultado;
+  }
+
+  else{
+    const resultado = await controlador.get("nada")
+    return resultado
+  }
+
+}
+
+async function main() {
+  const params = parseaParams(process.argv.slice(2));
+  const controlador = new PelisController;
+  const finalParams = parametrosYControlador(controlador, params);
+  
+
+  console.log(await finalParams)
 }
 
 main();
--- models.ts
+++ models.ts
@@ -1,4 +1,5 @@
 import * as jsonfile from "jsonfile";
+import * as lodash from "lodash";
 
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
@@ -8,11 +9,48 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
-    });
+  async getAll(): Promise<Peli[]> {
+    const todasLasPelis = jsonfile.readFile("./pelis.json");
+    return todasLasPelis;
+   
+  }
+  async getById(id:number){
+    const pelis =  await this.getAll()
+    const resultado = await lodash.find(pelis, function(i){return i.id === id})
+    return resultado;
+  }
+  async search(options:any){
+    if(options.title && options.tag){
+      const pelis = await this.getAll();
+      const buscarTitle = await lodash.filter(pelis, function(i){return (i.title).toLowerCase().includes(options.title)});
+      const resultado = await buscarTitle.filter((peli)=> peli.tags.find(tag => tag == options.tag)) 
+      return resultado
+    }
+
+    else if(options.title){
+      const pelis =  await this.getAll();
+      const resultado = await lodash.filter(pelis, function(i){return (i.title).toLowerCase().includes(options.title)})
+      return resultado;
+    }
+    else if(options.tag){
+      const pelis =  await this.getAll();
+      const resultado = await lodash.filter(pelis, function(i){return i.tags.includes(options.tag)})
+      return resultado;
+    }
+  }
+  async add(peli:Peli){
+    const pelis = await this.getAll();
+    const repetido =  this.getById(peli.id)
+    if(await repetido){
+      return false
+    }
+    else{
+      const data = pelis;
+      data.push(peli);
+      const resultado = await jsonfile.writeFile("./pelis.json",data);
+      return resultado
+    }
+
   }
 }
 export { PelisCollection, Peli };
