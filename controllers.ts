d1eeee386164fee85d44026845855c258c51ff30
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2021-11-27 23:22:58 -0300

    segundo commit
    chequeando test

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,34 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  CollPelis : PelisCollection;
+  constructor() {
+    this.CollPelis = new PelisCollection();
+  }
+
+  /* get(options) recibe un objeto y, según cuales sean sus propiedades */
+
+  get(options:any){
+    if(options.id){
+     return this.CollPelis.getById(options.id);
+    } else if(options.search){
+      if (options.search.title && options.search.tag){
+       return this.CollPelis.search({
+        title: options.search.title,
+        tag: options.search.tag
+       })
+      } else if (options.search.title) {
+        return this.CollPelis.search({title: options.search.title})
+      } else if (options.search.tag){
+        return this.CollPelis.search({tag: options.search.tag})
+      } 
+    } else {
+      return this.CollPelis.getAll();
+    }
+  };
+
+  add(peli:Peli){
+    return this.CollPelis.add(peli);
+  }
 }
 export { PelisController };
--- models.ts
+++ models.ts
@@ -8,11 +8,60 @@ class Peli {
 }
 
 class PelisCollection {
+  //Metodo getAll:devuelve un array del tipo Peli con todas las pelis que se encuentren guardadas en el archivo JSON de pelis.
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
+    return jsonfile.readFile("./pelis.json").then((dataPelis) => {
       // la respuesta de la promesa
-      return [];
+      return dataPelis;
     });
   }
+  //Metodo getById(id:number):devuelve la peli que tenga el id que se le pase por parámetro.
+  getById(id:number) {
+    return this.getAll().then(peliculas => {
+       const buscarxId = peliculas.find(pelis => pelis.id == id);
+    return buscarxId;   
+    });
+  }
+  //Metodo Search:(title),(tag).
+  search(options:any): Promise<any> {
+    return this.getAll().then((x)=>{
+      if(options.title && options.tag){
+        return x.filter((searchPelis)=> { return searchPelis.title.includes(options.title) && searchPelis.tags.includes(options.tag)
+        });   
+   }
+   //Search por Title. 
+   else if(options.title){
+    const filtroxTitle = x.filter((searchPelis) => searchPelis.title.includes(options.title));
+      return filtroxTitle;
+   }
+   //Search por tag.
+   else if(options.tag){
+      return x.filter((searchPelis)=> { return searchPelis.tags.includes(options.tag);
+        
+      });
+    };
+  });
+};
+
+  /* Metodo Add:recibe un objeto y crea una peli en base a él.*/
+  add(peli:Peli): Promise<boolean> {
+    const promesaUno = this.getById(peli.id).then((peliExistente)=>{
+      if(peliExistente){
+        return false;
+      } else{
+        // magia que agrega la pelicula a un objeto data
+        return this.getAll().then((addPelis)=>{
+          const data = addPelis;
+          addPelis.push(peli)
+        const promesaDos = jsonfile.writeFile("./pelis.json", data)
+        return promesaDos.then(()=>{
+          return true;
+        });
+      });
+    };
+      });
+    
+    return promesaUno;
+  };
 }
 export { PelisCollection, Peli };
