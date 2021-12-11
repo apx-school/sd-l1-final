d91db8504d7c58f1a8023cdc0652a7b26501387a
Julian Gibelli <julian.gibelli@watea.com.ar>
2021-12-11 16:19:41 -0300

    models completed, controllers in progress

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,43 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  collection: PelisCollection;
+
+  constructor() {
+    this.collection = new PelisCollection();
+  }
+
+  get(option?: any) {
+    var result;
+    if (option == undefined) {          
+      return result = this.collection.getAll();      
+    }
+    if (option.id) {
+      result = this.collection.getById(option.id);
+    } else if (option.search.title && option.search.tags) {
+            
+      return this.collection.search({tags:option.search.tags}).then((res)=>{
+        const respuesta = res.filter((peli)=>{
+          return peli.title.includes(option.search.title)
+        })
+        return respuesta
+      })
+    } else if (option.search.title) {
+      result = this.collection.search(option.search);
+    } else if (option.search.tags) {
+      result = this.collection.search(option.search);
+    } 
+    return result;
+  }
+
+  add(peli:Peli){
+    return this.collection.add(peli);
+  }
+
 }
+
 export { PelisController };
+
+/*  const prueba = new PelisController();
+prueba.add({ id: 11, title: "probando", tags: ["Drama"] }).then((res)=>{console.log(res)})
+  */
--- models.ts
+++ models.ts
@@ -1,3 +1,5 @@
+//funcionando
+
 import * as jsonfile from "jsonfile";
 
 // no modificar estas propiedades, agregar todas las que quieras
@@ -8,11 +10,65 @@ class Peli {
 }
 
 class PelisCollection {
+  coleccion: Peli[] = [];
+
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json").then((pelis) => {
+      return (this.coleccion = pelis);
     });
+  }
+
+  getById(id: number) {
+    return this.getAll().then((peli) => {
+      const peliHallada = peli.find((e) => {
+        return e.id == id;
+      });
+      return peliHallada;
+    });
+  }
+
+  search(option: any) {
+    if (option.title) {
+      return this.getAll().then((peli) => {
+        let pelisHalladas = peli.filter((e) => {
+          return e.title.includes(option.title);
+        });
+        return pelisHalladas;
+      });
+    } else {
+      if (option.tags) {
+        return this.getAll().then((peli) => {
+          const hallado = peli.filter((e) => {
+            return e.tags.find((r) => r == option.tags);
+          });
+          return hallado;
+        });
+      }
+    }
+  }
+
+  add(pelicula: Peli): Promise<boolean> {
+    //toma una pelicula como parametro y devuelve una promesa de tipo boolean
+    const promesa1 = this.getById(pelicula.id).then((peliExistente) => {
+      if (peliExistente) {
+        return false; //osea si encuentra una peli con el mismo id devuelve false
+      } else {
+        var data = jsonfile.readFileSync("./pelis.json");        
+        data.push(pelicula);
+      
+        return jsonfile.writeFile("./pelis.json", data).then(() => {
+            return true;
+          });
+      }
+    });
+    return promesa1;
   }
 }
 export { PelisCollection, Peli };
+
+
+/* const biblio = new PelisCollection();
+const laPromesa = biblio.add({ id: 10, title: "matrix", tags: ["Drama"] }).then((e) => {
+    console.log(e);
+  });
+ */
