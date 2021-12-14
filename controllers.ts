143504bcfaca53462a3f769cced58cff68f76a76
unknown <marian.b.pastran@gmail.com>
2021-12-14 17:15:17 -0300

    viendo que esta mal

--- controllers.ts
+++ controllers.ts
@@ -1,34 +1,33 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
- data: PelisCollection;
+  data: PelisCollection;
   constructor() {
     this.data = new PelisCollection();
   }
-
+  
   get(options:any){
     
-    if (options.id) {
-      return  this.data.getById(options.id).then((res)=>console.log(res))
+  if (options == undefined) {
+    return  this.data.getAll().then((res)=>{console.log(res)})
 
-    }else if (options.search.title && options.search.tag) {
+    } else if (options.id) {
+      return  this.data.getById(options.id).then((res)=>res)
 
-      return  this.data.search({tags:options.search.tag}).then((res)=>{
-        var respons = res.filter((peli)=>{ 
+    }else if (options.search.title && options.search.tags) {
+      return  this.data.search({tags:options.search.tags}).then((res)=>{
+        var response = res.filter((peli)=>{ 
           return peli.includes(options.search.title)
         })
-         return respons
+         return response
       })
+      
     }else if (options.search.title) {
       
       return  this.data.search({title: options.search.title}).then((res)=> res )
 
     }else if (options.search.tags) {
-      return  this.data.search({tag: options.search.tags}).then((res)=>res)
-
-    }else if (options == undefined) {
-      return  this.data.getAll().then((res)=>{console.log(res)})
-    }
+      return  this.data.search({tags: options.search.tags}).then((res)=>res)}
   return ;
   }
 
--- index.ts
+++ index.ts
@@ -1,17 +1,23 @@
 import * as minimist from "minimist";
 import {PelisController} from "./controllers"
+
 function parseaParams(argv) {
-  const resultado = minimist(argv);
-  return resultado;
+  const params = minimist(argv);
+  if(params.search){
+    return {search: params.search}
+  }else if(params.get){
+    
+  }
   
 }
+
 function processOption(params){
   const controller = new PelisController();
- 
+}
 
 function main() {
   const params = parseaParams(process.argv.slice(2));
-  return processOption(params).then((res)=> console.log(res));
+  //return processOption(params).then((res)=> console.log(res));
   
 };
-main()
+main();
