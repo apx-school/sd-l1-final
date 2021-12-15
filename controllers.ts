66b705aff8a3d67da36f0a5c63d86857e7472cac
unknown <marian.b.pastran@gmail.com>
2021-12-15 20:40:19 -0300

    Modifique controllers

--- controllers.ts
+++ controllers.ts
@@ -7,32 +7,28 @@ class PelisController {
   }
   
   get(options:any){
-    
-  if (options == undefined) {
-    return  this.data.getAll().then((res)=>{console.log(res)})
-
-    } else if (options.id) {
-      return  this.data.getById(options.id).then((res)=>res)
-
-    }else if (options.search.title && options.search.tags) {
-      return  this.data.search({tags:options.search.tags}).then((res)=>{
-        var response = res.filter((peli)=>{ 
-          return peli.includes(options.search.title)
-        })
-         return response
-      })
-      
-    }else if (options.search.title) {
-      
-      return  this.data.search({title: options.search.title}).then((res)=> res )
-
-    }else if (options.search.tags) {
-      return  this.data.search({tags: options.search.tags}).then((res)=>res)}
-  return ;
+    if(options.id){
+      return this.data.getById(options.id)
+    } else if (options.search.title && options.search.tags){
+      return this.data.search({
+        title: options.search.title,
+        tags: options.search.tags,
+      });
+    } else if(options.search.title){
+      return this.data.search({
+        title: options.search.title
+      });
+    } else if (options.search.tags){
+      return this.data.search({
+        tags: options.search.tags
+      });
+    }else{
+      return this.data.getAll().then((res)=>{console.log(res)});
+    }
   }
-
+  
   add(peli:Peli){
-    this.data.add(peli);
+    return this.data.add(peli);
   }
 
 }
