a18a2ecb3911f44e5a058a89d620aa54140d53e0
unknown <marian.b.pastran@gmail.com>
2021-12-21 21:49:54 -0300

    le quite los console log innecesarios

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import * as mini from "minimist";
 import {PelisController} from "./controllers"
 import * as _ from "lodash";
-import { Cipher } from "crypto";
+
 
 function parseaParams(argv) {
   return  mini(argv);
@@ -19,13 +19,13 @@ function processOptions(params) {
     }
 
   if (params._[0] == "get") {
-    console.log("entro en getiD")
+  
     return controller.get({ id: params._[1] }).then((res) => res);
  }
 
 
   if (params._[0] == "search" && params.title && params.tag) {
-    console.log("entro en search.title y tag")
+  
     return controller
       .get({ search: { title: params.title, tag: params.tag } })
       .then((res) => res);
@@ -33,19 +33,19 @@ function processOptions(params) {
 
 
   if (params._[0] == "search" && params.title) {
-    console.log("entro en search.title")
+
     return controller
       .get({ search: { title: params.title } })
       .then((res) => res);
   }
 
    if (params._[0] == "search" && params.tag) {
-     console.log("entro en search.tag")
+   
     return controller.get({ search: { tag: params.tag } }).then((res) => res);
   }
   
   if (_.isEmpty(params._[0])) {
-    console.log("entro en empty")
+   
     return controller.get({empty:{argv:"vacia"}}).then((res)=>res);
    
   }
@@ -53,7 +53,6 @@ function processOptions(params) {
 
 function main() {
   const params = parseaParams(process.argv.slice(2));
-  console.log(params);
   return processOptions(params).then((res) => console.log(res));
   
 };
