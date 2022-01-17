9e71760dbb7f261b9f4bacb9c5e79842dfe5cf39
Leandro Monteleone <monteleonelea@gmail.com>
2022-01-17 12:23:13 -0300

    Controller finalizado

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,43 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  peliculas: PelisCollection;
+  constructor() {
+    const pelis = new PelisCollection;
+    this.peliculas = pelis;
+  }
+
+  get (options) {
+    if (options.id) {
+      return this.peliculas.getById(options.id);
+    }
+
+    else if (options.search){
+      if (options.search.title) {
+        return this.peliculas.search(options.search);
+      }
+      else if (options.search.tags) {
+        return this.peliculas.search(options.tags);
+      }
+      else if (options.search.title && options.search.tags) {
+        return this.peliculas.search(options.search.tags).then((conTag) => {
+          return conTag.title.includes(options.search.title);
+        });
+      }
+    }
+
+    else if (!options) {
+      return this.peliculas.getAll();
+    }
+  }
+
+  add (peli: Peli) {
+    return this.peliculas.addPeli(peli);
+  }
+  
 }
+
+
+
+
 export { PelisController };
