a2550862a6027f6795ee38aaecdfa1b63db8d77d
Leandro Monteleone <monteleonelea@gmail.com>
2022-01-17 11:50:59 -0300

    Model finalizado V 1.0

--- models.ts
+++ models.ts
@@ -9,10 +9,67 @@ class Peli {
 
 class PelisCollection {
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json").then((peliculas) => {
+      
+      return peliculas;
     });
+  };
+  
+ getById(id:number) {
+   return this.getAll().then ((peliculas) =>{
+     const resultado = peliculas.find(p => {
+       return p.id == id;
+     });
+     return resultado;
+   });
+
+ }
+
+  search(options: any) {
+    return this.getAll().then((peliculas) => {
+      if (options.title) {
+        const resultado = peliculas.find((pelis) => {
+          return pelis.title.includes(options.title);
+        })
+        return resultado;
+      }
+      else if (options.tags) {
+        const resultado = peliculas.find(pelis => {
+          return pelis.tags == options.tags;
+        });
+        return resultado;
+      }
+    });
+
+  };
+
+  addPeli(peli:Peli):Promise<boolean> {
+   const promesauno = this.getById(peli.id).then ((encontrada) => {
+      if (encontrada) {
+      return false;
+    }
+      else {
+        return this.getAll().then((peliculas)=> {
+            peliculas.push(peli);
+            return jsonfile.writeFile("./pelis.json", peliculas).then (() => {
+              return true;
+        });
+
+      });
+    }
+   });
+   return promesauno;
   }
 }
+
+
+
+const prueba = new PelisCollection;
+
+prueba.search({tags: "epic"}).then((p => {
+  console.log (p);
+}))
+
 export { PelisCollection, Peli };
+
+
