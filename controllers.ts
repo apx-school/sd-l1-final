6444ef766c64ed9b78311983b5a217717abab820
Richard Miguel Irala <richardirala47@hotmail.com>
2022-02-02 13:10:52 -0300

    Desafio completado: commit 1

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,27 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  pelis: PelisCollection;
+  constructor() {
+    this.pelis = new PelisCollection();
+  }
+  async get(options): Promise<any> {
+    if (options.id) {
+      return this.pelis.getById(options.id).then((r) => r);
+    } else if (options.search.title || options.search.tag) {
+      return this.pelis.search(options.search).then((r) => r);
+    } else {
+      return this.pelis.getAll();
+    }
+  }
+
+  add(peli: Peli) {
+    const promesa = this.pelis.add(peli);
+    return promesa;
+  }
+  async getAll() {
+    const promesa = await this.pelis.getAll();
+    return promesa;
+  }
 }
 export { PelisController };
--- models.ts
+++ models.ts
@@ -8,11 +8,77 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  //getAll lee y retorna lo que esta en el archivo JSON
+  async getAll(): Promise<Peli[]> {
+    const promesa = await jsonfile.readFile("./pelis.json").then((data) => {
+      return data;
     });
+
+    // la respuesta de la promesa -- Estoy usando async y Await, se resuelve la promesa y se retorna
+    return promesa;
+  }
+
+  //Método de búsqueda de pelis por ID
+  async getById(id: number) {
+    const todasLasPelis = await this.getAll();
+
+    const encontrado = todasLasPelis.find((pelis) => {
+      return pelis.id == id;
+    });
+    return encontrado;
+  }
+
+  //Método de busqueda de pelis, por titulo, tag o ambos al mismo tiempo
+  async search(options: any) {
+    const todasLasPelis = await this.getAll();
+    //opcion de búsqueda por titulo y tag
+    if (options.title && options.tag) {
+      const filtredByTag = todasLasPelis.filter((pelis) => {
+        return (
+          pelis.title.includes(options.title) &&
+          pelis.tags.includes(options.tag)
+        );
+      });
+      return filtredByTag;
+    }
+    //opción de búsqueda por título
+    else if (options.title) {
+      const filtradoPorTitulo = todasLasPelis.filter((pelis) => {
+        return pelis.title.includes(options.title);
+      });
+      return filtradoPorTitulo;
+      //opción de búsqueda por tag
+    } else if (options.tag) {
+      const filtradoPorTag = todasLasPelis.filter((pelis) => {
+        return pelis.tags.includes(options.tag);
+      });
+      return filtradoPorTag;
+      //Ante cualquier inconveniente se retorna getAll()
+    } else {
+      return this.getAll();
+    }
+  }
+  async add(peli: Peli) {
+    //Este método añade películas al archivo JSON, en caso de repetirse un ID, retorna un mensaje - el objeto que quisiste añadir - y no cambia ni añade nada al archivo JSON
+
+    const todasLasPelis = await this.getAll();
+    const peliNueva = peli;
+    const existeUnaPeliConElId = await this.getById(peli.id).then((p) => p); //Si no llegase a existir una peli con el ID indicado. Se añade, en caso contrario retorna el mensaje después del if
+
+    if (existeUnaPeliConElId) {
+      return [peli, "ya existe y por ende no puede ser añadido"];
+    } else {
+      todasLasPelis.push(peliNueva);
+      const promesa = await jsonfile
+        .writeFile("./pelis.json", todasLasPelis)
+        .then(() => {
+          return peli;
+        });
+      return promesa;
+    }
+
+    //Al finalizar se reescribe el archivo json, y se le agrega la peliNueva
   }
 }
+
 export { PelisCollection, Peli };
