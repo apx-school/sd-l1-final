bae616c13509c3b3d4a66e7df8f7ba5fd9915a09
adriel <adriel.m.marchesini@gmail.com>
2022-02-02 16:41:18 -0300

    avances en models

--- models.ts
+++ models.ts
@@ -8,11 +8,41 @@ class Peli {
 }
 
 class PelisCollection {
+  colecciondepeliculas: Peli;
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
+    return jsonfile("/pelis.json").then((peliculas) => {
       // la respuesta de la promesa
-      return [];
+      return [peliculas];
+    });
+  }
+  getById(id: number): Promise<any> {
+    return this.getAll().then((listaPelis) => {
+      const devolucion = listaPelis.find((peliConId) => {
+        return peliConId.id == id;
+      });
+      return devolucion;
+    });
+  }
+  search(options: any): Promise<any> {
+    return this.getAll().then((listaPelis) => {
+      if (options.title && options.tag) {
+        return listaPelis.filter((peli) => {
+          return (
+            peli.title.includes(options.title) &&
+            peli.tags.includes(options.tag)
+          );
+        });
+      } else if (options.title) {
+        return listaPelis.filter((peli) => peli.title.includes(options.title));
+      } else if (options.tag) {
+        return listaPelis.filter((peli) => peli.tags.includes(options.tag));
+      }
     });
   }
 }
+
+const coleccionPeliculas = new PelisCollection();
+coleccionPeliculas.getAll().then((resultado) => {
+  console.log(resultado);
+});
 export { PelisCollection, Peli };
