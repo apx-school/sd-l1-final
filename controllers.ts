60faad23a9751e7fa8198bbfcfd1996acf0adea0
jauday <joaquin.auday@gmail.com>
2022-02-04 20:57:47 -0300

    actualizacion

--- controllers.ts
+++ controllers.ts
@@ -1,24 +1,74 @@
+// import { PelisCollection, Peli } from "./models";
+
+// class PelisController {
+//   peliculas: PelisCollection;
+//   constructor() {
+//     this.peliculas = new PelisCollection();
+//   }
+//   async get(options: any): Promise<any> {
+// 		if (options.id) {
+// 			return await this.peliculas.getById(options.id);
+// 		}
+// 		if (options.search) {
+// 			return await this.peliculas.search(options.search);
+// 		}
+// 		if (options.empty) {
+// 			return await this.peliculas.getAll();
+// 		}
+// 	}
+// 	add(peli: Peli): Promise<boolean> {
+// 		return this.peliculas.add(peli);
+// 	}
+
+// }
+// export { PelisController };
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  peliculas: PelisCollection;
-  constructor() {
-    this.peliculas = new PelisCollection();
-  }
-  async get(options: any): Promise<any> {
-		if (options.id) {
-			return await this.peliculas.getById(options.id);
-		}
-		if (options.search) {
-			return await this.peliculas.search(options.search);
-		}
-		if (options.empty) {
-			return await this.peliculas.getAll();
-		}
-	}
-	add(peli: Peli): Promise<boolean> {
-		return this.peliculas.add(peli);
-	}
+  listaPelis: PelisCollection;
 
+  constructor() {
+    this.listaPelis = new PelisCollection();
+  }
+
+  get(options) {
+    //si la opcion id existe entra
+    if (options.id) {
+      return this.listaPelis.getById(options.id);
+    }
+    //si existe, title o tag, o incluso ambas entra
+    /* if (options.search.title && options.search.tag) {
+      var soloTag = { tag: options.search.tag };
+      return this.listaPelis.search(soloTag).then((peliculas) => {
+        return peliculas.filter((peli) => {
+          //si alguna de las pelis incluye options.title la devuelvo
+          if (peli.title.includes(options.search.title)) {
+            return peli;
+          }
+        });
+      });
+      //solo titulo
+    }*/
+
+    if (options.search.title) {
+      return this.listaPelis.search(options.search);
+      //solo tag
+    }
+    if (options.search.tag) {
+      return this.listaPelis.search(options.search);
+    }
+    if (options.search) {
+      return this.listaPelis.search(options.search);
+    }
+    //si no hay ni id, title o tag, entonce retorna todas las pelis
+    if (!(options.id || options.title || options.tags)) {
+      return this.listaPelis.getAll();
+    }
+  }
+
+  add(peli: Peli) {
+    return this.listaPelis.add(peli);
+  }
 }
+
 export { PelisController };
