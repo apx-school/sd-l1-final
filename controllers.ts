8d32fdbcaeaea5338077d00b1f2fc492bf4aedbf
unknown <juankliabaras@gmail.com>
2022-02-04 21:20:03 -0300

    Desafio completado con los 10 test pasados correctamente en mi pc

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,21 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  modelo: PelisCollection;
+  constructor() {
+    this.modelo = new PelisCollection();
+  }
+  get(options: any): Promise<any> {
+    if (options.id) {
+      return this.modelo.getById(options.id);
+    } else if (options.search && (options.search.title || options.search.tag)) {
+      return this.modelo.search(options.search);
+    } else {
+      return this.modelo.getAll();
+    }
+  }
+  add(peli: Peli) {
+    return this.modelo.add(peli);
+  }
 }
 export { PelisController };
