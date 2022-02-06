cc60d2843363cb126bb298f2a9b81cb3be8bdd73
Santiago <san_chan97@hotmail.com>
2022-02-06 14:18:36 -0300

    update models.ts

--- models.ts
+++ models.ts
@@ -1,4 +1,5 @@
 import * as jsonfile from "jsonfile";
+import { pull } from "lodash";
 
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
@@ -8,11 +9,63 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  async getAll(): Promise<Peli[]> {
+    var answer = await jsonfile.readFile("./pelis.json");
+    return answer;
+  }
+  async getById(id: number): Promise<Peli> {
+    var answer = await this.getAll().then((pelis) => {
+      const peliEncontrada = pelis.find((x) => {
+        return x.id == id;
+      });
+      return peliEncontrada;
     });
+    return answer;
+  }
+  async search(options: any) {
+    if (options.title && !options.tag) {
+      return await this.getAll().then((pelis) => {
+        const pelisFiltradasTitle = pelis.filter((x) =>
+          x.title.includes(options.title)
+        );
+        return pelisFiltradasTitle;
+      });
+    } else if (options.tag && !options.title) {
+      return await this.getAll().then((pelis) => {
+        const pelisFiltradasTags = pelis.filter((peli) => {
+          return peli.tags.includes(options.tag);
+        });
+        return pelisFiltradasTags;
+      });
+    } else if (options.title && options.tag) {
+      const filtroTitle = await this.getAll().then((pelis) => {
+        const pelisFiltradasTitle = pelis.filter((x) => {
+          return x.title.includes(options.title);
+        });
+        return pelisFiltradasTitle;
+      });
+      const filtroTags = filtroTitle.filter((peli) => {
+        return peli.tags.includes(options.tag);
+      });
+      return filtroTags;
+    }
+  }
+  async add(peli: Peli): Promise<boolean> {
+    if (
+      await this.getById(peli.id).then((res) => {
+        return res;
+      })
+    ) {
+      return false;
+    } else {
+      const misPelis = await this.getAll().then((res) => {
+        return res;
+      });
+      misPelis.push(peli);
+      await jsonfile.writeFile("./pelis.json", misPelis);
+      return true;
+    }
   }
 }
+
 export { PelisCollection, Peli };
