3a994f9415470cee261bcd6c87ee6e685e43333a
{WalterMilanesi} <{waltermilanesi@hotmail.com}>
2022-02-06 09:30:34 +0200

    Commit con todos los cambios necesarios para pasar los test de Controller y Models. Archivo listo para presentar

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,28 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  movies: PelisCollection;
+  constructor() {
+    this.movies = new PelisCollection();
+  }
+
+  async get(options: any): Promise<any> {
+    if (options.id) {
+      var peliEncontrada = await this.movies.getById(options.id);
+      return peliEncontrada;
+    }
+    if (options.search) {
+      var resultadoBusqueda = await this.movies.search(options.search);
+      return resultadoBusqueda;
+    }
+    if ({}) {
+      return await this.movies.getAll();
+    }
+  }
+
+  async add(peli: Peli) {
+    return await this.movies.add(peli);
+  }
 }
-export { PelisController };
+
+export { PelisController }
