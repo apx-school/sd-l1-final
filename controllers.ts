e4791cf620971bbee78218983bf60cf4003e438f
unknown <bissonilaura@gmail.com>
2022-02-13 13:04:16 -0300

    mas commits

--- controllers.ts
+++ controllers.ts
@@ -10,8 +10,8 @@ Pelis: PelisCollection;
  async get(options):Promise <any>{
   
    if (options.id){ return await this.Pelis.getById(options.id)}
-   if (options.search){ return await this.Pelis.search(options.search)}
-   if (options.empty){return await this.Pelis.getAll()}
+  else if (options.search){ return await this.Pelis.search(options.search)}
+  else if (options.empty){return await this.Pelis.getAll()}
  }
  async add(peli:Peli){ return await this.Pelis.add(peli)}
  }
