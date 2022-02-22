3c878ea44f8f7f591226953a8a5fefb50e461126
unknown <bissolaura@gmail.com>
2022-02-22 15:10:28 -0300

    ootros commits

--- index.ts
+++ index.ts
@@ -1,42 +1,41 @@
 import * as minimist from "minimist";
-
-import  { PelisController } from "./controllers"
+import * as _ from "lodash";
+import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
   var resultado = minimist(argv);
   return resultado;
 }
 
-async function processOptions(args){
-  const controller = new PelisController ()
-  if (args.add){
-    return await controller.add({
-      id:args.id,
-      title:args.title,
-      tags:args.tags
-    })
-  .then((res)=> {return res})
+async function processOptions(args) {
+  const controller = new PelisController();
+  if (args._[0] == "add") {
+    return await controller
+      .add({
+        id: args.id,
+        title: args.title,
+        tags: args.tags,
+      })
+      .then((res) => {
+        return res;
+      });
+  } else if (args._[0] == "get") {
+    return await controller.get({ id: args._[1] });
+  } else if (args._[0] == "search") {
+    return await controller.get({
+      search: { title: args.title, tag: args.tag },
+    });
+  } else if (_.empty) {
+    return await controller.get({ empty: "empty" });
   }
- else if (args.get){
-    return await controller.get({id:13})
-  }
- else if (args.search){
-    return await controller.get({search:args.title, tag:args.tag})
-  }
-else  if (args.empty){
-    return await controller.get({empty: "empty"} )
-
-  }
-
 }
- function main() {
- 
+function main() {
   const params = parseaParams(process.argv.slice(2));
- console.log(params)
-  return processOptions(params).then((res)=> {
-    return console.log(res)
-  })
- 
+  console.log(params);
+  return processOptions(params).then((res) => {
+    console.log(res);
+    return res;
+  });
 }
 
 main();
--- models.ts
+++ models.ts
@@ -9,67 +9,63 @@ class Peli {
 
 class PelisCollection {
   async getAll(): Promise<Peli[]> {
-    const promesa = await jsonfile.readFile("./pelis.json")
-      // la respuesta de la promesa
-  return promesa
+    const promesa = await jsonfile.readFile("./pelis.json");
+    // la respuesta de la promesa
+    return promesa;
   }
-  async getById(id:number){
-    const resultado = await this.getAll()
-    return resultado.find((peli)=>{ return peli.id == id})
-  }
-
-  async search(options:any){
-    var result = await this.getAll();
-     if (options.title && options.tags) {  
-     var filter = result.filter((peli)=>{
-       return peli.title.includes(options.title)&&
-        peli.tags.includes(options.tags)
-     })
-     return result = filter
-    }
-    else if (options.title){
-      var filterTitle = result.filter((peli)=>{ 
-        return peli.title.includes(options.title)})
-        return result = filterTitle
-    }
-    else if (options.tags){
-       var filterTags = result.filter((peli)=>{
-         return peli.tags.includes(options.tags)})
-      return result = filterTags
-       }
-
-     return result
-  }
-
-    add(peli: Peli): Promise<boolean> {
-    return this.getAll().then((json)=>{
-    const promesaUno = this.getById(peli.id).then((peliExistente) => {
-      if (peliExistente) {
-        return false;
-      } else {
-        // magia que agrega la pelicula a un objeto data
-        const data = json
-        data.push({id: peli.id,
-          title: peli.title,
-          tags: peli.tags})
-        const promesaDos = jsonfile.writeFile("./pelis.json", data);
- 
-        return promesaDos.then(() => {
-          return true;
-        });
-      }
+  async getById(id: number) {
+    const resultado = await this.getAll();
+    return resultado.find((peli) => {
+      return peli.id == id;
     });
-    return promesaUno;
-  })
-}
+  }
+
+  async search(options: any) {
+    var result = await this.getAll();
+    if (options.title && options.tags) {
+      var filter = result.filter((peli) => {
+        return (
+          peli.title.includes(options.title) && peli.tags.includes(options.tags)
+        );
+      });
+      return (result = filter);
+    } else if (options.title) {
+      var filterTitle = result.filter((peli) => {
+        return peli.title.includes(options.title);
+      });
+      return (result = filterTitle);
+    } else if (options.tags) {
+      var filterTags = result.filter((peli) => {
+        return peli.tags.includes(options.tags);
+      });
+      return (result = filterTags);
+    }
+
+    return result;
+  }
+
+  add(peli: Peli): Promise<boolean> {
+    return this.getAll().then((json) => {
+      const promesaUno = this.getById(peli.id).then((peliExistente) => {
+        if (peliExistente) {
+          return false;
+        } else {
+          // magia que agrega la pelicula a un objeto data
+          const data = json;
+          data.push({ id: peli.id, title: peli.title, tags: peli.tags });
+          const promesaDos = jsonfile.writeFile("./pelis.json", data);
+
+          return promesaDos.then(() => {
+            return true;
+          });
+        }
+      });
+      return promesaUno;
+    });
+  }
 }
 
-  
-
-// const pelis = new PelisCollection
-// pelis.add({
-//   id: 90,
-//   title: "La cueva",
-//   tags:[ "Suspenso"]}).then((resultado)=> console.log(resultado))
+// );const pelis = new PelisCollection();
+// pelis.search({ tags: "drama" }).then((resultado) => console.log(resultado)
 
 export { PelisCollection, Peli };
