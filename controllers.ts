e68130053ed420ad00d898b6ae7f654948c2e019
unknown <bissolaura@gmail.com>
2022-02-23 12:26:23 -0300

    mas commits

--- controllers.ts
+++ controllers.ts
@@ -1,20 +1,23 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-
-Pelis: PelisCollection;
+  Pelis: PelisCollection;
 
   constructor() {
-    this.Pelis = new PelisCollection()
+    this.Pelis = new PelisCollection();
   }
- async get(options):Promise <any>{
-  
-   if (options.id){ return await this.Pelis.getById(options.id)}
-  else if (options.search){ return await this.Pelis.search(options.search)}
-  else if (options.empty){return await this.Pelis.getAll()}
- }
- async add(peli:Peli){ return await this.Pelis.add(peli)}
- }
-
+  async get(options): Promise<any> {
+    if (options.id) {
+      return await this.Pelis.getById(options.id);
+    } else if (options.search) {
+      return await this.Pelis.search(options.search);
+    } else if (options.empty) {
+      return await this.Pelis.getAll();
+    }
+  }
+  add(peli: Peli) {
+    return this.Pelis.add(peli);
+  }
+}
 
 export { PelisController };
--- index.ts
+++ index.ts
@@ -6,7 +6,6 @@ function parseaParams(argv) {
   var resultado = minimist(argv);
   return resultado;
 }
-
 async function processOptions(args) {
   const controller = new PelisController();
   if (args._[0] == "add") {
@@ -19,14 +18,17 @@ async function processOptions(args) {
       .then((res) => {
         return res;
       });
-  } else if (args._[0] == "get") {
-    return await controller.get({ id: args._[1] });
-  } else if (args._[0] == "search") {
-    return await controller.get({
+  }
+  if (args._[0] == "get") {
+    return controller.get({ id: args._[1] });
+  }
+  if (args._[0] == "search") {
+    return controller.get({
       search: { title: args.title, tag: args.tag },
     });
-  } else if (_.empty) {
-    return await controller.get({ empty: "empty" });
+  }
+  if (_.isEmpty(args._[0])) {
+    return controller.get({ empty: "empty" });
   }
 }
 function main() {
