b1b32d6ead28c275c971b1470adb395125e4d246
ayrton juarez <ayrtonjuarez90@gmail.com>
2022-02-27 18:35:41 -0300

    desafio modulo 2 completado

--- models.ts
+++ models.ts
@@ -1,18 +1,64 @@
-import * as jsonfile from "jsonfile";
+import * as js from "jsonfile";
 
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
-  id: number;
-  title: string;
-  tags: string[];
+    id: number;
+    title: string;
+    tags: string[];
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
-    });
-  }
+    async getAll(): Promise<Peli[]> {
+        const movies = await js.readFile("./pelis.json");
+        return movies;
+    }
+
+    async getById(id: number): Promise<Peli> {
+        const movies = await this.getAll();
+        const result = movies.find((movies) => {
+            return movies.id == id;
+        });
+        return result;
+    }
+
+    async search(options: any): Promise<Peli[]> {
+        const movies = await this.getAll();
+
+        if (!!options.title) {
+            const result = movies.filter((movies) => {
+                return movies.title.includes(options.title);
+            });
+            return result;
+        } else if (!!options.tags) {
+            const result = movies.filter((movies) => {
+                return movies.tags.includes(options.tag);
+            });
+            return result;
+        } else if (options.title && options.tag) {
+            const result = movies.filter((movies) => {
+                return movies.tags.includes(options.tag, options.title);
+            });
+            return result;
+        }
+    }
+    async add(movie: Peli): Promise<boolean> {
+        const promiseOne = this.getById(movie.id).then(
+            async (peliExistente) => {
+                if (peliExistente) {
+                    return false;
+                } else {
+                    const movies = await this.getAll();
+                    movies.push(movie);
+                    const promiseTwo = js.writeFile("./pelis.json", movies);
+
+                    return promiseTwo.then(() => {
+                        return true;
+                    });
+                }
+            }
+        );
+        return promiseOne;
+    }
 }
+
 export { PelisCollection, Peli };
