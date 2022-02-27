8b76776f28c8c124b1bfb4938984131f73b1ea66
Jimena <jimenafernandez.business@gmail.com>
2022-02-27 03:57:49 -0300

    clases creadas en controllers.ts, models.ts mejorado, argumentos parseados. Todos los test pasan.

--- index.ts
+++ index.ts
@@ -1,15 +1,42 @@
 import * as minimist from "minimist";
+import { PelisController } from "./controllers"
+import * as isEmpty from "lodash"
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
 
-  return resultado;
+  return resultado
+}
+
+async function processOptions(opt) {
+  const controller = new PelisController()
+  if (opt["_"][0] == "add") {
+    return await controller.add({
+      id: opt.id,
+      title: opt.title,
+      tags: opt.tags
+    }).then((res) => { return res })
+    
+  } else if (opt["_"][0] == "get") {
+    return await controller.get({id: opt.id})
+  
+  } else if (opt["_"][0] == "search") {
+    return await controller.get({
+      search: { title: opt.title, tag: opt.tag }
+    }).then((res) => { return res })
+  } else if(isEmpty(opt)){
+    return await controller.get(isEmpty())
+  }
+  
+  
 }
 
 function main() {
-  const params = parseaParams(process.argv.slice(2));
-
-  console.log(params);
+  const comandos = parseaParams(process.argv.slice(2));
+  return processOptions(comandos).then((res) => {
+    console.log(res)
+    return res
+  })
 }
 
 main();
--- models.ts
+++ models.ts
@@ -8,37 +8,39 @@ class Peli {
 }
 
 class PelisCollection {
-  datos: Peli[]
-  constructor(){}
+
   getAll(): Promise<Peli[]> {
-    return jsonfile.readFile("./pelis.json").then((peliculas) => {
+    return jsonfile.readFile("./pelis.json").then(async (peliculas) => {
       // la respuesta de la promesa
-      return peliculas;
+      return await peliculas;
     });
   }
-  getById(id: number) {
-    return this.getAll().then((peliculas) => {
+  async getById(id: number): Promise<Peli> {
+    return await this.getAll().then((peliculas) => {
       const res = peliculas.find(p => {return p.id === id})
       return res
     })
   }
-  search(options: any) {
-    var newProp = Object.getOwnPropertyNames(options)[0].toLowerCase()
-    var value = Object.values(options)[0]
-    options = { [newProp]: value }
-    
-    if (options.title) {
-      return this.getAll().then((peliculas) => {
+  async search(options: any): Promise<Peli[]> {
+
+    if (options.title && options.tag) {
+      return await this.getAll().then((peliculas) => {
+        const res = peliculas.filter(p => {
+          return p.title.includes(options.title) && p.tags.includes(options.tag)
+        })
+        return res
+      })
+    } else if (options.title) {
+      return await this.getAll().then((peliculas) => {
         const res = peliculas.filter(p => {
           return p.title.includes(options.title)
         })
         return res
       })
     } else if (options.tag) {
-      console.log("chau")
-      return this.getAll().then((peliculas) => {
+      return await this.getAll().then((peliculas) => {
         const res = peliculas.filter(p => {
-        return p.tags.includes(options.tag.toLowerCase())
+        return p.tags.includes(options.tag)
       })
         return res
       })
@@ -47,15 +49,16 @@ class PelisCollection {
     }
   }
   add(peli: Peli): Promise<boolean> {
-    const promesaUno = this.getById(peli.id).then((idExiste) => {
+    const promesaUno = this.getById(peli.id).then(async (idExiste) => {
       if (idExiste) {
         return false
       } else {
-        this.getAll().then((peliculas) => {
+        await this.getAll().then((peliculas) => {
           peliculas.push(peli)
-          jsonfile.writeFile("./pelis.json", peliculas)
+          const promesaDos = jsonfile.writeFile("./pelis.json", peliculas)
+
+          return promesaDos.then(()=>{ return true})
         })
-        return true
       }
     });
     return promesaUno
