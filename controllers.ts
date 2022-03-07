b6a2fd4a2ac58bbfb1933911444271f7c779f80e
CelesBM <90277599+CelesBM@users.noreply.github.com>
2022-03-07 18:26:39 -0300

    Update controllers.ts

--- controllers.ts
+++ controllers.ts
@@ -11,7 +11,6 @@ get(options: any): Promise<any> {
 
  if(options.id){
     return this.pelisCollection.getById(options.id);
-
  } else if (options.search){
     if(options.search.title && options.search.tag){
        return this.pelisCollection.search({
@@ -27,17 +26,12 @@ get(options: any): Promise<any> {
           tag: options.search.tag
        })
    }
-
  } else {
     return this.pelisCollection.getAll();
  }
 }
-
 add(peli: Peli){
-
     return this.pelisCollection.add(peli);
 }
-
-
   }
 export { PelisController };
