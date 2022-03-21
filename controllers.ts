ba83285892266d6dae6c3631cfac080e9ce0b404
Rodrigo Salinas <ras123gg@gmail.com>
2022-03-21 17:09:30 -0300

    desafio terminado

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,27 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  peliculas:PelisCollection ;
+
+  constructor() {
+   this.peliculas = new PelisCollection(); 
+  }
+
+  async get(options:any):Promise<any>{
+    if (options.id){
+      return await this.peliculas.getById(options.id);
+    }
+    if (options.search.title || options.search.tag){
+      return await this.peliculas.search(options.search);
+    }
+    if(options.search){
+      return await this.peliculas.getAll();
+    }
+  }
+  
+  add(peli:Peli){
+    return  this.peliculas.add(peli);
+  }
 }
 export { PelisController };
+
