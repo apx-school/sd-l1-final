755079da0cfa81981600a6da8d0d9c182957a843
kbrol126 <nahu_sank06@hotmail.com>
2022-03-28 16:31:38 -0300

    modificando el models

--- models.ts
+++ models.ts
@@ -9,10 +9,49 @@ class Peli {
 
 class PelisCollection {
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    const archivo = "./pelis.json";
+    return jsonfile.readFile(archivo).then((respuesta) => {
+      return respuesta;
     });
+  }
+  getById(id: number): Promise<Peli> {
+    const respuesta = this.getAll().then((pelis) => {
+      const resultado = pelis.find((peli) => {
+        return peli.id == id;
+      });
+      return resultado;
+    });
+    return respuesta;
+  }
+  search(options: any): Promise<any> {
+    if (options.title) {
+      const PelisFiltradas = this.getAll().then((arrayDePelis) => {
+        const filtro = arrayDePelis.filter((peli) => {
+          const title = peli.title.toLowerCase();
+          return title.includes(options.title);
+        });
+        return filtro;
+      });
+      return PelisFiltradas;
+    }
+    if (options.tags) {
+      const PelisFiltradas = this.getAll().then((arrayDePelis) => {
+        const filtro = arrayDePelis.filter((peli) => {
+          const textoLowerCase = options.tags.toLowerCase();
+          const tagsLowerCase = peli.tags.map((tag) => tag.toLocaleLowerCase());
+          return tagsLowerCase.includes(textoLowerCase);
+        });
+
+        return filtro;
+      });
+      return PelisFiltradas;
+    }
   }
 }
 export { PelisCollection, Peli };
+
+function main() {
+  const nuevo = new PelisCollection();
+  nuevo.search({ title: "Z" }).then((resultado) => console.log(resultado));
+}
+main();
