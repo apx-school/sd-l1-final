9825c6ab715cf2f0ccd641f33c81a743510ce2b6
frpulido <franciscopulidocal@gmail.com>
2022-03-31 00:38:30 -0300

    primer commit

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,23 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  peliculas: PelisCollection;
+  constructor() {
+    this.peliculas = new PelisCollection();
+  }
+
+  async get(options: any): Promise<any> {
+    if (options.id) {
+      return await this.peliculas.getById(options.id);
+    } else if (options.search) {
+      return await this.peliculas.search(options.search);
+    } else {
+      return await this.peliculas.getAll();
+    }
+  }
+
+  add(peli: Peli) {
+    return this.peliculas.add(peli);
+  }
 }
 export { PelisController };
