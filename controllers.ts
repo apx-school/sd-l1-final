f754b985c1d9ee8f9e5de06c2079f35ab88cacac
Gabriel González <mgabrieltbf@gmail.com>
2022-04-17 15:02:39 -0300

    Tests ok

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,28 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  __listaPeliculas : PelisCollection = new PelisCollection;
+
+  constructor() {
+
+  }
+
+  async get(options:any) : Promise<any> {
+    if(options["id"]){
+      return this.__listaPeliculas.getById(options["id"]);
+    }
+
+    if(options["search"]){
+      if(options["search"]["title"] || options["search"]["tag"]){
+        return this.__listaPeliculas.search(options["search"]);
+      }
+    }
+
+    return this.__listaPeliculas.getAll();
+  }
+
+  async add(peli:Peli) : Promise<boolean> {
+    return this.__listaPeliculas.add(peli);
+  }
 }
 export { PelisController };
--- models.ts
+++ models.ts
@@ -1,5 +1,7 @@
 import * as jsonfile from "jsonfile";
 
+const directorioPeliculas = __dirname + "/pelis.json";
+
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
   id: number;
@@ -8,11 +10,63 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
+  __listaPeliculas : Peli[] = [];
+
+  async getAll(): Promise<Peli[]> {
+    return jsonfile.readFile(directorioPeliculas).then((respuesta : Peli[]) => {
       // la respuesta de la promesa
-      return [];
+      this.__listaPeliculas = respuesta;
+
+      return respuesta;
+    });
+  }
+
+  async getById(id:number) : Promise<Peli>{
+    return jsonfile.readFile(directorioPeliculas).then((respuesta : Peli[]) => {
+      // la respuesta de la promesa
+      this.__listaPeliculas = respuesta;
+
+      return this.__listaPeliculas.find((item:Peli) => {
+        return item.id == id;
+      });
+    });
+  }
+
+  async search(options:any) : Promise<Peli[]>{
+    return jsonfile.readFile(directorioPeliculas).then((respuesta : Peli[]) => {
+      // la respuesta de la promesa
+      this.__listaPeliculas = respuesta;
+
+      if(options["title"] != undefined){
+        respuesta = respuesta.filter((item) => {
+          return item.title.indexOf(options["title"]) != -1;
+        });
+      };
+      
+      if(options["tag"] != undefined){
+        respuesta = respuesta.filter((item) => {
+          return item.tags.indexOf(options["tag"]) != -1;
+        });
+      };
+
+      return respuesta;
+    });
+  }
+
+  async add(peli:Peli) : Promise<boolean>{
+    return this.getById(peli.id).then((peliExistente) => {
+      if(peliExistente){
+        return false;
+      }
+      else{
+        this.__listaPeliculas.push(peli);
+
+        return jsonfile.writeFile(directorioPeliculas, this.__listaPeliculas, {spaces: 2}).then(() => {
+          return true
+        });
+      };
     });
   }
 }
+
 export { PelisCollection, Peli };
