28d493a0d2165277db9be360f31e456e3529861f
Veronica Isabel Pereira <47119996+Alfombrasvoladoras@users.noreply.github.com>
2022-04-18 09:01:29 -0300

    actualizacion_01

--- models.ts
+++ models.ts
@@ -39,7 +39,6 @@ class PelisCollection {
         return tagsDePelisEnLowerCase.includes(options.tag);
       });
     } else {
-      console.log("Ese comando no existe, las peliculas son las siguientes:");
       return pelis;
     }
   }
@@ -47,15 +46,11 @@ class PelisCollection {
   async add(peli: Peli): Promise<Boolean> {
     const idPeliExiste = await this.getById(peli.id);
     if (idPeliExiste) {
-      console.log(
-        "No se admiten agregar películas con un Id que ya existe en la base de datos"
-      );
       return false;
     }
     const pelis = await this.getAll();
     pelis.push(peli);
     await jsonfile.writeFile("./pelis.json", pelis);
-    console.log("La pelicula:", peli, "se agrego correctamente");
     return true;
   }
 }
