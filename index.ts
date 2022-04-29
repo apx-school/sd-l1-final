5344195c9b93b80b838ddeccaa6fcd9c60ed89c9
NahuelAlfaro <98058039+NahuelAlfaro@users.noreply.github.com>
2022-04-28 23:04:32 -0300

    Desafio final

--- index.ts
+++ index.ts
@@ -1,15 +1,33 @@
 import * as minimist from "minimist";
+import {PelisController} from "./controllers"
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
+  const controller = new PelisController()
+  const infoDeTerminal = resultado._
+  const params = infoDeTerminal[0]
+  if (params === "get"){
+    return controller.get({id: infoDeTerminal[1]})
+  }
+  else if (params === "search"){
+    return controller.get({search: resultado})
+  }
+  else if (params === "add"){
+    return controller.add({
+      "title":infoDeTerminal.title,
+      "id": infoDeTerminal.id,
+      "tags":infoDeTerminal.tag
+    })
+    
+  }
 
-  return resultado;
+  else return controller.get({})
 }
 
-function main() {
-  const params = parseaParams(process.argv.slice(2));
-
-  console.log(params);
+async function main() {
+  const params = (process.argv.slice(2));
+  const resultado = await parseaParams(params)
+  console.log(resultado);
 }
 
 main();
