8742c1a77c05a498785b4282f6cf3a49e3ace76b
imprenart <imprenart@gmail.com>
2022-05-01 13:43:12 -0300

    Desafío -Primer commit

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,28 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  peliculas: PelisCollection;
+  constructor() {
+    this.peliculas = new PelisCollection();
+  }
+
+  async get(options: any) {
+    if (options.id) {
+      return await this.peliculas.getById(options.id);
+    }
+    if (options.search) {
+      return await this.peliculas.search(options.search);
+    }
+    if (options.search) {
+      return await this.peliculas.getAll();
+    }
+  }
+
+  async add(peli: Peli) {
+    return await this.peliculas.add(peli);
+  }
 }
 export { PelisController };
+
+const peli = new PelisController();
+peli.get(5).then((r) => console.log(r));
--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
 import * as minimist from "minimist";
+import * as _ from "lodash";
+
+import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
@@ -6,10 +9,26 @@ function parseaParams(argv) {
   return resultado;
 }
 
-function main() {
-  const params = parseaParams(process.argv.slice(2));
+async function processOptions(args) {
+  const pelis = new PelisController();
+  if (args._[0] === "get") {
+    return pelis.get({ id: args._[1] });
+  }
+  if (args._[0] === "search") {
+    return pelis.get({ search: args });
+  }
+  if (args._[0] === "add") {
+    return pelis.add({ id: args.id, title: args.title, tags: args.tags });
+  }
+  if (!args._[0]) {
+    return pelis.peliculas.getAll();
+  }
+}
 
-  console.log(params);
+function main() {
+  const argumentos = process.argv.slice(2);
+  const parseados = parseaParams(argumentos);
+  processOptions(parseados).then((r) => console.log(r));
 }
 
 main();
