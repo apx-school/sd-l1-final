9e44d59c0879e2e7a1e6667f1603ecf0f9daa2dc
gabriel carreño <carrenocarlosgabriel@gmail.com>
2022-05-02 14:32:01 -0300

    challenge finished

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,21 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  listFilms: PelisCollection;
+  constructor() {
+    this.listFilms = new PelisCollection();
+  }
+  async get(options) {
+    if (options.id) {
+      return await this.listFilms.getById(options.id);
+    } else if (options.search) {
+      return await this.listFilms.search(options.search);
+    } else {
+      return await this.listFilms.getAll();
+    }
+  }
+  add(films: Peli) {
+    return this.listFilms.add(films);
+  }
 }
 export { PelisController };
--- index.ts
+++ index.ts
@@ -1,15 +1,38 @@
 import * as minimist from "minimist";
+import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
-  const resultado = minimist(argv);
+  const result = minimist(argv);
+  //destructuring of the object by the terminal
+  const { _: optionByTerminal, ...rest } = result;
 
-  return resultado;
+  if (optionByTerminal.includes("get")) {
+    return { id: optionByTerminal[1] };
+  } else if (optionByTerminal.includes("add")) {
+    return rest;
+  } else if (optionByTerminal.includes("search")) {
+    return { search: rest };
+  } else {
+    return "index.ts";
+  }
 }
 
-function main() {
+const result = async () => {
+  const pelisController = new PelisController();
   const params = parseaParams(process.argv.slice(2));
+  let result;
+  if (params == "index.ts") {
+    result = await pelisController.listFilms.getAll();
+  } else if (params.id && params.title && !params.search) {
+    result = await pelisController.add(params);
+  } else if (params.id || params.search) {
+    result = await pelisController.get(params);
+  }
+  console.log(result);
+};
 
-  console.log(params);
+function main() {
+  result();
 }
 
 main();
