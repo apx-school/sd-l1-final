b7f8267195427fbfc4e77f25d1535c6252d2378d
CrisArevalo <cris.are.3496@gmail.com>
2022-05-02 17:00:12 -0300

    Commit PR

--- models.ts
+++ models.ts
@@ -2,17 +2,76 @@ import * as jsonfile from "jsonfile";
 
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
-  id: number;
-  title: string;
-  tags: string[];
+    id: number;
+    title: string;
+    tags: string[];
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
-    });
-  }
+    datos: Peli[] = [];
+    getAll(): Promise<Peli[]> {
+        return jsonfile.readFile("./pelis.json").then((t) => t);
+    }
+
+    //Funciona
+    getById(id: number): Promise<any> {
+        return this.getAll().then((films) =>
+            films.find((film) => film.id == id)
+        );
+    }
+
+    //Funciona
+    add(peli: Peli): Promise<boolean> {
+        const promesaUno = this.getById(peli.id).then((peliExistente) => {
+            if (peliExistente) {
+                return false;
+            } else {
+                // magia que agrega la pelicula a un objeto data
+                const promesaDos = this.getAll().then((films) => {
+                    const data = films.concat(peli);
+                    return jsonfile.writeFile("./pelis.json", data);
+                });
+
+                return promesaDos.then(() => {
+                    return true;
+                });
+            }
+        });
+
+        return promesaUno;
+    }
+
+    //Funciona
+    search(options: any): Promise<any> {
+        const promise = this.getAll().then((films) => {
+            if (
+                options.hasOwnProperty("title") &&
+                options.hasOwnProperty("tag")
+            ) {
+                return films.filter(
+                    (film) =>
+                        film.title.includes(options.title) &&
+                        film.tags.includes(options.tag.toLowerCase())
+                );
+            }
+            if (options.hasOwnProperty("title")) {
+                return films.filter((film) =>
+                    film.title.includes(options.title)
+                );
+            }
+            if (options.hasOwnProperty("tag")) {
+                return films.filter((film) => {
+                    let tagIdentico = film.tags.includes(
+                        options.tag.toLowerCase()
+                    );
+                    if (tagIdentico) {
+                        return film;
+                    }
+                });
+            }
+        });
+        return promise;
+    }
 }
+
 export { PelisCollection, Peli };
