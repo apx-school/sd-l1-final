b16f298ad19c37488c42860f1cad52516532eb9f
Laura <bissonilaura@ehotmail.com>
2022-05-02 14:34:51 -0300

    todos los commits

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,22 @@
 import { PelisCollection, Peli } from "./models";
-
+// se instancia la clase PelisCollection
+// se crea la clase PelisController 
 class PelisController {
-  constructor() {}
-}
+
+Pelis: PelisCollection;
+
+  constructor() {
+    this.Pelis = new PelisCollection()
+  }
+  // se agregan métodos asincronicos que usan metodos del modelo
+ async get(options):Promise <any>{
+  
+   if (options.id){ return await this.Pelis.getById(options.id)}
+  else if (options.search){ return await this.Pelis.search(options.search)}
+  else if (options.empty){return await this.Pelis.getAll()}
+ }
+  add(peli:Peli){  return this.Pelis.add(peli)}
+ }
+
+
 export { PelisController };
