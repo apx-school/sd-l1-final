0b3f6677b96b9581664d76ff3a8d92cf984af957
Walter <daniwortiz003@gmail.com>
2022-05-07 02:10:17 -0300

    un commit

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,19 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  collection = new PelisCollection()
+  constructor() { }
+  async get(options):Promise<any> { 
+    if (options.id) { 
+      return await this.collection.getById(options.id)
+    } else if (options.search.title || options.search.tag) {  
+      return await this.collection.search(options.search)
+    } else if (options.search) {  
+      return await this.collection.getAll()
+    }
+  }
+  add(Peli: Peli) { 
+    return this.collection.add(Peli)
+  }
 }
 export { PelisController };
--- models.ts
+++ models.ts
@@ -8,11 +8,53 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  async getAll(): Promise<Peli[]> {
+    return await jsonfile.readFile("./pelis.json")
+  };
+  async getById(id: number) {
+    return await this.getAll().then(res => {
+      return res.find(i => i.id == id);
+    })
+  };
+  async search(options: any) {
+    if (options.title && options.tag) {
+      const filtraTitulo = await this.getAll().then(res => {
+        return res.filter(i => i.title.includes(options.title));
+      })
+      return filtraTitulo.filter(i => {
+        if (i.tags.includes(options.tag)) {
+            return i
+        }
+      })
+    } else if (options.title) {
+      return await this.getAll().then(res => {
+        return res.filter(i => i.title.includes(options.title))
+      })
+    } else if (options.tag) {
+      const aux = await this.getAll();
+      return await aux.filter(i => {
+          if (i.tags.includes(options.tag)) {
+            return i
+        }
+      })
+    }
+  }
+  add(peli: Peli): Promise<boolean> {
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+      if (peliExistente) {
+        return false;
+      } else {
+        // magia que agrega la pelicula a un objeto data
+        return this.getAll().then(res => {
+          res.push(peli);
+          const promesaDos = jsonfile.writeFile("./pelis.json", res);
+          return promesaDos.then(() => {
+            return true;
+          });
+        })
+      }
     });
+    return promesaUno;
   }
 }
 export { PelisCollection, Peli };
