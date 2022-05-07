fcfba5d725d9c2b3540a84ea12c1b63685d8bef7
Orlando <orlando.basualdo@hotmail.com>
2022-05-07 14:31:42 -0300

    desafio2

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,25 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+	peliculas: PelisCollection;
+	constructor() {
+		this.peliculas = new PelisCollection();
+	}
+
+	async get(options: any): Promise<any> {
+		if (options.id) {
+			return await this.peliculas.getById(options.id);
+		}
+		if (options.search) {
+			return await this.peliculas.search(options.search);
+		}
+		if (options.empty) {
+			return await this.peliculas.getAll();
+		}
+	}
+
+	add(peli: Peli): Promise<boolean> {
+		return this.peliculas.add(peli);
+	}
 }
 export { PelisController };
