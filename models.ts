9233051aded51e7f101b2159a010e9194c0e6488
Franco Jalil <francojalil7@gmail.com>
2022-05-16 17:48:40 -0300

    prueba

--- models.ts
+++ models.ts
@@ -8,11 +8,49 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  async getAll(): Promise<Peli[]> {
+    return await jsonfile.readFile("./peliculas.json").then((peliculas) => {
+      return peliculas;
     });
   }
+  async getById(id:number){
+    const pelis = await this.getAll();
+    const found = pelis.find((peli) => peli.id == id);
+    return found;
+  }
+  async search(options: any): Promise<any> {
+
+    const PELIS = await this.getAll();
+
+    if(options.title && options.tag) {
+
+        const TITLES_FOUND = PELIS.filter( (e) => e.title.includes(options.title) );
+
+        return TITLES_FOUND.filter( (e: any) => e.tags.find( (tag: any) => tag == options.tag ) == options.tag );
+
+    } else if(options.title) {
+
+        return PELIS.filter( (e: any) => e.title.includes(options.title) );
+
+    } else if(options.tag) {
+
+        return PELIS.filter( (e: any) => e.tags.find( (tag: any) => tag == options.tag ) == options.tag );
+    }
 }
+
+async add(peli: any): Promise<boolean> {
+    const EXISTE = await this.getById(peli.id);
+            
+    if(EXISTE) {
+        return false;
+    } else {
+        const pelis = await this.getAll();
+        pelis.push(peli);
+        await jsonfile.writeFile("./peliculas.json", pelis);
+        return true;
+    }                
+  }
+}
+
 export { PelisCollection, Peli };
+
