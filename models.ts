259cc9733086897b86f1ac2e8ed0cd8e63a345c1
IvoASandoval <ivoasandoval@gmail.com>
2022-05-16 22:18:32 -0300

    Resolucion DWF-M2

--- models.ts
+++ models.ts
@@ -8,11 +8,40 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
-    });
+  async getAll(): Promise<Peli[]> {
+    return await jsonfile.readFile("./pelis.json");
+  }
+  async getById(id: number): Promise<any> {
+    const resp = await this.getAll();
+    return resp.find((p: Peli) => p.id == id);
+  }
+  async search(options: any): Promise<any> {
+    var resp = await this.getAll();
+    if (options.title) {
+      var result = resp.filter((p) => {
+        return p.title.includes(options.title);
+      });
+      resp = result;
+    }
+    if (options.tag) {
+      var result = resp.filter((pelis) => {
+        return pelis.tags.includes(options.tag);
+      });
+      resp = result;
+    }
+    return resp;
+  }
+  async add(peli: Peli): Promise<boolean> {
+    const promesaUno = await this.getById(peli.id);
+    if (promesaUno) {
+      return false;
+    } else {
+      // magia que agrega la pelicula a un objeto data
+      const data = await this.getAll();
+      data.push(peli);
+      await jsonfile.writeFile("./pelis.json", data);
+      return true;
+    }
   }
 }
 export { PelisCollection, Peli };
