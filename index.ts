9b86c2fa91c47dc0f0eacdbf2820e03704eae9a1
Agustina <agustinamagallanez.gmail.com>
2022-05-20 16:33:09 -0300

    commit all test passed

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import * as minimist from "minimist";
+import {PelisController} from "./controllers"
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
@@ -6,10 +7,42 @@ function parseaParams(argv) {
   return resultado;
 }
 
-function main() {
-  const params = parseaParams(process.argv.slice(2));
+function processOptions(controller, params) {
 
-  console.log(params);
+  if(params._[0] == "add") { 
+    return controller.add({id: params.id, title: params.title, tags: params.tags}).then((respuesta) => {
+      return respuesta;
+    });
+  } else if (params._[0] == "get" && params._[1]){
+    return controller.get({id: params._[1]}).then((respuesta) => {
+      return respuesta;
+    });
+  } else if (params._[0] == "search" && params.title && params.tag){
+    return controller.get({search: {title: params.title, tag: params.tag}}).then((respuesta) => {
+      return respuesta;
+    })
+  } else if (params._[0] == "search" && params.title){
+    return controller.get({search: {title: params.title}}).then((respuesta) =>{
+      return respuesta;
+    });
+  } else if (params._[0] == "search" && params.tag){
+    return controller.get({search: {tag: params.tag}}).then((respuesta) => {
+      return respuesta;
+    });
+  } else if ({}) {
+    return controller.get({}).then((respuesta) =>{
+      return respuesta;
+    })
+  }
 }
+
+function main() {
+  const controller = new PelisController();
+  const params = parseaParams(process.argv.slice(2));
+  const resultado = processOptions(controller, params);
+  resultado.then((respuesta) => {
+    console.log(respuesta);
+  });
+};
 
 main();
