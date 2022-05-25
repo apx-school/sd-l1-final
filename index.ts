56cd41e1202f48b95ee636f91e6512c72637c1bd
Lucas Ariel Rotta <lucas.ariel.rotta@gmail.com>
2022-05-24 21:05:13 -0300

    desafio 2

--- index.ts
+++ index.ts
@@ -1,15 +1,49 @@
-import * as minimist from "minimist";
+import { PelisController } from "./controllers";
+
+
+var minimist = require('minimist');
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
-
+  
   return resultado;
 }
 
-function main() {
-  const params = parseaParams(process.argv.slice(2));
 
-  console.log(params);
+
+ function main() {
+  const params = parseaParams(process.argv.slice(2));
+  const peliculaController = new PelisController();
+
+
+  if(params._[0] === undefined){
+
+      return peliculaController.get().then(p=>console.log(p));
+
+  }else if (params._[0] === "get"){
+    if(typeof(params._[1]) !== "number"){
+      return console.log("No ingresaste un numero de ID para buscar la pelicula")
+    }else {
+      return peliculaController.get({id:params._[1]}).then(p => console.log(p));
+    }
+    
+  }else if(params._[0]==="add"){
+    return peliculaController.add({
+      id: params.id,
+      title:params.title,
+      tags:params.tags
+    }).then(p=>console.log(p));
+
+  }else if(params._[0] ==="search"){
+    
+    return peliculaController.get({search:{title:params.title, tag:params.tag}}).then(p => console.log(p));
+
+  }else {
+
+    return console.log("Parametros inexistentes");
+  }
+    
 }
+  
 
 main();
--- models.ts
+++ models.ts
@@ -1,4 +1,6 @@
+var minimist = require('minimist');
 import * as jsonfile from "jsonfile";
+
 
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
@@ -7,12 +9,69 @@ class Peli {
   tags: string[];
 }
 
+
 class PelisCollection {
+
+  data: Peli[] = [];
+  
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
+    return jsonfile.readFile(__dirname + "/pelis.json").then((archivoPeliculas) => {
       // la respuesta de la promesa
-      return [];
+
+      return  this.data = archivoPeliculas; 
     });
   }
+
+  getById(id:number){
+    return this.getAll().then((peliculas) =>{
+      return  peliculas.find(peli => peli.id == id);
+    })
+  }
+
+  search(options:any){
+    let resultadoPelis = this.getAll();
+    
+    if(options.title){
+       resultadoPelis = resultadoPelis.then((peliculas)=>{
+        return peliculas.filter(peli =>  peli.title.toLowerCase().includes(options.title.toLowerCase()));
+      })
+    }
+
+    if(options.tag){
+        
+       resultadoPelis =  resultadoPelis.then((peliculas) => {
+        return peliculas.filter(peli =>  peli.tags.find((tag) => {
+          
+          return tag == options.tag
+        }));
+      })
+      
+    }
+
+   
+    return resultadoPelis;
+  }
+
+  add(peli:Peli):Promise<boolean>{
+    
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+        if(peliExistente){
+          return false;
+        }else {
+          
+           this.data.push(peli) ;
+          
+          const promesaDos = jsonfile.writeFile(__dirname + "/pelis.json",this.data);
+
+          return promesaDos.then(()=>  true)
+        }
+    })
+    return promesaUno;
+  }
+  
 }
+
+
+
+
 export { PelisCollection, Peli };
