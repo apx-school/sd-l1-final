847cc9828031439ca813790ad1a5b0740c238087
Alberto8692 <aosanguin@gmail.com>
2022-06-18 18:30:49 -0300

    cambios en el repo

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,27 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  peliculas: PelisCollection;
+
+  constructor() {
+    this.peliculas = new PelisCollection();
+  }
+async get (options) {
+
+  if (options.id) {
+    return await this.peliculas.getById(options.id)
+  }
+  else if (options.search) {
+    return await this.peliculas.search(options.search);
+
+  } else {
+    return await this.peliculas.getAll();
+  }
+  }
+
+  async add(peli: Peli) {
+    return this.peliculas.add(peli)
+  }
+
 }
 export { PelisController };
--- models.ts
+++ models.ts
@@ -8,11 +8,55 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
-    });
+  async getAll(): Promise<Peli[]> {
+    return await jsonfile.readFile("./pelis.json");
+  };
+
+  async getById(id: number): Promise <Peli> {
+      const peliculas = await this.getAll();
+      return peliculas.find((item) => {
+        return item.id === id
+      });
   }
+      
+
+  async search(options: any): Promise <any> {
+    const films = await this.getAll();
+    
+    if (options.title && options.tag) {
+        const buscaTitulos = films.filter((tituloPeli)=>{
+        return tituloPeli.title.includes (options.title) && tituloPeli.tags.includes (options.tag)
+})
+    return buscaTitulos
 }
-export { PelisCollection, Peli };
+    else if (options.title) {
+        const tituloDev = films.filter((tituloPeli)=> {
+            return tituloPeli.title.includes(options.title)
+        } );
+        return tituloDev
+} 
+
+else if (options.tag) {
+    const tagDev = films.find ((tagPeli)=> {
+      return tagPeli.tags.includes (options.tag)
+    });
+    return tagDev
+
+
+  }};
+
+  async add(peli: Peli): Promise <boolean> {
+    const filmExist = await this.getById(peli.id);
+
+      if (filmExist) {
+        return false;
+ }else { 
+        const films = await this.getAll();
+        films.push(peli)
+        await jsonfile.writeFile ("./pelis.json", films)
+          return true;
+        }
+      
+   
+} } 
+    export { PelisCollection, Peli };
