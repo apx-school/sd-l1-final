d65a8f98807c1670d26938b4790277b5e61c81f5
Javier Diaz <diazjavierdavid@hotmail.com>
2022-06-18 13:42:07 -0300

    Tamo

--- controllers.ts
+++ controllers.ts
@@ -1,46 +1,34 @@
 import { PelisCollection, Peli } from "./models";
-
-
 class PelisController {
+  coleccionPelis : PelisCollection;
 
-  coleccionDePelis: PelisCollection;
-  
   constructor() {
-    this.coleccionDePelis = new PelisCollection();
+    this.coleccionPelis = new PelisCollection();
   }
 
-  get(options?:any){
-
-    if(!options){
-      return this.coleccionDePelis.getAll()  ;
-    }
-
+  get(options:any){
     if(options.id){
-      return this.coleccionDePelis.getById(options.id);
-    } else if (options.search){
-      if(options.search.title && options.search.tag){
-        return this.coleccionDePelis.search({title: options.search.title}).then( data => {
-          return this.coleccionDePelis.search({tag: options.search.tag}).then( peli => {
-            return(data.filter( dato => {
-              return peli.find( peli => peli.id === dato.id);
-            }));
-          });
-        });
-
-      } else if (options.search.title){
-        return this.coleccionDePelis.search(options.search);
+     return this.coleccionPelis.getById(options.id);
+    } else if(options.search){
+      if (options.search.title && options.search.tag){
+       return this.coleccionPelis.search({
+        title: options.search.title,
+        tag: options.search.tag
+       })
+      } else if (options.search.title) {
+        return this.coleccionPelis.search({title: options.search.title})
       } else if (options.search.tag){
-        return this.coleccionDePelis.search(options.search);
-      }
+        return this.coleccionPelis.search({tag: options.search.tag})
+      } 
     } else {
-      return this.coleccionDePelis.getAll();
+      return this.coleccionPelis.getAll();
     }
-
-  }
+  };
 
   add(peli:Peli){
-    this.coleccionDePelis.add(peli);
+    return this.coleccionPelis.add(peli);
   }
 }
+
 
 export { PelisController };
--- index.ts
+++ index.ts
@@ -3,22 +3,45 @@ import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
-
   return resultado;
+};
+
+function resultadoFinalParseado(controller, params) {
+
+  if(params._[0] == "add") { 
+    return controller.add({id: params.id, title: params.title, tags: params.tags}).then((respuesta) => {
+      return respuesta;
+    });
+  } else if (params._[0] == "get" && params._[1]){
+    return controller.get({id: params._[1]}).then((respuesta) => {
+      return respuesta;
+    });
+  } else if (params._[0] == "search" && params.title && params.tag){
+    return controller.get({search: {title: params.title, tag: params.tag}}).then((respuesta) => {
+      return respuesta;
+    })
+  } else if (params._[0] == "search" && params.title){
+    return controller.get({search: {title: params.title}}).then((respuesta) =>{
+      return respuesta;
+    });
+  } else if (params._[0] == "search" && params.tag){
+    return controller.get({search: {tag: params.tag}}).then((respuesta) => {
+      return respuesta;
+    });
+  } else if ({}) {
+    return controller.get({}).then((respuesta) =>{
+      return respuesta;
+    })
+  }
 }
 
 function main() {
+  const controller = new PelisController();
   const params = parseaParams(process.argv.slice(2));
-  const controladorDePelis = new PelisController();
-  if(params._[0] == "add"){
-    controladorDePelis.add({id: params.id, title: params.title, tags: params.tags});
-  } else if (params._[0] == "get"){
-    controladorDePelis.get({id: params._[1]}).then( data => console.log(data));
-  } else if ( params._[0] == "search"){
-    controladorDePelis.get({search:{title: params.title, tag: params.tag}}).then(data => console.log(data));
-  } else {
-    controladorDePelis.get().then(data => console.log(data));
-  }
-}
+  const resultadoFinal = resultadoFinalParseado(controller, params);
+  resultadoFinal.then((respuesta) => {
+    console.log(respuesta);
+  });
+};
 
 main();
