4f71aaf17a9b54a4e8cb72e4bd27a9fed29b66a9
Martin Sarmiento <sarmientomartin678@gmail.com>
2022-07-13 20:50:55 -0300

    cambios

--- index.ts
+++ index.ts
@@ -8,12 +8,12 @@ function parseaParams(argv): any {
   if (options == 'get') {
     return { id: params._[1] };
   } else if (options == 'search') {
-    if (params.title && params.tags) {
-      return { search: { title: params.title, tags: params.tags } };
+    if (params.title && params.tag) {
+      return { search: { title: params.title, tags: params.tag } };
     } else if (params.title) {
       return { search: { title: params.title } };
     } else if (params.tag) {
-      return { search: { tags: params.tags } };
+      return { search: { tags: params.tag } };
     }
   } else if (options == 'add') {
     return { add: { id: params.id, title: params.title, tags: params.tags } };
@@ -25,7 +25,7 @@ function parseaParams(argv): any {
 async function main() {
   const params = parseaParams(process.argv.slice(2));
   const controller = new PelisController();
-
+  console.log(params);
   controller.get(params).then((res) => {
     console.log(res);
   });
