7a98b4da00a3e153703d5a522ddab3ab4e86e803
Guillenjulian <guille.julian1985@gmial.com>
2022-07-13 20:40:16 -0300

    commit con errores es search

--- index.ts
+++ index.ts
@@ -1,15 +1,32 @@
 import * as minimist from "minimist";
+import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
-  const resultado = minimist(argv);
-
-  return resultado;
+  const params = minimist(argv);
+  const option = params._[0];
+  if (option == "get") {
+    return { id: params._[1] };
+  } else if (option == "search") {
+    if (params.titel && params.tags) {
+      return { search: { titel: params.titel, tags: params.tags } };
+    } else if (params.titel) {
+      return { search: { titel: params.titel } };
+    } else if (params.tags) {
+      return { search: { tags: params.tags } };
+    }
+  } else if (option == "add") {
+    return { add: { id: params.id, titel: params.titel, tags: params.tags } };
+  } else {
+    return { all: 1 };
+  }
 }
 
 function main() {
   const params = parseaParams(process.argv.slice(2));
-
-  console.log(params);
+  const controllers = new PelisController();
+  controllers.get(params).then((resultado) => {
+    console.log(resultado);
+  });
 }
 
 main();
--- models.ts
+++ models.ts
@@ -8,11 +8,44 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  async getAll(): Promise<Peli[]> {
+    return await jsonfile.readFile("./pelis.json");
+  }
+  async add(peli: Peli): Promise<boolean> {
+    const exite = await this.getById(peli.id);
+    if (exite) {
+      return false;
+    } else {
+      const data = await this.getAll();
+      data.push(peli);
+      await jsonfile.writeFile("./pelis.json", data);
+      return true;
+    }
+  }
+  async getById(id: number) {
+    const peli = await this.getAll();
+    return peli.find((e) => {
+      return e.id == id;
     });
+  }
+  async search(options: any) {
+    const peli = await this.getAll();
+    if (options.title && options.tag) {
+      const respuesta = peli.filter((e) => {
+        e.title.includes(options.title) && e.tags.includes(options.tag);
+      });
+      return respuesta;
+    } else if (options.title) {
+      const respuesta = peli.filter((e) => {
+        return e.title.includes(options.title);
+      });
+      return respuesta;
+    } else if (options.tag) {
+      const respuesta = peli.filter((e) => {
+        e.tags.includes(options.tag);
+      });
+      return respuesta;
+    }
   }
 }
 export { PelisCollection, Peli };
