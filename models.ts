9b9bae7e0cbcf344f720b79e969e676e4e5c853f
Julieta <julietabertenasco@gmail.com>
2022-08-02 10:47:00 -0300

    in process

--- models.ts
+++ models.ts
@@ -9,10 +9,52 @@ class Peli {
 
 class PelisCollection {
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
+    return jsonfile.readFile("./pelis.json").then((collection) => {
       // la respuesta de la promesa
-      return [];
+      return collection;
     });
   }
+  getById(id:number) {
+    return this.getAll().then((collection) => {
+      return collection.find((item) => item.id == id)
+    })
+  }
+  search(options:any) {
+    if (options.title) {
+      return this.getAll().then((collection) => {
+        return collection.filter((item) => item.title == options.title)
+      })
+    } else if(options.tag) {
+      return this.getAll().then((collection) => {
+        return collection.filter((item) => item.tags == options.tag)
+      })
+    }
+  }
+  add(peli:Peli) {
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+      if (peliExistente) {
+        return false;
+      } else {
+        // magia que agrega la pelicula a un objeto data
+        const data = {...};
+        const promesaDos = jsonfile.writeFile("./pelis.json", data);
+
+        return promesaDos.then(() => {
+          return true;
+        });
+      }
+    });
+
+    return promesaUno;
+  }
 }
+
 export { PelisCollection, Peli };
+
+
+
+
+const prueba = new PelisCollection()
+console.log("getAll", prueba.getAll());
+console.log("getById", prueba.getById(2));
+console.log("search", prueba.search({title: "time"}));
