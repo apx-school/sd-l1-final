2b225058f67cadd2292e8f434f823caa178d91f0
Radamés Ibañez <soyradames@gmail.com>
2022-08-03 21:18:46 -0300

    First commit.

--- index.ts
+++ index.ts
@@ -1,15 +1,108 @@
 import * as minimist from "minimist";
+import { PelisController } from "./controllers";
+import { Peli } from "./models";
+
+type SearchMovieParameters = {
+  search?: {
+    title?: string | null;
+    tag?: string | null;
+  };
+};
+
+type ConsoleActions = {
+  action?: "add" | "get" | "search" | null;
+  id?: number | null;
+  addPeliData?: Peli;
+  options?: SearchMovieParameters | null;
+};
 
 function parseaParams(argv) {
-  const resultado = minimist(argv);
+  const input = minimist(argv);
 
-  return resultado;
+  // _Array tendrá lo que trae la propiedad _ de minimist, básicamente la opción que me pasan por consola, pudiendo ser...
+  // []: pide todas las películas
+  // ['get', ID]: pide una película, e ID es el ID de la película
+  // ['search']: pide buscar una película por title y/o tag (eso lo tomo por otro lado)
+  // ['add']: pide agregar una película, recibo id/title/tags por otro lado
+
+  const _Array = input._;
+
+  let params: ConsoleActions;
+
+  // si _Array está vacío, pidieron todas las películas
+  if (_Array.length === 0) {
+    params = {
+      action: null,
+    };
+  }
+
+  // si _Array tiene get, pidieron una película en particular
+  if (_Array.length === 2) {
+    params = {
+      action: _Array[0],
+      id: _Array[1],
+    };
+  }
+
+  // si _Array tiene un elemento, pidieron buscar o agregar una película
+  if (_Array.length === 1) {
+    if (_Array[0] === "add") {
+      params = {
+        action: _Array[0],
+        addPeliData: {
+          id: input.id,
+          title: input.title,
+          tags: input.tags,
+        },
+      };
+    } else if (_Array[0] === "search") {
+      params = {
+        action: _Array[0],
+        options: {
+          search: {
+            title: input.title,
+            tag: input.tag,
+          },
+        },
+      };
+    } else params = {};
+  }
+  return params;
 }
 
 function main() {
   const params = parseaParams(process.argv.slice(2));
+  const pelisController = new PelisController();
 
-  console.log(params);
+  switch (params.action) {
+    case null:
+      pelisController.get().then((todasLasPelis) => console.log(todasLasPelis));
+      break;
+    case "get":
+      pelisController
+        .get(params)
+        .then((peliBuscada) => console.log(peliBuscada));
+      break;
+    case "search":
+      pelisController
+        .get(params.options)
+        .then((peliBuscada) => console.log(peliBuscada));
+      break;
+    case "add":
+      pelisController.add(params.addPeliData).then((res) => {
+        if (res === false) {
+          console.log(
+            "La película no pudo ser añadida (error de escritura o película ya existente)."
+          );
+        } else {
+          console.log("Película añadida correctamente.");
+        }
+      });
+      break;
+    default:
+      console.log("Parámetros incorrectos.");
+      break;
+  }
 }
 
 main();
--- models.ts
+++ models.ts
@@ -1,18 +1,102 @@
 import * as jsonfile from "jsonfile";
 
-// no modificar estas propiedades, agregar todas las que quieras
+// no es buena práctica importar todo
+import * as _ from "lodash";
+
 class Peli {
   id: number;
   title: string;
+  rating?: number;
   tags: string[];
+  starring?: string[];
+  year?: number;
+  length?: number;
+  streamingService?: string;
 }
 
 class PelisCollection {
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json").then((allPelis) => {
+      return allPelis;
     });
   }
+
+  getById(searchedId: number): Promise<Peli> {
+    return jsonfile.readFile("./pelis.json").then((allPelis) => {
+      return _.find(allPelis, { id: searchedId });
+    });
+  }
+
+  search(options: any): Promise<Peli[] | Peli> {
+    return jsonfile.readFile("./pelis.json").then((allPelis) => {
+      // options.title = algo; options.tag = algo
+      if (options.title && !options.tag) {
+        // devolver todas las películas con ese string en su title
+        return _.filter(allPelis, (peli) => {
+          if (peli.title.includes(options.title)) {
+            return peli;
+          }
+        });
+      }
+      if (!options.title && options.tag) {
+        // devolver todas las películas con ese tag en sus tags
+        return _.filter(allPelis, (peli) => {
+          if (peli.tags.includes(options.tag)) {
+            return peli;
+          }
+        });
+      }
+      if (options.title && options.tag) {
+        // devolver todas las películas con ese string en el title y el tag en sus tags
+        const pelisSearchedByTitle = _.filter(allPelis, (peli) => {
+          if (peli.title.includes(options.title)) {
+            return peli;
+          }
+        });
+        const pelisSearchedByTitleAndTag = _.filter(
+          pelisSearchedByTitle,
+          (peli) => {
+            if (peli.tags.includes(options.tag)) {
+              return peli;
+            }
+          }
+        );
+        return pelisSearchedByTitleAndTag;
+      }
+    });
+  }
+
+  add(peli: Peli): Promise<boolean> {
+    // si existe la película, la promesa devuelve falso
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+      if (peliExistente) return false;
+      else {
+        const addNewPeli = async () => {
+          let allPelis = await jsonfile.readFile("./pelis.json");
+          allPelis.push(peli);
+          await jsonfile.writeFile("./pelis.json", allPelis);
+          return true;
+        };
+        return addNewPeli().then((res) => res);
+      }
+    });
+    return promesaUno;
+  }
 }
+
 export { PelisCollection, Peli };
+
+// const testPelisCollection = new PelisCollection();
+// testPelisCollection.getAll().then((res) => console.log(res));
+// testPelisCollection.getById(5).then((res) => console.log(res));
+// testPelisCollection.search({ title: "Volver" }).then((res) => console.log(res));
+// testPelisCollection
+//   .search({ tag: "argentina" })
+//   .then((res) => console.log(res));
+// testPelisCollection
+//   .search({ title: "Hoy", tag: "argentina" })
+//   .then((res) => console.log(res));
+
+// testPelisCollection
+//   .add({ id: 1345, title: "Sarasa cósmica", tags: ["test"] })
+//   .then((res) => console.log(res));
