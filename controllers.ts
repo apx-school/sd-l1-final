ebfa90d4c1f910cc9558d1859cfd0fa49777005c
Elias Rossini <elias.rossini18@gmail.com>
2022-08-09 22:44:42 -0300

    desafio modulo 2

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,21 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  data: PelisCollection;
+  constructor() {
+    this.data = new PelisCollection();
+  }
+  async get(options: any) {
+    if (options.id) {
+      return await this.data.getById(options.id);
+    } else if (options.search) {
+      return await this.data.search(options.search);
+    } else {
+      return await this.data.getAll();
+    }
+  }
+  add(peli: Peli) {
+    return this.data.add(peli);
+  }
 }
 export { PelisController };
--- models.ts
+++ models.ts
@@ -1,6 +1,6 @@
 import * as jsonfile from "jsonfile";
+import { formatWithOptions } from "util";
 
-// no modificar estas propiedades, agregar todas las que quieras
 class Peli {
   id: number;
   title: string;
@@ -8,11 +8,51 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  peliculas: Peli[];
+  async getAll(): Promise<any> {
+    const json = await jsonfile.readFile("./pelis.json").then((resp) => {
+      return resp;
+    });
+    this.peliculas = json;
+    return this.peliculas;
+  }
+  async getById(id: number) {
+    return await this.getAll().then((peliculas) => {
+      return peliculas.find((peli) => {
+        return peli.id == id;
+      });
     });
   }
+  async search(option: any) {
+    const peli = await this.getAll();
+    if (option.title && option.tag) {
+      return peli.filter(
+        (pelis) =>
+          pelis.title.includes(option.title) && pelis.tags.includes(option.tag)
+      );
+    } else if (option.title) {
+      return peli.filter((pelis) => pelis.title.includes(option.title));
+    } else if (option.tag) {
+      return peli.filter((pelis) => pelis.tags.includes(option.tag));
+    }
+  }
+
+  async add(peli: Peli) {
+    const promesaUno = this.getById(peli.id).then((pelisExistente) => {
+      if (pelisExistente) {
+        return false;
+      } else {
+        const promesaDos = this.getAll().then((pelis) => {
+          const data = pelis.concat(peli);
+          return jsonfile.writeFile("./pelis.json", data);
+        });
+        return promesaDos.then(() => {
+          return true;
+        });
+      }
+    });
+    return await promesaUno;
+  }
 }
+
 export { PelisCollection, Peli };
