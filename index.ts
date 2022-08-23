d62b9848899a5bbfb78bb5541361c97dea6657d6
Martin Belmonte <99141142+MartinBelmonte23@users.noreply.github.com>
2022-08-23 16:53:58 -0300

    .
    Resuelto

--- index.ts
+++ index.ts
@@ -1,15 +1,39 @@
+import { PelisController } from "./controllers";
 import * as minimist from "minimist";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
 
-  return resultado;
+  const pelisControllers = new PelisController();
+
+  const accion = resultado._;
+
+  const parametros = accion[0]
+
+  if (parametros === "get") {
+
+    return pelisControllers.get({ id: accion[1] })
+    
+  } else if (parametros === "search") {
+
+    return pelisControllers.get({ search:resultado });
+    
+  } else if (parametros === "add") {
+    const peliNueva = {
+      id: resultado.id,
+      title: resultado.title,
+      tags: resultado.tags
+    }
+    
+    return pelisControllers.add(peliNueva);
+  }
+    return pelisControllers.peliculas.getAll();
+  
 }
 
-function main() {
-  const params = parseaParams(process.argv.slice(2));
-
-  console.log(params);
+async function main() {
+  const params = await parseaParams(process.argv.slice(2));
+  console.log( params);
 }
 
 main();
