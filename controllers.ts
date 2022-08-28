236658a770b2051f3c9c9e66a76f2d83daf25f8e
srv013@gmail.com <97910311+SRV013@users.noreply.github.com>
2022-08-28 08:56:56 -0300

    si funciona

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,18 @@
+// LLAMADO A MOD
 import { PelisCollection, Peli } from "./models";
 
+// CLASE Y CON
 class PelisController {
-  constructor() {}
+	peliculas: PelisCollection;
+	constructor() {
+		this.peliculas = new PelisCollection();
+	}
+// TIPO
+	async get(tipo: any): Promise<any> {
+		if (tipo.id) {return await this.peliculas.getById(tipo.id);}
+		else if (tipo.search) {return await this.peliculas.search(tipo.search);}
+		else if (tipo.empty) {return await this.peliculas.getAll();}
+	}
+	add(peli: Peli): Promise<boolean> {return this.peliculas.add(peli);}
 }
 export { PelisController };
