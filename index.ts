93b21ecc384aced8bc0034105c5405607e1ec5fc
Erica Guerrero <guerreroerica1989@gmail.com>
2022-09-14 20:48:15 -0300

    completé desafio

--- index.ts
+++ index.ts
@@ -1,15 +1,27 @@
 import * as minimist from "minimist";
-
+import {PelisController  } from "./controllers";
 function parseaParams(argv) {
-  const resultado = minimist(argv);
-
-  return resultado;
+	const resultado = minimist(argv);
+	const newPeli = new PelisController();
+	const accion = resultado._;
+	const parametro = accion[0];
+	if (parametro === 'get') return newPeli.get({ id: accion[1] });
+	else if (parametro === 'search') return newPeli.get({ search: resultado });
+	else if (parametro === 'add') {
+		const peli = {
+			id: resultado.id,
+			title: resultado.title,
+			tags: resultado.tags,
+		};
+		return newPeli.add(peli);
+	}
+	return newPeli.get({});
 }
 
-function main() {
-  const params = parseaParams(process.argv.slice(2));
-
-  console.log(params);
+async function main() {
+	const param = process.argv.slice(2);
+	const res = await parseaParams(param);
+	console.log(res);
 }
 
 main();
--- models.ts
+++ models.ts
@@ -9,10 +9,49 @@ class Peli {
 
 class PelisCollection {
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
+    return jsonfile.readFile("./pelis.json").then((pelis) => {
       // la respuesta de la promesa
-      return [];
+    return pelis;
     });
   }
+  getById(id:number){
+    return this.getAll().then((pelis)=>{
+      const peliPorId = pelis.find((p)=>{
+        return p.id == id;
+      });
+      return peliPorId;
+    })
+  }
+  async search(options: any) {
+		const pelis = await this.getAll();
+		if (options.title && options.tag) {
+			return pelis.filter((peli) => {
+				const peliPorTitulo = peli['title'].includes(options.title);
+				const resultado = peli.tags.map((tag) => tag.toLowerCase());
+				const peliPorTag = resultado.includes(options.tag);
+				return peliPorTitulo && peliPorTag;
+			});
+		} else if (options.title) {
+			return pelis.filter((peli) => peli['title'].includes(options.title));
+		} else if (options.tag) {
+			return pelis.filter((peli) => {
+				const resultado = peli.tags.map((tag) => tag.toLowerCase());
+				return resultado.includes(options.tag);
+			});
+		} else {
+			return pelis;
+		}
+	}
+  async add(peli: Peli): Promise<Boolean> {
+		const estaElId= await this.getById(peli.id);
+		if (estaElId) {
+			//console.log("existente")
+		}
+		const pelis = await this.getAll();
+		pelis.push(peli);
+		await jsonfile.writeFile('./pelis.json', pelis);
+		//console.log( peli,);
+		return true;
+	}
 }
 export { PelisCollection, Peli };
