05cac7a0f88a9d3a9b8bad0dfe167dc004756809
Ezequiel <ezequielezequiel9@gmail.com>
2022-09-14 10:27:03 -0300

    primer commit

--- models.ts
+++ models.ts
@@ -1,5 +1,4 @@
 import * as jsonfile from "jsonfile";
-
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
   id: number;
@@ -8,11 +7,52 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  // data: Peli[] = []
+
+  async getAll(): Promise<Peli[]> {
+    return await jsonfile.readFile("./pelis.json");
+  }
+  async getById(id: number): Promise<Peli> {
+    const lasPelis = await this.getAll();
+    return lasPelis.find((idPeli) => {
+      return idPeli.id === id;
     });
   }
+
+  async search(options: any): Promise<any> {
+    const pelis = await this.getAll();
+
+    if (options.title && options.tag) {
+      const encontrar = pelis.filter((titlePeli) => {
+        return (
+          titlePeli.title.includes(options.title) &&
+          titlePeli.tags.includes(options.tag)
+        );
+      });
+      return encontrar;
+    } else if (options.title) {
+      const tituloEncontrado = pelis.filter((titlepeli) => {
+        return titlepeli.title.includes(options.title);
+      });
+      return tituloEncontrado;
+    } else if (options.tag) {
+      const tagEncontrado = pelis.find((tagPeli) => {
+        return tagPeli.tags.includes(options.tag);
+      });
+      return tagEncontrado;
+    }
+  }
+  async add(peli: Peli) {
+    const peliIdExistente = await this.getById(peli.id);
+    if (peliIdExistente) {
+      return false;
+    }
+    const pelis = await this.getAll();
+    pelis.push(peli);
+    await jsonfile.writeFile("./pelis.json", pelis);
+
+    return true;
+  }
 }
+
 export { PelisCollection, Peli };
