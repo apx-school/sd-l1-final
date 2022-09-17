91ebf29ddbfc3545bae217dfb7a2861e47b241a4
Matias Cortese <matiasemanuelcortese@gmail.com>
2022-09-17 19:22:40 -0300

    Primer intento del desafio

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,36 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  pelis: PelisCollection;
+  promesa: Promise<any>
+  constructor() {
+    this.pelis = new PelisCollection();
+    const promesa = this.pelis.getAll();
+    this.promesa = promesa;
+  }
+  async get(options:any):Promise<any>{
+    // si el objeto tiene la propiedad id (ej: { id:1234 }), debe devolver la película con ese id.
+    if (options.id) {
+      return await this.pelis.getById(options.id);
+    }
+    // Este da undefined. Seguir desde acá
+    if (options.search) {
+      if (options.search.title) {
+        return await this.pelis.search(options.search);
+      }
+      if (options.search.tag) {
+        return await this.pelis.search(options.search);
+      }
+      if (options.search.title && options.search.tag) {
+        return await this.pelis.search(options.search.title && options.search.tag);
+      }
+    }
+    if (options.empty) {
+      return await this.pelis.getAll();
+    }
+  }
+  async add(peli:Peli){
+    return await this.pelis.add(peli);
+  }
 }
 export { PelisController };
--- models.ts
+++ models.ts
@@ -8,11 +8,46 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
-    });
+  pelis: Peli[];
+  // getAll() devuelve un array del tipo Peli con todas las pelis que se encuentren guardadas en el archivo JSON de pelis.
+  async getAll(): Promise<Peli[]> {
+    const json = await jsonfile.readFile(__dirname + '/pelis.json');
+    return this.pelis = json;
+  }
+  // getById(id:number) devuelve la peli que tenga el id que se le pase por parámetro.
+  async getById(id:number): Promise<Peli>{
+    await this.getAll();
+    return this.pelis.find(peli => peli.id == id);
+  }
+  // search(options:any) recibe un objeto y, según cuales sean sus propiedades, hay dos opciones:
+  async search(options:any): Promise<any>{
+    await this.getAll();
+    if (options.title) {
+      return this.pelis.filter(peli => peli.title.toLowerCase().includes(options.title.toLowerCase()));
+    }
+    if (options.tag) {
+      return this.pelis.filter(peli => peli.tags.includes(options.tag.toLowerCase()));
+    }
+  }
+  add(peli:Peli): Promise<boolean>{
+    const promesaUno = this.getById(peli.id).then(peliExistente => {
+      if (peliExistente) {
+        return false;
+      }
+      else {
+        return this.getAll().then(pelis => {
+          pelis.push(peli);
+          const promesaDos = jsonfile.writeFile("./pelis.json", pelis);
+          return promesaDos.then(() => {
+            return true
+          });
+        })
+      }
+    })
+    return promesaUno;
   }
 }
+
 export { PelisCollection, Peli };
+
+
