d49fff5a8ca35ac52513f7ea08d8ad7b9f91cc20
Felipe Ponce <feli.piponce@gmail.com>
2022-09-19 19:42:01 -0300

    primer commit

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,24 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  pelis: PelisCollection;
+  constructor() {
+    this.pelis = new PelisCollection();
+  }
+  async get(options: any) {
+    var respuesta: any;
+    if (options.id) {
+      respuesta = await this.pelis.getById(options.id);
+    } else if (options.search) {
+      respuesta = await this.pelis.search(options.search);
+    } else if (options.empty) {
+      respuesta = await this.pelis.getAll();
+    }
+    return respuesta;
+  }
+  async add(peli: Peli): Promise<any> {
+    const respuesta = await this.pelis.add(peli);
+    return respuesta;
+  }
 }
 export { PelisController };
