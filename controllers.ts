b838c06b80ebbc05b5d13bf41ee1de7d2c168118
Matias Cortese <matiasemanuelcortese@gmail.com>
2022-09-22 23:28:08 -0300

    New try

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,25 @@
+import { timeStamp } from "console";
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  pelis: PelisCollection;
+  constructor() {
+    this.pelis = new PelisCollection;
+  }
+  async get(options?:any) {
+    if (!options) {
+      return await this.pelis.getAll();
+    }
+    if (options.id) {
+      return await this.pelis.getById(options.id);
+    }
+    if (options.search.title || options.search.tags) {
+      return await this.pelis.search(options.search);
+    }
+  }
+  async add(peli: Peli){
+    await this.pelis.add(peli);
+  }
 }
+
 export { PelisController };
--- models.ts
+++ models.ts
@@ -8,11 +8,42 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
-    });
+  async getAll(): Promise<Peli[]> {
+    return await jsonfile.readFile("./pelis.json");
+  }
+  async getById(id:number): Promise<Peli> {
+    const pelis = await this.getAll();
+    return pelis.find(peli => {
+      return peli.id == id;
+    })
+  }
+  async search(options?:any): Promise<Peli[]> {
+    const pelis = await this.getAll();
+    if (options.title){
+      return pelis.filter(peli => {
+        return peli.title.toLowerCase().includes(options.title.toLowerCase());
+      });
+    }
+    if (options.tags){
+      return pelis.filter(peli => {
+        return peli.tags.includes(options.tags.toLowerCase());
+      });
+    }
+  }
+  async add(peli: Peli): Promise<boolean> {
+    const encontrado = await this.getById(peli.id);
+    if (encontrado) {
+      return false;
+    }
+    else {
+      const pelis = await this.getAll();
+      pelis.push(peli);
+      await jsonfile.writeFile('./pelis.json', pelis);
+      return true;   
+    }
   }
 }
-export { PelisCollection, Peli };
+
+export { PelisCollection, Peli }
+
+
