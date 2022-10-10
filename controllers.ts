28ba0d0baa024efb2d77ef27db2ecf9651233b93
brunoken22 <bruno_am_22@hotmail.com>
2022-10-10 18:58:29 -0300

    Actualizado

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,22 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  pelis: PelisCollection;
+  constructor() {
+    this.pelis = new PelisCollection();
+  }
+  async get(options) {
+    if (options.id) {
+      return await this.pelis.getById(options.id);
+    }
+    if (options.search) {
+      return await this.pelis.search(options.search);
+    }
+    return await this.pelis.getAll();
+  }
+
+  add(peli: Peli) {
+    return this.pelis.add(peli).then((result) => result);
+  }
 }
 export { PelisController };
--- index.ts
+++ index.ts
@@ -1,15 +1,39 @@
 import * as minimist from "minimist";
+import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
-
   return resultado;
 }
 
-function main() {
-  const params = parseaParams(process.argv.slice(2));
+async function procesador(option) {
+  let controller = new PelisController();
 
-  console.log(params);
+  if (option._[0] == "add") {
+    return await controller.add({
+      id: option.id,
+      title: option.title,
+      tags: option.tag,
+    });
+  }
+
+  if (option._[0] === "get") {
+    return controller.get({ id: option._[1] });
+  }
+  if (option._[0] === "search") {
+    return controller.get({
+      search: { title: option.title, tag: option.tag },
+    });
+  }
+  return controller.get({});
+}
+function main() {
+  const param = parseaParams(process.argv.slice(2));
+
+  return procesador(param).then((i) => {
+    console.log(i);
+    return i;
+  });
 }
 
 main();
