f1cbb63ff5e3a7ec7f218df67df36aa2ec1004dc
sebakraus150 <sebakraus93@gmail.com>
2022-10-18 20:58:22 -0300

    desafio del modulo 2 resuelto

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,40 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  peliculas: PelisCollection
+  constructor(){
+    this.peliculas = new PelisCollection
+  }
+  async get(options){
+    if (options.id){
+      return await this.peliculas.getById(options.id)
+    }
+    if (options.search){
+      return await this.peliculas.search(options.search)
+    }
+    if (options.nada){
+      return await this.peliculas.getAll()
+    }
+  }
+  async add(peli:Peli){
+    await this.peliculas.add(peli)
+  }
+
+ /*  peliculas: PelisCollection
+  promesa: Promise <any>
+  constructor() {
+    this.peliculas = new PelisCollection();
+    const promesa = this.peliculas.getAll();
+    this.promesa = promesa
+  }
+  processOptions(options: PelisControllerOptions){
+    var resultado;
+    if (options.params.id){
+      resultado = this.peliculas.getById(options.params.id);
+    } else if (options.action == "add"){
+      resultado = this.peliculas.add(options.action)
+    }
+  } */
+
 }
 export { PelisController };
