e3dc02679e8d3eba5cc58662b103422636f7dd77
norbertobaez <norbertodanielbaez@yahoo.com.ar>
2022-10-17 22:01:27 -0300

    primer commit

--- index.ts
+++ index.ts
@@ -1,15 +1,33 @@
+import { PelisController } from "./controllers";
 import * as minimist from "minimist";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
-
+  ejecutar(resultado);
   return resultado;
+}
+
+async function ejecutar(params:any) {
+  const controller = new PelisController();
+  if (params._[0] == "search") {
+    return controller.get({ search: params });
+  }
+  if (params._[0] == "get") {
+    return controller.get({ id: params._[1] });
+  }
+  if (params._[0] == "add") {
+    return controller.add(params).then((respuesta) => {
+      return respuesta;
+    });
+  }
+  return controller.collection.getAll();
 }
 
 function main() {
   const params = parseaParams(process.argv.slice(2));
-
-  console.log(params);
+  ejecutar(params).then((res) => {
+    console.log(res);
+  });
 }
 
 main();
--- models.ts
+++ models.ts
@@ -8,11 +8,60 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  async getAll(): Promise<Peli[]> {
+    return await jsonfile.readFile(__dirname + "/pelis.json").then((respuesta) => {
+        
+        return respuesta;
+      });
+  }
+  async getById(id: number): Promise<Peli> {
+    const pelis = await this.getAll();
+    const peliConId = pelis.find((i) => i.id == id);
+    return peliConId;
+  }
+
+  async search(options: any): Promise<any> {
+    const pelis = await this.getAll();
+
+    if (options.title && options.tag) {
+      const peliTitleTags = pelis.filter((item) => {
+        return (
+          item.title.includes(options.title) && item.tags.includes(options.tag)
+        );
+      });
+
+      return peliTitleTags;
+    } else if (options.title) {
+      const peliConTitle = pelis.filter((item) => {
+        return item.title.includes(options.title);
+      });
+      return peliConTitle;
+    } else if (options.tag) {
+      const peliConTags = pelis.filter((item) => {
+        return item.tags.includes(options.tag);
+      });
+      return peliConTags;
+    }
+  }
+  async add(peli: Peli): Promise<boolean> {
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+      if (peliExistente) {
+        return false;
+      } else {
+        
+        return this.getAll().then((peliculas) => {
+          peliculas.push(peli);
+          const promesaDos = jsonfile.writeFile("./pelis.json", peliculas);
+
+          return promesaDos.then(() => {
+            return true;
+          });
+        });
+      }
     });
+
+    return promesaUno;
   }
 }
+
 export { PelisCollection, Peli };
