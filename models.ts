9b6d8c71f00c54321c6103e6749ca4b80eaa519a
Emanuel Caceres <emanuelcaceres010@gmail.com>
2022-10-26 15:03:53 -0300

    desafio terminado

--- models.ts
+++ models.ts
@@ -8,11 +8,40 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  async getAll(): Promise<Peli[]> {
+    return await jsonfile.readFile("./pelis.json"); 
+  }
+  async getById(id:number):Promise<Peli> {
+    const peliculas = await this.getAll();
+    return peliculas.find(peli => {
+      return peli.id == id;
     });
   }
+  async search(options?:any):Promise<Peli[]> {
+    const peliculas = await this.getAll();
+    if (options.title){
+      return peliculas.filter(peli =>{
+        return peli.title.includes(options.title);
+      });
+    }
+      if (options.tags){
+        return peliculas.filter(peli => {
+          return peli.tags.includes(options.tags);
+        });
+      }
+    }
+    async add(peli:Peli): Promise<boolean> {
+      const resultado = await this.getById(peli.id);
+      if (resultado){
+        return false;
+      }
+      else {
+        const pelis = await this.getAll();
+        pelis.push(peli);
+        await jsonfile.writeFile('./pelis.json', pelis);
+        return true;
+      }
+    }
 }
+
 export { PelisCollection, Peli };
