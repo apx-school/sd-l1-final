28b26a486c10e47289beb370e5da3db7d38d7602
Mervin González <gonzalez.mervin@gmail.com>
2022-11-18 20:56:32 -0300

    tercer commit

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,39 @@
 import { PelisCollection, Peli } from "./models";
 
-class PelisController {
-  constructor() {}
+class PelisControllerOptions {
+  id: number;
+  search: {
+    title: string;
+    tag: string[];
+  };
 }
-export { PelisController };
+
+class PelisController {
+  pelisCollection: PelisCollection;
+  constructor() {
+    const pelisCollection = new PelisCollection();
+    this.pelisCollection = pelisCollection;
+  }
+  get(options: PelisControllerOptions) {
+    if (options.id) {
+      return this.pelisCollection.getById(options.id);
+    } else if (options.search) {
+      if (options.search.title) {
+        return this.pelisCollection.search(options.search.title);
+      } else if (options.search.tag) {
+        return this.pelisCollection.search(options.search.tag);
+      } else if (options.search.title && options.search.tag) {
+        return (
+          this.pelisCollection.search(options.search.title) &&
+          this.pelisCollection.search(options.search.tag)
+        );
+      } else {
+        return this.pelisCollection.getAll();
+      }
+    }
+  }
+  add(peli: Peli) {
+    return this.pelisCollection.add(peli);
+  }
+}
+export { PelisController, PelisControllerOptions };
