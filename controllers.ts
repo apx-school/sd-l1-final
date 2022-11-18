9f59e2d8373ab35ce1e5c9ec859e629266b793a0
Joaquin <joaquinvamonde7@gmail.com>
2022-11-18 10:55:41 -0300

    los test pasaron

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,24 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  pelis: PelisCollection;
+  constructor() {
+    this.pelis = new PelisCollection();
+  }
+  get(options) {
+    if (options._ == "") {
+      return this.pelis.getAll();
+    } else if (options.id) {
+      return this.pelis.getById(options.id);
+    } else if (options.add) {
+      return this.pelis.add(options.add);
+    } else if (options.search) {
+      return this.pelis.search(options.search);
+    }
+  }
+  add(peli: Peli) {
+    return this.pelis.add(peli);
+  }
 }
+
 export { PelisController };
