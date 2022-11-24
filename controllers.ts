0d53d791f6fcb467eae157dba2ac732f62fbf27c
Matias Gallardo <matias.jgallardo@gmail.com>
2022-11-24 18:32:47 -0300

    1er commit desafio-m2

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,47 @@
 import { PelisCollection, Peli } from "./models";
 
-class PelisController {
-  constructor() {}
+class ControllerOptions{
+  id?: number;
+  search?: {title?:string,tag?:string}
 }
-export { PelisController };
+
+class PelisController {
+  constructor() {
+    this.pelis = new PelisCollection();
+    this.promesa = this.pelis.load();
+  }
+
+  pelis: PelisCollection;
+  promesa: Promise<any>;
+
+  async get(options?: ControllerOptions): Promise<any>{
+    await this.promesa;
+    let resultado: any;
+    if(!options) {resultado = await this.pelis.getAll()}else{
+      if (options.id) resultado = await this.pelis.getById(options.id);
+      if (options.search){
+        if(options.search.title) this.pelis.data = await this.pelis.search({title: options.search.title});
+        if(options.search.tag) this.pelis.data = await this.pelis.search({tag: options.search.tag});
+        resultado = this.pelis.data;
+        await this.pelis.load();
+      }};
+    return await resultado;
+    }
+
+    async add(peli: Peli){
+      await this.promesa;
+      const resultado = await this.pelis.add(peli);
+      return resultado;
+    }
+  }
+
+export { PelisController, ControllerOptions};
+
+// async function main(){
+//   const foo = new PelisController();
+//   console.log(await foo.get({id: 24}));
+//   console.log(await foo.get({search: {title:"THE", tag: "drama"}}));
+//   console.log(await foo.get({search: {tag: "ACCIÓN"}}));
+// }
+
+// main();
--- models.ts
+++ models.ts
@@ -1,18 +1,68 @@
-import * as jsonfile from "jsonfile";
+import * as js from "jsonfile";
 
-// no modificar estas propiedades, agregar todas las que quieras
+const jsonPath = __dirname + '/pelis.json';
+
 class Peli {
   id: number;
   title: string;
   tags: string[];
+  rating: number;
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
-    });
+  
+  data: Peli[];
+
+  async load(): Promise<any>{
+    this.data = await js.readFile(jsonPath);
+  }
+
+  async getAll(): Promise<Peli[]> {
+    return this.data;
+  }
+
+  async getById(id:number): Promise<Peli>{
+    const getResult =  this.data.find(e => e.id == id);
+    return getResult;
+  }
+
+  async search(options: {title: string} | {tag: string}): Promise<Peli[]>{
+    const optionsKey = Object.keys(options)[0];
+    var searchResult = [];
+
+    (optionsKey == "title") ?
+    this.data.forEach(e=> {if(e.title.toUpperCase().includes(options[optionsKey].toUpperCase())) searchResult.push(e)}):
+    (optionsKey == "tag") ?
+    this.data.forEach(e=>{e.tags.forEach(t=> {if(t.toUpperCase() == options[optionsKey].toUpperCase())searchResult.push(e)})}):
+    null;
+
+    if(searchResult.length == 0){
+      console.log(`La busqueda no arrojó ningún resultado`);
+      return;
+    }
+    return searchResult;
+  }
+
+  async add(peli: Peli): Promise<boolean>{
+    if(await this.getById(peli.id)) {
+      return false;
+    }else{
+     this.data.push(peli);
+     await js.writeFile(jsonPath,this.data);
+     return true;
+    }
   }
 }
+
 export { PelisCollection, Peli };
+
+// async function main(){
+//   const foo = new PelisCollection();
+//   await foo.load();
+//   console.log(foo.data);
+//   (await foo.add({id:31,title:'One Piece: Red',tags:["acción","animé","humor","piratas","fantasía"],rating: 9.8})) ?
+//   console.log('Se agrego la peli') :
+//   console.log('No se agregó la peli');
+// }
+
+// main();
