73b5a2f46a61721ab3b2bbf41a65f06d8f794d91
silviojrcc <silviojrcc@hotmail.com>
2023-01-03 23:08:21 -0300

    pasando los test

--- index.ts
+++ index.ts
@@ -1,15 +1,32 @@
 import * as minimist from "minimist";
+import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
-
   return resultado;
 }
 
-function main() {
-  const params = parseaParams(process.argv.slice(2));
+function processOptions(argumentos) {
+  const pelisController = new PelisController();
+  const params = argumentos._[0];
+  if (params == "get") {
+    return pelisController.get({id: argumentos._[1]});
+  }
+  if (params == "search") {
+    return pelisController.get({search: argumentos});
+  }
+  if (params == "add") {
+    return pelisController.add({id: argumentos.id, title: argumentos.title, tags: argumentos.tags})
+  }
+  if (!params) {
+    return pelisController.pelis.getAll();
+  }
+}
 
-  console.log(params);
+async function main() {
+  const params = parseaParams(process.argv.slice(2));
+  const resultado = await processOptions(params);
+  console.log(resultado);
 }
 
 main();
