3e1f81fc388431ade43334eeb36b5c66d6291586
Juan Bautista <jv604824@gmail.com>
2023-02-02 21:52:16 -0300

    Primer commit de Juan

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,13 @@
+import { type } from "os";
 import { PelisCollection, Peli } from "./models";
-
+type Options = {
+  id?: number;
+  search?: {
+    title?: string;
+    tag?: string;
+  };
+};
 class PelisController {
-  constructor() {}
+  constructor() { }
 }
-export { PelisController };
+export { PelisController, Options };
--- models.ts
+++ models.ts
@@ -1,6 +1,7 @@
 import * as jsonfile from "jsonfile";
+import { find, includes } from "lodash"
 
-// no modificar estas propiedades, agregar todas las que quieras
+type SearchOptions = { title?: any; tag?: string };
 class Peli {
   id: number;
   title: string;
@@ -8,11 +9,48 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  pelis: Peli[] = [];
+  async getAll(): Promise<Peli[]> {
+    return await jsonfile.readFile("./pelis.json").then((pelis) => {
+      this.pelis = pelis;
+      return pelis;
     });
+  };
+  async getById(id: number): Promise<Peli> {
+    await this.getAll()
+    const buscarId = find(this.pelis, (p) => p.id == id)
+    return buscarId;
+  };
+  async add(peli: Peli): Promise<boolean> {
+    await this.getAll();
+    if (await this.getById(peli.id)) {
+      return false;
+    } else {
+      this.pelis.push(peli);
+      await jsonfile.writeFile("./pelis.json", this.pelis);
+      return true;
+    }
+  };
+  async search(options: SearchOptions) {
+
+    const lista = await this.getAll();
+    const listaFiltrada = lista.filter((peli) => {
+      let esteVa = false;
+      if (options.tag) {
+        esteVa = includes(peli.tags, options.tag)
+      } else if (options.title) {
+        esteVa = includes(peli.title.charAt(0).toLowerCase(), options.title)
+      }
+      return esteVa;
+
+    });
+    if (options.tag && options.title) {
+      return listaFiltrada.filter((peli) => {
+        return includes(peli.title.charAt(0).toLowerCase(), options.title)
+      });
+    }
+    console.log(listaFiltrada);
+
   }
 }
 export { PelisCollection, Peli };
