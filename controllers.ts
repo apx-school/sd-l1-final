f302f9d47a9143e1d3556d5b93c1fefcf0924113
Juan Bautista <jv604824@gmail.com>
2023-02-08 21:03:04 -0300

    Desafío Modulo 2 Terminado

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,31 @@
+import { type } from "os";
+import {includes} from "lodash"
 import { PelisCollection, Peli } from "./models";
-
+type Options = {
+  id?: number;
+  search?: {
+    title?: string;
+    tag?: string;
+  };
+};
 class PelisController {
-  constructor() {}
+  pelis: PelisCollection;
+  constructor() {
+    this.pelis = new PelisCollection();
+    this.pelis.getAll()
+  }
+  async get(options?: Options) {  
+    if (options.id) {
+      return this.pelis.getById(options.id);
+    } else if (options.search) {
+    return this.pelis.search(options.search);
+    }
+  }
+  async add(peli: Peli): Promise<any> {    
+    return this.pelis.add(peli);
+  }
 }
-export { PelisController };
+export { PelisController, Options };
+
+
+
