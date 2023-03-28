ca51987d70ea14dff0d86c22ec68fb3ffe42f6a9
Thiago <thiagosalaberry99@gmail.com>
2023-03-28 20:02:27 -0300

    desafio completado

--- index.ts
+++ index.ts
@@ -1,15 +1,30 @@
 import * as minimist from "minimist";
-
-function parseaParams(argv) {
+import { PelisController } from "./controllers";
+function parseaParams(argv:string[]) {
   const resultado = minimist(argv);
-
   return resultado;
 }
-
-function main() {
-  const params = parseaParams(process.argv.slice(2));
-
-  console.log(params);
+const optionsController = async (objeto:any) => {
+  const controlador = new PelisController()
+  let resultado = await controlador.get()
+  if(Object.values(objeto)[0][0] == "get") {
+      resultado = await controlador.get({id: Object.values(objeto)[0][1]})
+  } else if(Object.values(objeto)[0][0] == "search" && objeto.title && objeto.tag) {
+      resultado = await controlador.get({search: {title: objeto.title, tag: objeto.tag}})
+  } else if(Object.values(objeto)[0][0] == "search" && objeto.title) {
+      resultado = await controlador.get({search: {title: objeto.title}})
+  } else if(Object.values(objeto)[0][0] == "search" && objeto.tag) {
+      resultado = await controlador.get({search: {tag: objeto.tag}})
+  } else if(Object.values(objeto)[0][0] == "add") {
+      resultado = await controlador.add({id: objeto.id, title: objeto.title, tags: objeto.tags})
+  }
+  return resultado
 }
 
-main();
+async function main() {
+  const input = parseaParams(process.argv.slice(2))
+    const resultado = await optionsController(input)
+    console.log(resultado);
+}
+
+main()
