c369677e0e7482a6cb3b85700568686136a03221
Grahl <Mgrahl@emergencias.com.ar>
2023-03-30 18:12:40 -0300

    Cambios en los archivos

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,44 @@
 import { PelisCollection, Peli } from "./models";
 
+type Options = {
+  id?: number;
+  search?: {
+    title?: string;
+    tag?: string;
+  };
+};
+
 class PelisController {
-  constructor() {}
+  pelis: PelisCollection;
+
+  constructor() {
+    this.pelis = new PelisCollection();
+  }
+
+  async get(options?: Options): Promise<any> {
+    console.log(`options: ${options}`);
+
+    if (Object.keys(options).length == 0) {
+      return await this.pelis.getAll();
+    }
+
+    if (options.id) {
+      return await this.pelis.getById(options.id);
+    }
+
+    if (options.search) {
+      return await this.pelis.search(options.search);
+    }
+  }
+
+  async add(peli: Peli): Promise<boolean> {
+    return await this.pelis.add(peli);
+  }
 }
+
+/* PRUEBAS:
+//  */
+// const peliController = new PelisController();
+// peliController.pelis.getAll().then((res) => console.table(res));
+
 export { PelisController };
--- index.ts
+++ index.ts
@@ -1,15 +1,57 @@
 import * as minimist from "minimist";
+import * as _ from "lodash";
+import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
-
   return resultado;
 }
 
-function main() {
-  const params = parseaParams(process.argv.slice(2));
+async function processOptions(params) {
+  const controller = new PelisController();
+  console.log(`processOptions params: ${params}`);
 
-  console.log(params);
+  // ts-node index.ts add --id=4321865 --title="peli de la terminal 4321865" --tags=rr --tags=ww && ava
+  if (params._[0] == "add") {
+    return await controller
+      .add({
+        id: params.id,
+        title: params.title,
+        tags: params.tags,
+      })
+      .then((res) => {
+        return res;
+      });
+  }
+
+  // Prueba: ts-node index.ts get --id=2
+  if (params._[0] == "get") {
+    return await controller.get({ id: params._[1] });
+  }
+
+  // Prueba: ts-node index.ts search --title="a"
+  if (params._[0] == "search") {
+    return controller.get({
+      search: { title: params.title, tag: params.tags },
+    });
+  }
+
+  // Prueba: ts-node index.ts
+  if (_.isEmpty(params._[0])) {
+    return controller.get({});
+  }
+}
+
+function main() {
+  console.log(`process: ${process}`);
+
+  const params = parseaParams(process.argv.slice(2));
+  console.log(`index params: ${params}`);
+
+  return processOptions(params).then((res) => {
+    console.table(`index respuesta: ${res}`);
+    return res;
+  });
 }
 
 main();
