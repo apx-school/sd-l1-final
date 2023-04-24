ac3b407bc6ec6bb554592f4f77619040ba3dde06
Pablo Cogno <pablocogno.5@gmail.com>
2023-04-24 19:50:56 -0300

    desafio completo

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,56 @@
 import { PelisCollection, Peli } from "./models";
 
+type Options = {
+  id?: number;
+  search?: {
+    title?: string;
+    tag?: string;
+  };
+};
+
 class PelisController {
-  constructor() {}
+  movies: PelisCollection;
+  promesa: Peli[];
+
+  constructor() {
+    (async () => {
+      this.movies = new  PelisCollection();
+      const promesa = await this.movies.getAll()
+      this.promesa = promesa;
+      
+  })();
+
+    
+  }
+  async add(peli: Peli){
+    const process = await this.movies.add(peli)
+    if (process) {
+      console.log("Se resolvio exitosamente")
+    } else {
+      console.log("Se resolvio desfavorablemente")
+    }
+  }
+
+  async get(options?:Options){
+    var resultado;
+    if (options.id) {
+      resultado = await this.movies.getById(options.id);
+      return resultado;
+    } else if(options.search){
+      if (options.search.title && options.search.tag) {
+        resultado = await this.movies.search(options.search)
+        return resultado;
+      }else if (options.search.title) {
+        resultado = await this.movies.search(options.search)
+        return resultado;
+      } else if(options.search.tag){
+        resultado = await this.movies.search(options.search)
+        return resultado
+      }
+    } else {
+      resultado = await this.movies.getAll()
+      return resultado;
+    }
+  }
 }
 export { PelisController };
--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import * as minimist from "minimist";
+import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
@@ -6,10 +7,20 @@ function parseaParams(argv) {
   return resultado;
 }
 
-function main() {
+async function main() {
   const params = parseaParams(process.argv.slice(2));
-
-  console.log(params);
+  const paramsLimpio = limpiarObjeto(params);
+  const controller = new PelisController();
+  await controller.add(paramsLimpio)
 }
 
 main();
+
+function limpiarObjeto(obj: any) {
+  const objetoLimpio = {
+    id: obj.id,
+    title: obj.title,
+    tags: obj.tags
+  };
+  return objetoLimpio;
+}
