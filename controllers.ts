a848b969628a7231bbb65e20b922beb75a635052
Enzo saitta <enzosaitta@MacBook-Pro-de-Enzo.local>
2023-05-16 02:14:45 -0300

    falla controller get id

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,29 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  pelisCollection: PelisCollection;
+  constructor() {
+    this.pelisCollection = new PelisCollection();
+  }
+  async get(options: any) {
+    if (options._ == "") {
+      return this.pelisCollection.getAll();
+    }
+    if (options.id) {
+      return this.pelisCollection.getById(options.id);
+    } else if (options.search) {
+      if (options.add) {
+        return this.pelisCollection.add(options.add);
+      }
+      if (options.search) {
+        return this.pelisCollection.search(options.search);
+      }
+    }
+  }
+
+  add(peli: Peli) {
+    return this.pelisCollection.add(peli);
+  }
 }
+
 export { PelisController };
--- index.ts
+++ index.ts
@@ -1,15 +1,29 @@
 import * as minimist from "minimist";
+import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
-
+  if (resultado._[0] == "get") {
+    return { id: resultado._[1] };
+  } else if (resultado._[0] == "add") {
+    return {
+      add: { id: resultado.id, title: resultado.title, tags: resultado.tags },
+    };
+  } else if (resultado._[0] == "search") {
+    if (resultado.title && resultado.tag) {
+      return { search: { title: resultado.title, tag: resultado.tag } };
+    } else if (resultado.title) {
+      return { search: { tile: resultado.title } };
+    } else if (resultado.tag) {
+      return { search: { tag: resultado.tag } };
+    }
+  }
   return resultado;
 }
 
 function main() {
   const params = parseaParams(process.argv.slice(2));
-
-  console.log(params);
+  const peliculas = new PelisController();
+  peliculas.get(params).then((respuesta) => console.log(respuesta));
 }
-
 main();
