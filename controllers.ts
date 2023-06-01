d4fe71a5dfc7c140a5bf71b3bd1a9175e030f83d
Lucas <lucas.82@hotmail.com.ar>
2023-06-01 10:16:49 -0300

    Pasan todos los test

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,28 @@
 import { PelisCollection, Peli } from "./models";
 
+type Options = {
+  id?: number;
+  search?: {
+    title?: string;
+    tag?: string;
+  };
+};
+
 class PelisController {
-  constructor() {}
+  pelis: PelisCollection;
+  constructor() {
+    this.pelis = new PelisCollection();
+  }
+  async get(options?: Options): Promise<any> {
+    if (options.id) {
+      return await this.pelis.getById(options.id);
+    } else if (options.search) {
+      return await this.pelis.search(options.search);
+    }
+  }
+  async add(peli: Peli) {
+    return await this.pelis.add(peli);
+  }
 }
-export { PelisController };
+
+export { PelisController, Options };
--- models.ts
+++ models.ts
@@ -1,6 +1,5 @@
 import * as jsonfile from "jsonfile";
 
-// no modificar estas propiedades, agregar todas las que quieras
 class Peli {
   id: number;
   title: string;
@@ -8,11 +7,54 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  async getAll(): Promise<Peli[]> {
+    return jsonfile.readFile(__dirname + "/pelis.json").then((p) => {
+      return p;
     });
   }
+
+  async getById(id: number) {
+    return this.getAll().then((pelis) => {
+      const resultado = pelis.find((p) => {
+        return p.id == id;
+      });
+      return resultado;
+    });
+  }
+
+  async add(peli: Peli): Promise<boolean> {
+    return this.getById(peli.id).then((peliExistente) => {
+      if (peliExistente) {
+        return false;
+      } else {
+        return this.getAll().then((data) => {
+          data.push(peli);
+          const promesaDos = jsonfile.writeFile("./pelis.json", data);
+          return promesaDos.then(() => {
+            return true;
+          });
+        });
+      }
+    });
+  }
+
+  async search(options) {
+    const lista = await this.getAll();
+
+    const listraFiltrada = lista.filter(function (p) {
+      if (options.tag && options.title) {
+        return p.tags.includes(options.tag) && p.title.includes(options.title);
+      }
+      if (options.tag) {
+        return p.tags.includes(options.tag);
+      }
+      if (options.title) {
+        return p.title.includes(options.title);
+      }
+    });
+
+    return listraFiltrada;
+  }
 }
+
 export { PelisCollection, Peli };
