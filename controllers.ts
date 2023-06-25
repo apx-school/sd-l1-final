2eb13e85519a41548bafe53207cdfb69a969a613
Facundo Guardia <101948163+facuguardia@users.noreply.github.com>
2023-06-25 19:07:07 -0300

    desafio-n3-poo

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,32 @@
 import { PelisCollection, Peli } from "./models";
 
+type Options = {
+  id?: number;
+  search?: {
+    title?: string;
+    tag?: string;
+  };
+};
+
 class PelisController {
-  constructor() {}
+  pelis: PelisCollection;
+  constructor() {
+    this.pelis = new PelisCollection();
+  }
+
+  async get(options: Options): Promise<any> {
+    if (options.id) {
+      return this.pelis.getById(options.id);
+    } else if (options.search) {
+      return this.pelis.search(options.search);
+    } else {
+      return this.pelis.getAll();
+    }
+  }
+
+  async add(peli: Peli): Promise<boolean> {
+    return this.pelis.add(peli);
+  }
 }
-export { PelisController };
+
+export { PelisController, Options };
--- models.ts
+++ models.ts
@@ -1,6 +1,5 @@
 import * as jsonfile from "jsonfile";
 
-// no modificar estas propiedades, agregar todas las que quieras
 class Peli {
   id: number;
   title: string;
@@ -8,11 +7,53 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  // Get all the movies
+  async getAll(): Promise<Peli[]> {
+    const pelis = await jsonfile.readFile("./pelis.json");
+    return pelis;
+  }
+
+  // Get a movie by id
+  async getById(id: number): Promise<Peli | undefined> {
+    const pelis = await this.getAll();
+    const peli = pelis.find((p) => {
+      return p.id == id;
     });
+    return peli;
+  }
+
+  // Add a movie
+  async add(peli: Peli): Promise<boolean> {
+    const pelis = await this.getAll();
+    const peliExists = pelis.find((p) => {
+      return p.id == peli.id;
+    });
+    if (peliExists) {
+      return false;
+    } else {
+      pelis.push(peli);
+      await jsonfile.writeFile("./pelis.json", pelis);
+      return true;
+    }
+  }
+
+  // Search a movie
+  async search(options: any): Promise<any> {
+    const pelis = await this.getAll();
+    if (options.title && options.tag) {
+      return pelis.filter((p) => {
+        return p.title.includes(options.title) && p.tags.includes(options.tag);
+      });
+    } else if (options.title) {
+      return pelis.filter((p) => {
+        return p.title.includes(options.title);
+      });
+    } else if (options.tag) {
+      return pelis.filter((p) => {
+        return p.tags.includes(options.tag);
+      });
+    }
   }
 }
+
 export { PelisCollection, Peli };
