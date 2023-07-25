9a7e997257d8c915035daa0f9e8a59c0cda78cbc
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-07-25 04:09:40 -0300

    desafio PELIS POO finalizado

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,41 @@
 import { PelisCollection, Peli } from "./models";
-
+type Options = {
+  id?:number
+  search?:{
+    title?:string
+    tag?:string
+  }
+}
 class PelisController {
-  constructor() {}
+  movies: PelisCollection
+  constructor() {
+    this.movies = new PelisCollection()
+  }
+
+  async get(options?:Options) {
+    
+    if(!options) return await this.movies.getAll()
+    
+    if (options.id) {
+      return await this.movies.getById(options.id).then(res => {return res})
+    } 
+    if(options.search) {
+      if (options.search.tag && options.search.title) {
+        const moviesFiltered = await this.movies.search(options.search)
+        return moviesFiltered
+      }
+      if (options.search.tag) {
+        const moviesFiltered = await this.movies.search(options.search)
+        return moviesFiltered
+      }
+      if (options.search.title) {
+        const moviesFiltered = await this.movies.search(options.search)
+        return moviesFiltered
+      }
+    } 
+  }
+  async add(peli:Peli) {
+    return await this.movies.add(peli)
+  }
 }
 export { PelisController };
