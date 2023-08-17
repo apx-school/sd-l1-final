0f50ea280379a2d59ed7f472cae2c4a975bab67d
Mariano Barrera <marianobarrera16@gmail.com>
2023-08-17 10:48:50 -0300

    first commit

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,34 @@
 import { PelisCollection, Peli } from "./models";
 
+type Options = {
+  id?: number;
+  search?: {
+    title?: string;
+    tag?: string;
+  };
+};
+
 class PelisController {
-  constructor() {}
+  pelis: PelisCollection;
+  constructor() {
+    this.pelis = new PelisCollection();
+  }
+
+  async get(options?: Options) {
+    let resultado;
+    if (!options) {
+      resultado = await this.pelis.getAll();
+    } else if (options.id) {
+      resultado = await this.pelis.getById(options.id);
+    } else if (options.search) {
+      resultado = await this.pelis.search(options.search);
+    }
+    return resultado;
+  }
+
+  add(peli: Peli) {
+    return this.pelis.add(peli);
+  }
 }
+
 export { PelisController };
--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import * as minimist from "minimist";
+import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
@@ -6,10 +7,31 @@ function parseaParams(argv) {
   return resultado;
 }
 
-function main() {
-  const params = parseaParams(process.argv.slice(2));
+function optionsController(params) {
+  const controller = new PelisController();
 
-  console.log(params);
+  if (params._[0] === "add") {
+    delete params._;
+    return controller.add(params).then((r) => console.log(r));
+  }
+
+  if (params._[0] === "get") {
+    return controller.get({ id: params._[1] }).then((r) => console.log(r));
+  }
+
+  if (params._[0] === "search") {
+    delete params._;
+    return controller.get({ search: params }).then((r) => console.log(r));
+  }
+
+  if (params._) {
+    return controller.get().then((r) => console.log(r));
+  }
+}
+
+async function main() {
+  const params = parseaParams(process.argv.slice(2));
+  optionsController(params);
 }
 
 main();
