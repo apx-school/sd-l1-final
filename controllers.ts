18f1996c900cf6acf0729c7fea6f9b81873d11cc
Eze <Riccaesteban@gmail.com>
2023-08-28 14:47:34 -0300

    mi primer commit

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,40 @@
 import { PelisCollection, Peli } from "./models";
+type Options = {
+  id?: number;
+  search?: {
+    title?: string;
+    tag?: string;
+  };
+};
 
 class PelisController {
-  constructor() {}
+  peliculas: PelisCollection;
+  constructor() {
+    this.peliculas = new PelisCollection();
+  }
+  async add(peli: Peli) {
+    return await this.peliculas.add(peli);
+  }
+
+  async get(options?: Options) {
+    if (options == null) {
+      return await this.peliculas.getAll();
+    }
+    if (options.id) {
+      return await this.peliculas.getById(options.id);
+    }
+    if (options.search.title && options.search.tag) {
+      return await this.peliculas.search(options.search);
+    }
+    if (options.search.title) {
+      return await this.peliculas.search(options.search);
+    }
+    if (options.search.tag) {
+      return await this.peliculas.search(options.search);
+    }
+  }
 }
+const probandoController = new PelisController();
+// probandoController.get().then((res) => console.log(res));
+probandoController.get();
 export { PelisController };
--- models.ts
+++ models.ts
@@ -6,13 +6,77 @@ class Peli {
   title: string;
   tags: string[];
 }
+type SearchOptions = {
+  tag?: string;
+  title?: string | number;
+};
 
 class PelisCollection {
+  data: Peli[] = [];
+  load() {
+    const promesa = jsonfile.readFile(__dirname + "/pelis.json");
+    const json = promesa;
+    return json;
+  }
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json").then((res) => {
+      return res;
     });
   }
+  async getById(id: number): Promise<Peli> {
+    const peliculas = await this.getAll();
+    const pelicula = peliculas.find((peli) => peli.id == id);
+    return pelicula;
+  }
+  add(peli: Peli): Promise<boolean> {
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+      if (peliExistente) {
+        return false;
+      } else {
+        return jsonfile.readFile(__dirname + "/pelis.json").then((data) => {
+          data.push(peli);
+          return jsonfile
+            .writeFile(__dirname + "/pelis.json", data)
+            .then(() => {
+              return true;
+            });
+        });
+      }
+    });
+    return promesaUno;
+  }
+  async search(options: SearchOptions) {
+    const lista = await this.getAll();
+    if (options.tag && options.title) {
+      return lista.filter((peli) => {
+        return (
+          peli.tags.includes(options.tag) &&
+          peli.title.includes(options.title.toString())
+        );
+      });
+    }
+    if (options.title) {
+      return lista.filter((peli) => {
+        return peli.title.includes(options.title.toString());
+      });
+    }
+    if (options.tag) {
+      return lista.filter((peli) => {
+        return peli.tags.includes(options.tag);
+      });
+    }
+  }
 }
+const probando = new PelisCollection();
+// probando.search({ title: "El diario de Noa" }).then((res) => console.log(res));
+
+// const unaPeli = {
+//   id: 4,
+//   title: "probando",
+//   tags: ["Drama", "Romance"],
+// };
+// probando.add(unaPeli);
+// console.log(probando.getData());
+// probando.getAll().then((res) => console.log(res));
+// probando.getById(1).then((res) => console.log(res));
 export { PelisCollection, Peli };
