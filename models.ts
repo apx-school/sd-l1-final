eeebe4d4e1420f6abc063513b1e602d9bb5468d3
Noamreyes2022 <reyes.noam@gmail.com>
2023-09-14 07:00:37 -0300

    Agregando nuevas funciones

--- models.ts
+++ models.ts
@@ -1,18 +1,52 @@
 import * as jsonfile from "jsonfile";
 
-// no modificar estas propiedades, agregar todas las que quieras
-class Peli {
+type Peli = {
   id: number;
   title: string;
   tags: string[];
-}
+};
 
 class PelisCollection {
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json");
+  }
+
+  getById(id: number) {
+    return this.getAll().then((pelis) => {
+      return pelis.find((peli) => peli.id == id);
+    });
+  }
+
+  search(options: any): Promise<Peli[]> {
+    return this.getAll().then((pelis) => {
+      if (options.title && options.tag) {
+        return pelis.filter((peli) => {
+          return (
+            peli.title.includes(options.title) &&
+            peli.tags.includes(options.tag)
+          );
+        });
+      } else if (options.title) {
+        return pelis.filter((peli) => peli.title.includes(options.title));
+      } else if (options.tag) {
+        return pelis.filter((peli) => peli.tags.includes(options.tag));
+      }
+    });
+  }
+
+  add(peli: Peli): Promise<boolean> {
+    return this.getAll().then((pelis) => {
+      const existe = pelis.find((p) => p.id == peli.id);
+      if (existe) {
+        return false;
+      } else {
+        pelis.push(peli);
+        return jsonfile.writeFile("./pelis.json", pelis).then(() => {
+          return true;
+        });
+      }
     });
   }
 }
+
 export { PelisCollection, Peli };
