ff921e1cb7a7ae6bdf1d163064d68b322ffa09bc
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2023-11-08 17:24:11 -0300

    primer intento de nuevo fork, ava sigue sin andar

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,69 @@
 import { PelisCollection, Peli } from "./models";
 
-class PelisController {
-  constructor() {}
+export type Options = {
+  id?: number;
+  search?: {
+    title?: string;
+    tag?: string;
+  };
+};
+
+export class PelisController {
+  peliCollection: PelisCollection;
+
+  constructor() {
+    this.peliCollection = new PelisCollection();
+  }
+
+  async get(options?: Options) {
+    try {
+      this.peliCollection.data = await this.peliCollection.getAll();
+      if (options) {
+        if (options.id && !options.search) {
+          // Si el objeto tiene la property id, debe devolver la película con ese id.
+          const peli = await this.peliCollection.getById(options.id);
+          if (peli) {
+            return peli;
+          } else {
+            return new Error("Película no encontrada");
+          }
+        } else if (options.search?.title && !options.search.tag) {
+          // Si el objeto search tiene la property title, debe buscar las pelis que tengan ese string en el título.
+          return await this.peliCollection.search({
+            title: options.search.title,
+          });
+        } else if (
+          !options.id &&
+          options.search?.tag &&
+          !options.search.title
+        ) {
+          // Si el objeto search tiene la property tag, debe buscar las pelis que tengan ese tag.
+          return await this.peliCollection.search({ tag: options.search.tag });
+        } else if (options.search?.title && options.search.tag) {
+          // Puede recibir las dos opciones.
+          return await this.peliCollection.search({
+            title: options.search.title,
+            tag: options.search.tag,
+          });
+        }
+      }
+      // Si no recibe ningún parámetro, debe devolver todas las películas.
+      return this.peliCollection;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async add(peli: Peli) {
+    try {
+      const operationWasSuccesfull = await this.peliCollection.add(peli);
+      if (operationWasSuccesfull) {
+        console.log("Lista actualizada exitosamente: ");
+        const nuevaLista = await this.get();
+        return nuevaLista;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
 }
-export { PelisController };
--- models.ts
+++ models.ts
@@ -7,12 +7,65 @@ class Peli {
   tags: string[];
 }
 
+type SearchOptions = { title?: string; tag?: string };
+
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
-    });
+  data: Peli[] = [];
+
+  // Devuelve un array del tipo Peli con todas las pelis que se encuentren guardadas en el archivo JSON.
+  async getAll(): Promise<Peli[]> {
+    try {
+      const data = await jsonfile.readFile(__dirname + "/pelis.json");
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async add(peli: Peli): Promise<boolean> {
+    // Recibe una Peli y la guarda en el archivo.
+    try {
+      const peliExistente = await this.getById(peli.id);
+
+      // No debe admitir agregar IDs repetidos.
+      if (peliExistente) {
+        return false;
+      } else {
+        const peliList = await this.getAll();
+        peliList.push(peli);
+        await jsonfile.writeFile(__dirname + "/pelis.json", peliList);
+        return true;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // Devuelve la peli con el id que se le pase por parámetro.
+  async getById(id: number): Promise<Peli> {
+    try {
+      const allPelis = await this.getAll();
+      const foundPeli = allPelis.find((peli) => {
+        return peli.id === id;
+      });
+      return foundPeli;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async search(options: SearchOptions): Promise<Peli[]> {
+    try {
+      const lista = await this.getAll();
+      const listraFiltrada = lista.filter(
+        (peli) =>
+          peli.tags.includes(options.tag) || peli.title.includes(options.title)
+      );
+      return listraFiltrada;
+    } catch (error) {
+      throw error;
+    }
   }
 }
+
 export { PelisCollection, Peli };
