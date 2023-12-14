97ca3a3815a33b4fa76dbc8a9e9df8ccc9afeec6
Luciano Riente <lucianotomasriente@gmail.com>
2023-12-14 02:53:05 -0300

    Desafio terminado

--- models.ts
+++ models.ts
@@ -4,49 +4,77 @@ import * as jsonfile from 'jsonfile';
 class Peli {
 	title: string;
 	id: number;
-	rating: number;
+	rating?: number;
 	tags: string[];
-	añoDeLanzamiento: number;
+	añoDeLanzamiento?: number;
+
+	constructor(title: string, id: number, tags: string[], rating?: number, añoDeLanzamiento?: number) {
+		this.title = title;
+		this.id = id;
+		this.rating = rating;
+		this.tags = tags;
+		this.añoDeLanzamiento = añoDeLanzamiento;
+	}
 }
 
 type SearchOptions = { title?: string; tag?: string };
 
 class PelisCollection {
-	// pelis: Peli[] = [];
-	async getAll(): Promise<Peli[]> {
-		return jsonfile.readFile('./pelis.json').then((res) => res);
+	async getAll(): Promise<any> {
+		const pelis = await jsonfile.readFile('./pelis.json');
+		return pelis;
 	}
 	async getById(id: number): Promise<Peli> {
-		return this.getAll().then((pelis) => pelis.find((peli) => peli.id == id));
+		const pelis = await this.getAll();
+		return pelis.find((peli) => peli.id == id);
 	}
 	async search(options: SearchOptions): Promise<Peli[]> {
-		// fijarse de que se puedan combinar los dos parametros, pq ahora si se usan los dos parametros tiene en cuenta el primero unicamente
-		if (options.title) return this.getAll().then((pelis) => pelis.filter((peli) => peli.title.includes(options.title)));
-		if (options.tag) return this.getAll().then((pelis) => pelis.filter((peli) => peli.tags.includes(options.tag)));
+		const pelis = await this.getAll();
+		if (options.title && options.tag) {
+			return pelis.filter((peli) => {
+				const titleDePeliEnLowerCase = peli['title'].toLowerCase();
+				const peliFiltradaByTitle = titleDePeliEnLowerCase.includes(options.title.toLowerCase());
+				const tagsDePeliEnLowerCase = peli.tags.map((tag) => tag.toLowerCase());
+				const peliFiltradaByTag = tagsDePeliEnLowerCase.includes(options.tag.toLowerCase());
+				return peliFiltradaByTitle && peliFiltradaByTag;
+			});
+		} else {
+			if (options.title) {
+				return pelis.filter((peli) => {
+					const titleDePeliEnLowerCase = peli['title'].toLowerCase();
+					const peliFiltradaByTitle = titleDePeliEnLowerCase.includes(options.title.toLowerCase());
+					return peliFiltradaByTitle;
+				});
+			}
+			if (options.tag) {
+				return pelis.filter((peli) => {
+					const tagsDePeliEnLowerCase = peli.tags.map((tag) => tag.toLowerCase());
+					const peliFiltradaByTag = tagsDePeliEnLowerCase.includes(options.tag.toLowerCase());
+					return peliFiltradaByTag;
+				});
+			}
+		}
 	}
-	/* async add(peli: Peli): Promise<boolean> {
-    const promesaUno = this.getById(peli.id).then((peliExistente) => {
-      if (peliExistente) {
-        return false;
-      } else {
-        // magia que agrega la pelicula a un objeto data
-        const data = {...};
-        const promesaDos = jsonfile.writeFile("./pelis.json", data);
-  
-        return promesaDos.then(() => {
-          return true;
-        });
-      }
-    });
-  
-    return promesaUno;
-	} */
-}
 
-const peli = new PelisCollection();
-const opciones = {
-	title: 'p',
-};
-peli.search(opciones).then((res) => console.log(res)); // fijarse de poner todo a minusculas
+	async add(peli: Peli): Promise<Boolean> {
+		const idPeliExiste = await this.getById(peli.id);
+		if (idPeliExiste) return false;
+		else {
+			const pelis = await this.getAll();
+			pelis.push(peli);
+			// habria que mejorar la parte del error
+			return jsonfile
+				.writeFile('./peli.json', pelis)
+				.then(() => {
+					console.log('El archivo se escribió correctamente');
+					return true;
+				})
+				.catch((error) => {
+					console.error('Error al escribir el archivo:', error);
+					return false;
+				});
+		}
+	}
+}
 
 export { PelisCollection, Peli };
