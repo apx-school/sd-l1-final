db890ab25dff95f4445731d6f852ea6e4e26249d
Federico Encinaz Sayago <fencinazsayago@frba.utn.edu.ar>
2024-01-05 22:37:02 -0300

    Se finaliza desafio :)

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,33 @@
-import { PelisCollection, Peli } from "./models";
+import { PelisCollection, Pelicula } from "./models";
+
+type Options = {
+  id?: number;
+  search?: {
+    title?: string;
+    tag?: string;
+  };
+};
 
 class PelisController {
-  constructor() {}
+  data: PelisCollection;
+
+  constructor() {
+    this.data = new PelisCollection();
+  }
+
+  async get(options?: Options) {
+    if(options.id) {
+      return await this.data.getById(options.id);
+    } else if(options.search) {
+      return await this.data.search(options.search);
+    } else {
+      return await this.data.getAll();
+    }
+  }
+
+  async add(pelicula: Pelicula) {
+    return await this.data.add(pelicula);
+  }
+
 }
 export { PelisController };
--- index.ts
+++ index.ts
@@ -1,15 +1,42 @@
+import { PelisController } from "./controllers";
 import * as minimist from "minimist";
 
-function parseaParams(argv) {
-  const resultado = minimist(argv);
+// Objeto que mapea acciones a funciones del controlador.
+const actionHandlers = {
+  // Obtiene una película por su ID.
+  get: async (params, controller: PelisController) => controller.get({ id: params._[1] }), 
+  
+  // Realiza una búsqueda de películas.
+  search: async (params, controller: PelisController) => controller.get(params),
+  
+  // Agrega una nueva película.
+  add: async (params, controller: PelisController) => {
+    const { id, title, tags } = params;
+    const newPelicula = { id, title, tags };
+    return controller.add(newPelicula);
+  },
+  
+  // Acción por defecto, obtiene todas las películas.
+  default: async (params, controller: PelisController) => controller.get({}),
+};
 
-  return resultado;
+// Función para analizar los parámetros de la línea de comandos.
+function parseCommandLineArguments(argv) {
+  return minimist(argv);
 }
 
-function main() {
-  const params = parseaParams(process.argv.slice(2));
+async function executeMain() {
+  const controller = new PelisController();
+  const parsedParams = parseCommandLineArguments(process.argv.slice(2));
+  const action = parsedParams._[0]; // Acción proporcionada en la línea de comandos.
 
-  console.log(params);
+  try {
+    // Ejecuta la función correspondiente a la acción.
+    const result = await actionHandlers[action](parsedParams, controller);
+    console.log(result);
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
 }
 
-main();
+executeMain();
