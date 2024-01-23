5ea482d982f7d813f6f70eb86195fdf1bd9f587a
Axel <axelkevinagustinrojas@gmail.com>
2024-01-23 02:14:04 -0300

    Se eliminaron console.log() innecesarios

--- index.ts
+++ index.ts
@@ -2,41 +2,39 @@ import * as minimist from "minimist";
 import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
-    const resultado = minimist(argv);
-    console.log("parametros", resultado)
-    return resultado;
+  const resultado = minimist(argv);
+  return resultado;
 }
 
-async function ejecutaAccion(options){
-  const coll = new PelisController() 
+async function ejecutaAccion(options) {
+  const coll = new PelisController()
 
   //sin argumentos devuelve getAll
-  if (options._.length == 0){                   
+  if (options._.length == 0) {
     return coll.get({})
-  } else if( options._[0].toLowerCase() == 'get'){
+  } else if (options._[0].toLowerCase() == 'get') {
     //getByID                  
     const id = options._[1];
-    const pelicula = await coll.get({id: id})
+    const pelicula = await coll.get({ id: id })
 
     //Si no encuentra la pelicula devuelve un error
-      if(!pelicula){                            
+    if (!pelicula) {
       throw new Error("Pelicula no encontrada")
-      }
+    }
     return pelicula
-  }else if(options._[0].toLowerCase() == 'search'){
+  } else if (options._[0].toLowerCase() == 'search') {
 
     //Busqueda por tag y por titulo
-    if(options.tag && options.title){
-    }else if(options.tag){
-      return coll.get({search:{tag:options.tag}})
-    }else if(options.title){
-      return coll.get({search:{title: options.title}})
+    if (options.tag && options.title) {
+    } else if (options.tag) {
+      return coll.get({ search: { tag: options.tag } })
+    } else if (options.title) {
+      return coll.get({ search: { title: options.title } })
     }
     return false
-  }else if(options._[0].toLowerCase() == 'add'){
+  } else if (options._[0].toLowerCase() == 'add') {
     //Agregar una pelicula al JSON
-    console.log("entro al add")
-    const peliNueva ={id:options.id,title:options.title,rating: options.rating,tags:options.tag,year:options.year}
+    const peliNueva = { id: options.id, title: options.title, rating: options.rating, tags: options.tag, year: options.year }
     return coll.add(peliNueva)
   }
   return false
@@ -44,7 +42,7 @@ async function ejecutaAccion(options){
 
 function main() {
   const params = parseaParams(process.argv.slice(2));
-  ejecutaAccion(params).then((res)=>{console.log(res)})
+  ejecutaAccion(params).then((res) => { console.log(res) })
 }
 
 main();
