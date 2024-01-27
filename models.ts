f07d0ea64ee1a5dc7130c2f2d1f490ad34425a3a
Pablo Correa <pcorrea@GXC.local>
2024-01-27 19:40:43 -0300

    cambios

--- models.ts
+++ models.ts
@@ -1,4 +1,6 @@
 import * as jsonfile from "jsonfile";
+import * as find from "lodash/find";
+import * as concat from "lodash/concat";
 
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
@@ -8,11 +10,57 @@ class Peli {
 }
 
 class PelisCollection {
+
+  peliculas:Peli[] = []; 
+
   getAll(): Promise<Peli[]> {
-    return jsonfile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile("./pelis.json").then((pelis) => {
+      this.peliculas = pelis;
+      return this.peliculas;
     });
   }
+
+  getById(id:number):Promise<Peli> {
+    return this.getAll().then((p) => {
+      return find(p, { id: id });
+    });
+  }
+
+  add(peli: Peli): Promise<boolean> {
+    const promesaUno = this.getById(peli.id).then((peliExistente) => {
+      if (peliExistente) {
+        console.log("ya existe");
+        return false;
+      } else {
+        // magia que agrega la pelicula a un objeto data
+        concat(this.peliculas, peli);
+        const promesaDos = jsonfile.writeFile("./pelis.json", this.peliculas);
+
+        return promesaDos.then(() => {
+          console.log("peli agregada correctamente");
+          return true;
+        });
+      }
+    });
+
+    return promesaUno;
+  }
+
+  search(action, objetivo): Promise<any> {
+    if (action == "title") {
+      return this.getAll().then((res) => {
+        return find(res, { title: objetivo });
+      });
+    }
+
+    if (action == "tags") {
+      return this.getAll().then((pelis) => {
+        return pelis.filter((p) => {
+          return p.tags.includes(objetivo);
+        });
+      });
+    }
+  }
+ 
 }
 export { PelisCollection, Peli };
