64621faff76cd079563ec612f6a22ad633cc4ca2
demianmln <demianmolina27@gmail.com>
2024-02-08 17:29:40 -0300

    primer commit

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,6 +1,62 @@
+// import { PelisCollection, Peli } from "./models";
+
+// class PelisController {
+//   constructor() {}
+// }
+// export { PelisController };
 import { PelisCollection, Peli } from "./models";
 
+type Options = {
+  id?: number;
+  search?: {
+    title?: string;
+    tag?: string;
+  };
+};
+
 class PelisController {
-  constructor() {}
+  data: PelisCollection;
+  constructor() {
+    this.data = new PelisCollection();
+  }
+  async get(options?: Options): Promise<any> {
+    if (options.id) {
+      return await this.data.getById(options.id);
+    }
+    if (options.search) {
+      const filtro = await this.data.search(options.search);
+      return filtro;
+      // const searchOptions = options.search;
+      // let filteredResults;
+      // if (searchOptions.title && searchOptions.tag) {
+      //   filteredResults = await this.data.search({
+      //     title: searchOptions.title,
+      //     tag: searchOptions.tag,
+      //   });
+      // } else if (searchOptions.title) {
+      //   filteredResults = await this.data.search({
+      //     title: searchOptions.title,
+      //   });
+      // } else if (searchOptions.tag) {
+      //   filteredResults = await this.data.search({ tag: searchOptions.tag });
+      // }
+      // return filteredResults;
+    }
+    return await this.data.getAll();
+  }
+  async add(peli: Peli): Promise<any> {
+    try {
+      // // Crear la nueva película
+      const nuevaPeli = new Peli();
+      nuevaPeli.id = peli.id;
+      nuevaPeli.title = peli.title;
+      nuevaPeli.tags = peli.tags;
+      const resultado = await this.data.add(nuevaPeli);
+      return resultado;
+    } catch (error) {
+      console.error("Error al agregar la película:", error);
+      return false; // Manejar el error y devolver false
+    }
+  }
 }
 export { PelisController };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import * as minimist from "minimist";
+import minimist from "minimist";
+import { PelisController } from "./controllers";
+import { Peli } from "./models";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
@@ -6,10 +8,38 @@ function parseaParams(argv) {
   return resultado;
 }
 
-function main() {
-  const params = parseaParams(process.argv.slice(2));
-
-  console.log(params);
+async function main() {
+  const argumentos = parseaParams(process.argv.slice(2));
+  const controller = new PelisController();
+  if (argumentos._[0] === "add") {
+    const newPeli: Peli = {
+      id: argumentos.id,
+      title: argumentos.title,
+      tags: Array.isArray(argumentos.tags)
+        ? argumentos.tags
+        : [argumentos.tags],
+    };
+    const resultado = await controller.add(newPeli);
+    console.log(resultado ? "Pelicula agregada" : "Error al agregar pelicula ");
+  } else if (argumentos._[0] === "get") {
+    const id = parseInt(argumentos._[1]);
+    const resultado = await controller.get({ id });
+    console.log(resultado);
+  } else if (argumentos._[0] === "search") {
+    const searchOptions = {};
+    if (argumentos.title) {
+      searchOptions["title"] = argumentos.title;
+      console.log("search index ", searchOptions);
+    }
+    if (argumentos.tag) {
+      searchOptions["tag"] = argumentos.tag;
+      console.log("search tag ", searchOptions);
+    }
+    const resultado = await controller.get({ search: searchOptions });
+    console.log(resultado);
+  } else {
+    const allPelis = await controller.get({});
+    console.log(allPelis);
+  }
 }
-
 main();
