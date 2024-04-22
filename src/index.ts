fdcaf07a3080eb037dbb9fa422dc195ce7c47717
Eliana Ayarachi <elianaayarachi@gmail.com>
2024-04-22 14:40:23 -0300

    implementacion buscador peliculas

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,60 @@
-import * as minimist from "minimist";
+//minimist es una librería que facilita el análisis de argumentos de línea de comando. Esta importación permite usar minimist para interpretar los argumentos que se pasen al script.
+import minimist from "minimist";
+
+import { PelisController } from "./controllers";
+//parseaParams es una función diseñada para analizar los argumentos pasados al script. argv es un array de argumentos de línea de comando.
+//minimist(argv) analiza estos argumentos y los convierte en un objeto más manejable, almacenado en resultado.
+//Se imprime el resultado del análisis para depuración, mostrando cómo minimist ha interpretado los argumentos.
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
+  console.log("minimist", resultado);
+  console.log(resultado._[0]);
+  //resultado._ contiene los argumentos no asociados a opciones (sin guiones). resultado._[0] sería el primer argumento no opción, usado aquí para determinar la acción (add, get, search).
+  //options es un objeto que captura opciones específicas para las acciones, como id, title, y tags. Estas se extraen directamente del objeto resultado de minimist.
+  const params = {
+    // add / get / search / no te viene nada minimist te devuelve un array vacio
+    action: resultado._[0],
+    options: {
+      id: resultado.id || resultado._[1],
+      title: resultado.title,
+      tags: resultado.tags || resultado.tag,
+    },
+  };
 
-  return resultado;
+  return params;
 }
-
-function main() {
+//main es una función asíncrona que orquesta la ejecución del script.
+//parseaParams(process.argv.slice(2)) invoca parseaParams con los argumentos de línea de comando, excluyendo los dos primeros elementos que son el entorno de ejecución de Node y el nombre del script.
+//Se crea una instancia de PelisController.
+async function main() {
   const params = parseaParams(process.argv.slice(2));
+  const controller = new PelisController();
 
-  console.log(params);
+  //Basado en params.action, el script decide qué método del controlador llamar.
+  //Si no se especifica una acción, se llama a controller.get() sin argumentos para obtener todas las películas.
+  //Si la acción es add, se llama a controller.add con las opciones especificadas y se informa si la película fue agregada correctamente.
+  //Si la acción es search, se realiza una búsqueda basada en los títulos y tags proporcionados.
+  //Para cualquier otra acción (asumido aquí como obtener por id aunque no se especifica explícitamente como una acción get), se obtiene una película específica por su id.
+
+  if (params.action === undefined) {
+    console.log(await controller.get());
+  } else {
+    if (params.action === "add") {
+      const seAgrego = await controller.add(params.options);
+      seAgrego === true
+        ? console.log("Se agrego correctamente.")
+        : console.log("No se pudo agregar.");
+    } else if (params.action === "search") {
+      console.log(
+        await controller.get({
+          search: { title: params.options.title, tag: params.options.tags },
+        })
+      );
+    } else {
+      console.log(await controller.get({ id: params.options.id }));
+    }
+  }
 }
 
 main();
--- src/models.ts
+++ src/models.ts
@@ -1,9 +1,9 @@
-import * as jsonfile from "jsonfile";
-// sumo este import solo para que tsc lo tome y lo copie
-// en la app no usamos esto para acceder al archivo porque es dinámico
-import "./pelis.json";
+import * as jsonfile from "jsonfile"; // Importa el módulo jsonfile para trabajar con archivos JSON.
+
+type SearchOptions = { title?: string; tag?: string }; // Define un tipo para las opciones de búsqueda, que pueden incluir título y/o etiqueta.
 
 // no modificar estas propiedades, agregar todas las que quieras
+// Define la clase Peli con propiedades id, título y etiquetas.
 class Peli {
   id: number;
   title: string;
@@ -11,11 +11,55 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile.readFile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  movies: Peli[] = []; //Inicializa una propiedad 'movies' como un array vacío para almacenar las películas.
+
+  constructor() {
+    jsonfile.readFile(__dirname + "/pelis.json").then((movies) => {
+      this.movies = movies; // Lee el archivo de películas y almacena los datos en la propiedad 'movies'.
     });
   }
+
+  async getAll(): Promise<Peli[]> {
+    const movies = await jsonfile.readFile(__dirname + "/pelis.json"); //Asíncronamente lee el archivo JSON y retorna las películas.
+    return movies;
+  }
+
+  async add(peli: Peli): Promise<boolean> {
+    const peliExistente = await this.getById(peli.id); // Verifica si ya existe una película con el mismo ID.
+
+    if (peliExistente) {
+      return false; // Si la película existe, retorna false.
+    } else {
+      this.movies.push(peli); // Agrega la nueva película al array 'movies'.
+      await jsonfile.writeFile(__dirname + "/pelis.json", this.movies); // Escribe el array actualizado al archivo JSON.
+      return true; // Retorna true si la película fue agregada exitosamente.
+    }
+  }
+
+  async getById(id: number): Promise<Peli> {
+    const movies = await this.getAll(); // Obtiene todas las películas.
+    const movie = movies.find((m) => m.id === id); // Encuentra la película con el ID especificado.
+    return movie; // Retorna la película encontrada, o undefined si no existe.
+  }
+
+  async search(options?: SearchOptions): Promise<Peli[]> {
+    const movies = await this.getAll(); // Obtiene todas las películas.
+    const searchMovies = await movies.filter((movie) => {
+      let flag = false;
+
+      if (options.title && !options.tag) {
+        flag = movie.title.includes(options.title); // Busca películas que contengan el título especificado.
+      } else if (options.tag && !options.title) {
+        flag = movie.tags.includes(options.tag); // Busca películas que contengan la etiqueta especificada.
+      } else {
+        flag =
+          movie.tags.includes(options.tag) &&
+          movie.title.includes(options.title); //Busca películas que contengan tanto la etiqueta como el título especificados.
+      }
+      return flag; // Retorna true si la película cumple con los criterios de búsqueda.
+    });
+    return searchMovies; // Retorna las películas que cumplen con los criterios de búsqueda.
+  }
 }
-export { PelisCollection, Peli };
+
+export { PelisCollection, Peli, SearchOptions }; // Exporta las clases y tipos definidos para ser utilizados en otros módulos.
