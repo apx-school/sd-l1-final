5158cef4806bb52b16e58f37c89097e3b3534ebf
Rossini Maximo <maximorossini2016@gmail.com>
2024-05-01 13:53:41 -0300

    first commit test passed

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,63 @@
-import * as minimist from "minimist";
+import minimist from "minimist";
+import { PelisController } from "./controllers";
+
+type TerminalParams = {
+  action: string;
+  id: number;
+  title: string;
+  tags?: string[];
+  tag?: string;
+};
 
 function parseaParams(argv) {
-  const resultado = minimist(argv);
+  const result = minimist(argv);
+  let newObj: TerminalParams = {
+    action: null,
+    id: null,
+    title: "",
+    tags: [],
+    tag: "",
+  };
+  for (let prop in result) {
+    if (result[prop][0] === "add") {
+      newObj.action = "add";
+    }
+    if (result[prop][0] === "search") {
+      newObj.action = "search";
+    }
+    if (result[prop][0] === "get") {
+      console.log(result[prop][1]);
+      newObj.action = "get";
+      newObj.id = result[prop][1];
+    }
 
-  return resultado;
+    if (prop === "id") {
+      newObj[prop] = result[prop];
+    }
+    if (prop === "title") {
+      newObj[prop] = result[prop];
+    }
+    if (prop === "tag") {
+      newObj[prop] = result[prop];
+    }
+    if (prop === "tags") {
+      newObj[prop] = result[prop];
+    }
+  }
+  return newObj;
 }
 
 function main() {
   const params = parseaParams(process.argv.slice(2));
-
-  console.log(params);
+  const controller = new PelisController();
+  const result = controller.processOptions(params);
+  controller.promise.then(() => {
+    if (result === undefined) {
+      return controller.currentPeliColl;
+    } else {
+      result.then(() => {});
+    }
+  });
 }
 
 main();
--- src/models.ts
+++ src/models.ts
@@ -3,6 +3,8 @@ import * as jsonfile from "jsonfile";
 // en la app no usamos esto para acceder al archivo porque es dinámico
 import "./pelis.json";
 
+type SearchOptions = { title?: string; tag?: string };
+
 // no modificar estas propiedades, agregar todas las que quieras
 class Peli {
   id: number;
@@ -11,11 +13,61 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile.readFile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  pelis: Peli[];
+  async getAll() {
+    const json = jsonfile.readFile("src/pelis.json");
+    return json.then((data: Peli[]) => {
+      this.pelis = data;
+      return data;
     });
+  }
+  add(peli: Peli): Promise<Boolean> {
+    const firstProm = this.getById(peli.id).then((res) => {
+      if (res) {
+        return false;
+      } else {
+        this.pelis.push(peli);
+        const secondProm = jsonfile
+          .writeFile("src/pelis.json", this.pelis)
+          .then(() => {
+            return true;
+          });
+        return secondProm;
+      }
+    });
+    return firstProm;
+  }
+  async getById(id: number): Promise<Peli> {
+    const allPelis = this.getAll();
+    return allPelis.then((data) => {
+      return data.find((p: Peli) => {
+        if (p.id === id) {
+          return p;
+        }
+      });
+    });
+  }
+  async search(options: SearchOptions) {
+    const pelis = await this.getAll();
+    if (options.tag && options.title) {
+      return pelis.filter((p: Peli) => {
+        if (p.tags.includes(options.tag) && p.title.includes(options.title)) {
+          return p;
+        }
+      });
+    } else if (options.tag) {
+      return pelis.filter((p: Peli) => {
+        if (p.tags.includes(options.tag)) {
+          return p;
+        }
+      });
+    } else if (options.title) {
+      return pelis.filter((p: Peli) => {
+        if (p.title.includes(options.title)) {
+          return p;
+        }
+      });
+    }
   }
 }
 export { PelisCollection, Peli };
