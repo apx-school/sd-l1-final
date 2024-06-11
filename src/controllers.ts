0e87ade51d0ed3f58b6da7b89be4acd5688a8160
unknown <aldereteignacio.com>
2024-06-11 12:25:05 -0300

    cambios

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,6 +1,81 @@
-import { PelisCollection, Peli } from "./models";
+import { PelisCollection} from "./models";
 
 class PelisController {
-  constructor() {}
+  pelisCollection: PelisCollection;
+  
+  constructor() {
+    this.pelisCollection = new PelisCollection();
+  }
+
+  async add(movie) {
+    return await this.pelisCollection.add(movie);
+  }
+
+  async getById(id) {
+    return await this.pelisCollection.getById(id);
+  }
+
+  async search(options) {
+    return await this.pelisCollection.search(options);
+  }
+
+  async getAll() {
+    return await this.pelisCollection.getAll();
+  }
+  async  buscarPelicula(objeto) {
+    const peliculas = new PelisCollection();
+    const todasLasPeliculas = await peliculas.getAll(); // Esperar a que se resuelva la promesa
+
+    if (objeto.id) {
+        const idBuscado = objeto.id;
+        const peliculaEncontrada = await peliculas.getById(idBuscado);
+        return peliculaEncontrada;
+    } else if (objeto.search) {
+        let peliculasFiltradas = todasLasPeliculas;
+
+        if (objeto.title) {
+            const tituloBuscado = objeto.title;
+            peliculasFiltradas = peliculasFiltradas.filter(pelicula => pelicula.title.includes(tituloBuscado));
+        }
+
+        if (objeto.tag) {
+            const tagBuscado = objeto.tag;
+            peliculasFiltradas = peliculasFiltradas.filter(pelicula => pelicula.tags.includes(tagBuscado));
+        }
+
+        return peliculasFiltradas;
+    } else {
+        return todasLasPeliculas;
+    }
 }
+
+  
+
+
+}
+
+
+
+
+
+interface Peli {
+  id: number;
+  titulo: string;
+  tags: string[];
+}
+
+function crearPelicula(objetoPelicula: any): Peli {
+  const { id, titulo, tags } = objetoPelicula;
+  return { id, titulo, tags };
+}
+
+const objetoPelicula = { id: 1, titulo: "Mi Película", tags: ["acción", "aventura"] };
+const nuevaPelicula = crearPelicula(objetoPelicula);
+console.log(nuevaPelicula);
 export { PelisController };
+
+
+
+
+
+
