cc20ecf4af99524a7c8341c2a6bd2cb9577a5b72
soydat <classicwtink@gmail.com>
2024-06-22 18:03:01 -0300

    Exercise: [Desafio Final Nivel 1]: First Commit

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
-import * as minimist from "minimist";
+import minimist from "minimist";
+import { PelisController} from "./controllers";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
 
-  return resultado;
+  if (resultado.search){
+    return {
+      search: resultado.search
+    };
+  } else {
+    return {};
+  }
 }
 
 function main() {
   const params = parseaParams(process.argv.slice(2));
-
-  console.log(params);
+  const controller = new PelisController();
+  controller.get(params).then((resultado) => {
+    console.log(resultado);
+  })
 }
 
 main();
