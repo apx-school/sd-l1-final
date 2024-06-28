0632fb1f696307cfff16193f6b9b4e94f16be3bc
francisco <fran1506alcorta@gmail.com>
2024-06-28 18:13:27 -0300

    cambios finales

--- src/models.ts
+++ src/models.ts
@@ -1,9 +1,9 @@
+
 import * as jsonfile from "jsonfile";
-// sumo este import solo para que tsc lo tome y lo copie
-// en la app no usamos esto para acceder al archivo porque es dinámico
 import "./pelis.json";
 
-// no modificar estas propiedades, agregar todas las que quieras
+type SearchOptions = { title?: string; tag?: string };
+
 class Peli {
   id: number;
   title: string;
@@ -12,10 +12,60 @@ class Peli {
 
 class PelisCollection {
   getAll(): Promise<Peli[]> {
-    return jsonfile.readFile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json").then((pelis) => {
+      return pelis;
+    });
+  }
+
+  getById(id: number): Promise<Peli> {
+    return this.getAll().then((pelis) => {
+      const peli = pelis.find((peli) => {
+        return peli.id === id;
+      });
+      return peli;
+    });
+  }
+
+  async add(peli: Peli): Promise<boolean> {
+    try {
+      const peliExistente = await this.getById(peli.id);
+
+      if (peliExistente) {
+        return false; 
+      } else {
+        let data = await this.getAll(); 
+        peli.id = Number(peli.id);
+        data.push(peli); 
+
+        await jsonfile.writeFile("./pelis.json", data);
+
+        return true; 
+      }
+    } catch (error) {
+      console.error("Error al agregar la película:", error);
+      return false; 
+    }
+  }
+
+  async search(options: SearchOptions): Promise<Peli[]> {
+    const lista = await this.getAll();
+
+    return lista.filter(pelicula => {
+      let cumpleCondicion = true;
+
+      if (options.tag && !pelicula.tags.includes(options.tag)) {
+        cumpleCondicion = false;
+      }
+
+      if (options.title && !pelicula.title.toLowerCase().includes(options.title.toLowerCase())) {
+        cumpleCondicion = false;
+      }
+
+      return cumpleCondicion;
     });
   }
 }
+
 export { PelisCollection, Peli };
+
+
