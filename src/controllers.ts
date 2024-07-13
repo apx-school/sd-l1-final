3f581ea39a995b5d6b750e21e477f129394e9b51
alvaropaoletti <alvaropaoletti31@gmail.com>
2024-07-13 02:52:35 -0300

    terminado

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,6 +1,44 @@
+// controllers.ts
+
 import { PelisCollection, Peli } from "./models";
 
+type Options = {
+  id?: number;
+  search?: {
+    title?: string;
+    tag?: string;
+  };
+};
+
 class PelisController {
-  constructor() {}
+  coll: PelisCollection;
+  constructor() {
+    const nuevo = new PelisCollection();
+    this.coll = nuevo;
+  }
+
+  async get(options?: Options) {
+    if (options?.id !== undefined) {
+      return await this.coll.getById(options.id);
+    } else if (options?.search?.title) {
+      return await this.coll.search(options.search);
+    } else if (options?.search?.tag) {
+      return await this.coll.search(options.search);
+    } else if (options?.search?.tag && options?.search?.title) {
+      return await this.coll.search(options.search);
+    } else {
+      return await this.coll.getAll();
+    }
+  }
+
+  async add(peli: Peli) {
+    const added = await this.coll.add(peli);
+    if (added) {
+      console.log("Película añadida correctamente");
+    } else {
+      console.log("Error al añadir película: ID repetido");
+    }
+  }
 }
+
 export { PelisController };
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,42 @@
-import * as minimist from "minimist";
+// index.ts
+
+import minimist from "minimist";
+import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
-  const resultado = minimist(argv);
-
-  return resultado;
+  return minimist(argv);
 }
 
-function main() {
+async function main() {
   const params = parseaParams(process.argv.slice(2));
+  const pelisController = new PelisController();
 
-  console.log(params);
+  try {
+    if (params._[0] === "add") {
+      const nuevaPeli = {
+        id: params.id,
+        title: params.title,
+        tags: params.tags
+      };
+      await pelisController.add(nuevaPeli);
+    } else if (params._[0] === "get") {
+      const id = parseInt(params._[1]);
+      const result = await pelisController.get({ id });
+      console.log("Resultado de la búsqueda por ID:", result);
+    } else if (params._[0] === "search") {
+      const options = {
+        title: params.title,
+        tag: params.tag,
+      };
+      const result = await pelisController.get({ search: options });
+      console.log("Resultado de la búsqueda:", result);
+    } else {
+      const result = await pelisController.get();
+      console.log("Todas las películas:", result);
+    }
+  } catch (error) {
+    console.error("Error en el programa:", error);
+  }
 }
 
 main();
