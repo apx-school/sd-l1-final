66fbc3196c02cf5794315fe75937e189d11d1f98
Facundo Rodriguez <fr8080342@gmail.com>
2024-07-24 17:17:29 -0300

    Facundo Rodriguez 24.7.2024

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,6 +1,62 @@
 import { PelisCollection, Peli } from "./models";
 
-class PelisController {
-  constructor() {}
+interface Options {
+    id?: number;
+    search?: {
+        title?: string;
+        tag?: string }
 }
-export { PelisController };
+
+class PelisController {
+    coleccion: PelisCollection;
+    constructor() {
+        this.coleccion = new PelisCollection() };
+    
+    async get(opciones?: Options): Promise <any> {
+        try {   
+            if (opciones.id) { 
+            return this.coleccion.getById(opciones.id) }
+
+            else if (opciones.search.title && !opciones.search.tag) {
+                return this.coleccion.search( {title : opciones.search.title} ) }
+
+            else if (opciones.search.tag && !opciones.search.title) {
+                return this.coleccion.search( {tag : opciones.search.tag} ) }
+
+            else if (opciones.search.title && opciones.search.tag) {
+                let peliculas: Peli[] = await this.coleccion.getAll();
+                let title: string = opciones.search.title;
+                let tag: string = opciones.search.tag;
+                let primerFiltro: Peli[] = peliculas.filter( peli => peli.title.includes(title) );
+                let segundoFiltro: Peli[] = primerFiltro.filter( peli => peli.tags.includes(tag) );
+                if (segundoFiltro.length == 0) return false
+                return segundoFiltro }
+        } 
+        catch { return false } 
+    }
+
+    add (peli: Peli) { this.coleccion.add(peli) }
+}
+
+export { PelisController, Options };
+/*
+async function a () {
+    let a: PelisController = new PelisController();
+    let c: Options = {
+        search: {
+            title: "",
+            tag: "" } 
+    }
+    let z: Peli = { 
+        id: 4421, 
+        title: "Una peli", 
+        tags: ["classic", "action"] 
+      };
+    await a.add(z);
+
+ 
+}
+a();
+*/
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,52 @@
-import * as minimist from "minimist";
+import minimist from "minimist";
+import { PelisCollection, Peli, SearchOptions } from "./models";
+import { PelisController, Options } from "./controllers";
+    
+function parsearTerminal() {
+    const resultado: any = minimist(process.argv.slice(2));
+    return resultado }
 
-function parseaParams(argv) {
-  const resultado = minimist(argv);
+async function main () {
+    let comandos: any = parsearTerminal();
+    let controller: PelisController = new PelisController();
+    let colecction: PelisCollection = new PelisCollection();
 
-  return resultado;
-}
+    if (comandos._ == 'add') {
+        let peli: Peli = {
+            id: comandos.id,
+            title: comandos.title,
+            tags: [comandos.tags[0], comandos.tags[1]] 
+        };
+        controller.add(peli) }
 
-function main() {
-  const params = parseaParams(process.argv.slice(2));
+    else if (comandos._ == "get") {
+        let resultado: Peli = await controller.get( comandos._[1] );
+        return resultado }
 
-  console.log(params);
-}
+    else if (comandos._ == "search" && comandos.title && !comandos.tag) {
+        let opciones: SearchOptions = {
+            title: comandos.title };
+        let resultado: boolean | Peli[] = await colecction.search(opciones);
+        return resultado }
 
-main();
+    else if (comandos._ == "search" && comandos.tag  && !comandos.tag) {
+        let opciones: SearchOptions = {
+            tag: comandos.tag };
+        let resultado: boolean | Peli[] = await colecction.search(opciones);
+        return resultado }
+    
+    else if (comandos._ == "search" && comandos.title  && comandos.tag) {
+        let opciones: Options = {
+            search: {
+                title: comandos.title,
+                tag: comandos.tag } };
+        let resultado = await controller.get(opciones);
+        console.log(resultado) }
+
+    else {
+        let resultado = await colecction.getAll();
+        return resultado }
+};
+
+main()
+
