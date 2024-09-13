a40d6cf5bb328a0d8e095dbd9f0dfda0ab34ddb6
brunoSacchetti <sacchettibruno03@gmail.com>
2024-09-13 11:08:55 -0300

    desafio terminado

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import * as minimist from "minimist";
+import minimist from "minimist";
+import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
@@ -6,10 +7,25 @@ function parseaParams(argv) {
   return resultado;
 }
 
-function main() {
+async function main() {
   const params = parseaParams(process.argv.slice(2));
 
   console.log(params);
+
+  const controller = new PelisController();
+  if (params.add) {
+    const peli = {
+      id: params.id,
+      title: params.title,
+      tags: params.tags ? params.tags.split(",") : [] // Convertimos tags a array
+    };
+
+    await controller.add(peli);
+    console.log("Pelicula agregada con éxito");
+  } else {
+    const search = await controller.get(params);
+    console.table(search);
+  }
 }
 
 main();
