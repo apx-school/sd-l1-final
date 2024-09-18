0c61533b1525984d3a01ad7e9909143c1865a03f
Juan Manuel Di Florio <juanmadiflorio44@gmail.com>
2024-09-18 14:11:57 -0300

    Desafio de Buscador de Peliculas Finalizado

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,40 @@
-import * as minimist from "minimist";
+import minimist from "minimist";
+import { PelisCollection, Peli } from "./models";
+import { PelisController, Options } from "./controllers";
 
 function parseaParams(argv) {
-  const resultado = minimist(argv);
-
-  return resultado;
+  const args = minimist(process.argv.slice(2));
+  if (args.get) {
+    options.id = parseInt(args.get);
+  }
+  if (args.title) {
+    options.search = { title: args.title };
+  }
+  if (args.tag) {
+    options.search = { tag: args.tag };
+  }
+  if (args.add) {
+    const { id, title, tags = [] } = args;
+    const peliController = new PelisController(new PelisCollection());
+    peliController.add({ id, title, tags });
+  }
+  if (args.title && args.tag) {
+    options.search = {
+      title: args.title,
+      tag: args.tag,
+    };
+  } else {
+  }
+  return args;
 }
 
-function main() {
-  const params = parseaParams(process.argv.slice(2));
+const options: Options = {};
 
-  console.log(params);
+function main() {
+  const params = parseaParams(process.argv);
+  const pelisCollection = new PelisCollection();
+  const pelisController = new PelisController(pelisCollection);
+  pelisController.get(options).then((p) => console.log(p));
 }
 
 main();
--- src/models.ts
+++ src/models.ts
@@ -13,11 +13,92 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile.readFile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  async getAll(): Promise<Peli[]> {
+    return await jsonfile.readFile("./pelis.json").then(() => {
+      return jsonfile.readFile("./pelis.json");
     });
   }
+
+  async add(peli: Peli): Promise<boolean> {
+    const pelis = await jsonfile.readFile("./pelis.json");
+    const peliExistente = pelis.find((p) => p.id === peli.id);
+    if (peliExistente) {
+      console.log("La peli ya existe");
+      return false;
+    } else {
+      pelis.push(peli);
+      await jsonfile.writeFile("./pelis.json", pelis);
+      console.log("Peli agregada correctamente");
+      return true;
+    }
+  }
+
+  async getById(id: number): Promise<Peli> {
+    const peli = await jsonfile.readFile("./pelis.json");
+    const resultado = peli.filter((producto) => producto.id === id);
+    return resultado.length > 0 ? resultado[0] : null;
+  }
+  async search(options: { title?: string; tag?: string }): Promise<Peli[]> {
+    const lista = await jsonfile.readFile("./pelis.json");
+
+    const listaFiltrada = lista.filter((p: Peli) => {
+      if (options.title && options.tag) {
+        const regex = new RegExp(options.tag, "i");
+        return (
+          p.title.toLowerCase().includes(options.title.toLowerCase()) &&
+          p.tags.some((tag) => regex.test(tag))
+        );
+      } else if (options.title) {
+        return p.title.toLowerCase().includes(options.title.toLowerCase());
+      } else if (options.tag) {
+        const regex = new RegExp(options.tag, "i");
+        return p.tags.some((tag) => regex.test(tag));
+      } else {
+        return true;
+      }
+    });
+
+    return listaFiltrada;
+  }
 }
+
 export { PelisCollection, Peli };
+//Instancia de la Clsase PelisCollection metodo getAll
+/*const peli = new PelisCollection();
+peli.getAll().then((p) => console.log(p));*/
+
+//Instancia de la Clsase PelisCollection metodo add
+/*const peliCollection = new PelisCollection();
+const nuevaPeli = { id: 5, title: "El Padrino", tags: ["Drama", "Crimen"] };
+peliCollection
+  .add(nuevaPeli)
+  .then(() => {
+    console.log("Película agregada correctamente");
+  })
+  .catch((error) => {
+    console.error("Error al agregar la película:", error);
+  });*/
+
+//Instancia de la Clsase PelisCollection metodo getById
+/*const peliCollection2 = new PelisCollection();
+peliCollection2
+  .getById(1)
+  .then((peli) => {
+    console.log(peli);
+  })
+  .catch((error) => {
+    console.error("Error al obtener la peli:", error);
+  });
+
+*/
+
+//Instancia de la Clsase PelisCollection metodo search
+/*const peli3 = new PelisCollection();
+peli3
+  .search({ title: "a" })
+  .then((p) => console.log("Peliculas encontradas  por titulo: ", p));
+console.log("------");
+const peli4 = new PelisCollection();
+peli4
+  .search({ tag: "Drama" })
+  .then((p) => console.log(" Peliculas encontradas por tag", p));*/
