438d086645550777312d8ecff27496e791402ade
Mateo Quinteros <mateoquinteros11@gmail.com>
2024-10-07 16:28:31 -0300

    Primer commit del desafío completado

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import * as minimist from "minimist";
+import minimist from "minimist";
+import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
@@ -7,9 +8,63 @@ function parseaParams(argv) {
 }
 
 function main() {
+  const controller = new PelisController();
   const params = parseaParams(process.argv.slice(2));
-
-  console.log(params);
+  if (params._[0] === "add") {
+    controller.model
+      .add({
+        id: params.id,
+        title: params.title,
+        tags: params.tags,
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  }
+  if (params._[0] === "get") {
+    controller.get({ id: parseInt(params._[1]) }).then((res) => {
+      console.log(res);
+    });
+  }
+  if (params._[0] === "search" && params.title) {
+    controller
+      .get({
+        search: {
+          title: params.title,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  }
+  if (params._[0] === "search" && params.tag) {
+    controller
+      .get({
+        search: {
+          tag: params.tag,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  }
+  if (params._[0] === "search" && params.title && params.tag) {
+    controller
+      .get({
+        search: {
+          title: params.title,
+          tag: params.tag,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  }
+  if (process.argv.length === 2) {
+    controller.model.getAll().then((res) => {
+      console.log(res);
+    });
+  }
 }
 
 main();
--- src/models.ts
+++ src/models.ts
@@ -12,12 +12,57 @@ class Peli {
   tags: string[];
 }
 
+export type SearchOptions = { title?: string; tag?: string };
+
 class PelisCollection {
   getAll(): Promise<Peli[]> {
-    return jsonfile.readFile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json").then((data) => {
+      return data;
     });
+  }
+
+  getById(id: number): Promise<Peli> {
+    return this.getAll().then((p) => {
+      return p.find((pelicula) => pelicula.id === id);
+    });
+  }
+
+  async add(peli: Peli): Promise<boolean> {
+    const peliculas = await this.getAll();
+
+    const promesa1 = this.getById(peli.id).then((p) => {
+      if (p) {
+        console.log("Error, ya existe una pelicula con ese id");
+        return false;
+      } else {
+        peliculas.push(peli);
+        return jsonfile.writeFile("./pelis.json", peliculas).then(() => {
+          console.log("La pelicula se agregó correctamente");
+          return true;
+        });
+      }
+    });
+    return promesa1;
+  }
+
+  async search(options: SearchOptions): Promise<Peli[]> {
+    const lista = await this.getAll();
+
+    const listaFiltrada = lista.filter((p) => {
+      let esteVa = true;
+
+      if (options.tag) {
+        esteVa = esteVa && p.tags.includes(options.tag);
+      }
+
+      if (options.title) {
+        esteVa = esteVa && p.title.includes(options.title);
+      }
+
+      return esteVa;
+    });
+
+    return listaFiltrada;
   }
 }
 export { PelisCollection, Peli };
