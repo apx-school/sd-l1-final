6465c2997fd78c1be9d138b08abeb3034aec14b1
david <davidabsenta@gmail.com>
2024-10-30 12:56:23 -0300

    resuelto?

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,6 +1,26 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  private pelisCollection: PelisCollection;
+  constructor() {
+    this.pelisCollection = new PelisCollection();
+  }
+  async get(options?: {
+    id?: number;
+    search?: { title?: string; tag?: string };
+  }): Promise<Peli[] | Peli | null> {
+    if (options?.id) {
+      return await this.pelisCollection.getById(options.id);
+    } else if (options?.search) {
+      return await this.pelisCollection.search(options.search);
+    } else {
+      return await this.pelisCollection.getAll();
+    }
+  }
+
+  async add(peli: Peli): Promise<boolean> {
+    return await this.pelisCollection.add(peli);
+  }
 }
+
 export { PelisController };
--- src/models.ts
+++ src/models.ts
@@ -1,11 +1,8 @@
 import * as jsonfile from "jsonfile";
-// El siguiente import no se usa pero es necesario
 import "./pelis.json";
-// de esta forma Typescript se entera que tiene que incluir
-// el .json y pasarlo a la carpeta /dist
-// si no, solo usandolo desde la libreria jsonfile, no se dá cuenta
 
-// no modificar estas propiedades, agregar todas las que quieras
+type SearchOptions = { title?: string; tag?: string };
+
 class Peli {
   id: number;
   title: string;
@@ -13,11 +10,46 @@ class Peli {
 }
 
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile.readFile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  async getAll(): Promise<Peli[]> {
+    return await jsonfile.readFile("./pelis.json");
+  }
+
+  async getById(id: number): Promise<Peli | null> {
+    try {
+      const allPelis = await jsonfile.readFile("./pelis.json");
+      return allPelis.find((peli) => peli.id === id) || null;
+    } catch (error) {
+      console.error("Error al leer el archivo:", error);
+      return null;
+    }
+  }
+
+  async add(peli: Peli): Promise<boolean> {
+    const peliExistente = await this.getById(peli.id);
+    if (peliExistente) {
+      return false;
+    } else {
+      const pelis = await this.getAll();
+      pelis.push(peli);
+      await jsonfile.writeFile("./pelis.json", pelis);
+      return true;
+    }
+  }
+
+  async search(options: SearchOptions): Promise<Peli[]> {
+    const lista = await this.getAll();
+
+    const listaFiltrada = lista.filter((peli) => {
+      let esteVa = false;
+      if (options.tag) {
+        esteVa = peli.tags.includes(options.tag);
+      }
+      if (options.title) {
+        esteVa = peli.title.includes(options.title);
+      }
+      return esteVa;
     });
+    return listaFiltrada;
   }
 }
 export { PelisCollection, Peli };
