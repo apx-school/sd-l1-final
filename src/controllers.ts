924faffe552a0f8773cf48a3ad7fdf47517b3e7c
Valentin Goroso <valegoroso21@gmail.com>
2024-10-31 20:13:19 -0300

    Cambios necesarios para pasar todos los test

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,6 +1,37 @@
 import { PelisCollection, Peli } from "./models";
 
 class PelisController {
-  constructor() {}
+  private pelisCollection: PelisCollection; //propiedad interna
+  constructor() {
+    this.pelisCollection = new PelisCollection(); //Instanciacion del modelo
+  }
+
+  async get(options?: {
+    id?: number;
+    search?: { title?: string; tag?: string };
+  }) {
+    if (options?.id) {
+      const peli = await this.pelisCollection.getById(options.id);
+      return peli;
+    } else if (options?.search) {
+      return await this.pelisCollection.search(options.search);
+    } else return await this.pelisCollection.getAll();
+  }
+
+  async add(peli: Peli) {
+    const resultado = await this.pelisCollection.add(peli);
+    return resultado;
+  }
 }
+
+//El símbolo ? en TypeScript se utiliza para indicar que una propiedad es opcional. Esto significa que no es necesario que esa propiedad esté presente cuando se crea un objeto de ese tipo.
+//definicion del tipo Options
+
+// type Options = {
+//   id?: number;
+//   search?: {
+//     title?: string;
+//     tag?: string;
+//   };
+// };
 export { PelisController };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import * as minimist from "minimist";
+import minimist from "minimist";
+import { PelisController } from "./controllers";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
@@ -6,10 +7,45 @@ function parseaParams(argv) {
   return resultado;
 }
 
-function main() {
+async function main() {
   const params = parseaParams(process.argv.slice(2));
+  const pelisController = new PelisController();
 
-  console.log(params);
+  if (params._[0] === "add") {
+    //agg peliculas
+    const peli = {
+      id: params.id,
+      title: params.title,
+      tags: params.tags || [], // Asegurate que tags sea un array
+    };
+    const result = await pelisController.add(peli);
+    console.log(
+      result ? "Pelicula agregada con exito." : "Error al agregar la pelicula."
+    );
+  } else if (params._[0] === "get") {
+    const id = Number(params._[1]); //el segundo argumento es el id
+    const peli = await pelisController.get({ id });
+    console.log(peli || "Pelicula no encontrada");
+  } else if (params._[0] === "search") {
+    //busca pelicula por titulo o tag
+    const searchOptions = {
+      title: params.title,
+      tag: params.tag,
+    };
+    const pelis = await pelisController.get({ search: searchOptions });
+    if (Array.isArray(pelis)) {
+      console.log(pelis.length > 0 ? pelis : "No se encontraron peliculas.");
+    } else {
+      console.log(pelis);
+    }
+  } else {
+    const pelis = await pelisController.get();
+    if (Array.isArray(pelis)) {
+      console.log(pelis);
+    } else {
+      console.log([pelis]);
+    }
+  }
 }
 
 main();
