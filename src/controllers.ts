63401aa6ac354cc6641e938d08d25dbd215016a7
Santia <santiagogmuller@gmail.com>
2024-12-03 02:14:18 -0300

    El código pasa de forma intermitente los test

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,6 +1,70 @@
-import { PelisCollection, Peli } from "./models";
+import { PelisCollection, Peli} from "./models";
+const coleccionPelis = new PelisCollection;
+type Options = {
+  id?: number;
+  search?: {
+    title?: string;
+    tag?: string;
+  };
+};
+
+type SearchOptions = { title?: string; tag?: string }; 
 
 class PelisController {
-  constructor() {}
+  constructor() { }
+  
+  async get(options?: Options):Promise <Peli | undefined | any  /*undefined |Peli[]*/ > /* VER ALGUNOOTRO DEVUELVE ARRAY pELI*/ {
+    //const searchOptions = { title: options.search.title, tag : options.search.tag}
+    if (options?.id) {
+     return await coleccionPelis.getById(options.id) //ESTE PARECE HABER FUNCIONADO
+    }
+    // if (options?.search) {
+    //   let searchOptions = {
+    //     title: options.search.title,
+    //     tag: options.search.tag
+    //   }
+    //   return await coleccionPelis.search(searchOptions)
+    // }
+
+    // //-----LO DE ABAJO ANDAARÍA, ESTOY VIENDO SI PUEDO ESCRIBIR MENOS:
+
+    if (options.search) {
+      let dobleCheck = options.search
+      options.search.title && options.search.tag
+      return await coleccionPelis.search(dobleCheck)          ///Ver esto parece andar
+    }
+    else if (options.search) {
+      return await coleccionPelis.search(options.search)     
+    }
+  //------------------------------------------------------------------------------------------//
+    // if (options.search) {
+    //   let dobleCheck = true 
+    //   if (options.search.title) {
+    //   return await dobleCheck && coleccionPelis.search(options.search)
+    //   };
+  
+    //   if (options.search.tag) {
+    //   return  await dobleCheck && coleccionPelis.search(options.search)
+    //   };
+    //   return dobleCheck
+    // }
+  //------------------------------------------------------------------------------------------//
+         
+       
+    //   let searchOptions = { tag: options.search.tag }
+    //   return await coleccionPelis.search(searchOptions)
+    // };
+    // if (options.search.title && options.search.tag ) {
+
+    // }
+    else { return await coleccionPelis.getAll() }
+    
+  };
+  async add(peli: Peli) {
+   return await coleccionPelis.add(peli)   //VER SI ESTO FUNCIONA
+  }
+  
 }
+
+
 export { PelisController };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import * as minimist from "minimist";
-
+import minimist from "minimist";
+import { PelisCollection, Peli} from "./models";
+import { PelisController } from "./controllers";
+const pelisController = new PelisController
 function parseaParams(argv) {
   const resultado = minimist(argv);
 
@@ -8,8 +10,46 @@ function parseaParams(argv) {
 
 function main() {
   const params = parseaParams(process.argv.slice(2));
+  //console.log(params)
+  if (params._[0] === "add") {
+    const peliAgregada = new Peli(params.id, params.title, params.tags)
+    pelisController.add(peliAgregada).then((resultado) => resultado)
+  };
 
-  console.log(params);
-}
+  if (params._[0] === ("get") /*&& typeof (params._[1]) === "number"*/) {
+    const obj = { 
+      id: typeof (params._[1]) === "number" ? params._[1] : undefined
+      /////YO HABÍA HECHO ESTO: Mantenía el && arriba y hacia obj = {id : params._[1]}
+  
+    }
+    pelisController.get(obj).then((resultado) => console.log(resultado))
+  }
 
-main();
+ 
+
+  if (params.title && params.tag) {
+    const buscaPeli = { search: { title: params.title, tag: params.tag } }
+    pelisController.get(buscaPeli).then((resultado) => console.log(resultado))
+  }
+  else if (params.title) {
+    const buscaPeli = { search: { title: params.title } };
+    pelisController.get(buscaPeli).then((resultado) => console.log(resultado))
+  } 
+  else if (params.tag) {
+    const buscaPeli = { search: { tag: params.tag } };
+    pelisController.get(buscaPeli).then((resultado) => console.log(resultado))
+  }
+  else {
+    const nuevaPeliCollection = new PelisCollection; 
+    return nuevaPeliCollection.getAll().then((resupuesta) => console.log ( resupuesta))
+  }
+   
+     //.catch((error)=>console.error("Error papu", error));
+     
+   } // ESTO NO DEVUELVE NADAD.. VER
+
+  //console.log(params);
+  //}
+;
+
+main()  
