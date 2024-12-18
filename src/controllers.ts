66d6fc86edda3b8d1df7b56bc607b252d2a1c320
Maximiliano Martinez <maxi_martinezmza@hotmail.com>
2024-12-17 23:12:41 -0300

    Trabajo terminado

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,6 +1,38 @@
+
+
 import { PelisCollection, Peli } from "./models";
 
+type Options = {
+  id?: number;
+  search?: {
+    title?: string;
+    tag?: string;
+  };
+};
+
 class PelisController {
-  constructor() {}
+  pelis: PelisCollection;
+
+  constructor() {
+    this.pelis = new PelisCollection();
+  }
+
+  // Invoca a los métodos del modelo según las propiedades del options que recibe desde el index
+  async get(options: Options): Promise<any> {
+    if (options.id) {
+
+      return await this.pelis.getById(options.id);
+    }
+    if (options.search) {
+      return await this.pelis.search(options.search);
+    } else {
+      return await this.pelis.getAll();
+    }
+  }
+
+  async add(peli: Peli): Promise<boolean> {
+    return await this.pelis.add(peli);
+  }
 }
+
 export { PelisController };
--- src/models.ts
+++ src/models.ts
@@ -5,19 +5,60 @@ import "./pelis.json";
 // el .json y pasarlo a la carpeta /dist
 // si no, solo usandolo desde la libreria jsonfile, no se dá cuenta
 
-// no modificar estas propiedades, agregar todas las que quieras
-class Peli {
+
+interface Peli {
   id: number;
   title: string;
   tags: string[];
 }
 
 class PelisCollection {
+  data: Peli[] = []; // Inicializa data como un array vacío
+
+  // Método que carga el archivo json de películas
   getAll(): Promise<Peli[]> {
-    return jsonfile.readFile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("./pelis.json").then((data) => {
+      this.data = data;
+      return this.data; // Devuelve la data leída
+    });
+  }
+
+  // Método que obtiene una película según su id
+  async getById(id: number): Promise<Peli | undefined> {
+    const peliculas = await this.getAll();
+    const peli = peliculas.find((peli) => peli.id === id);
+    
+    if (!peli) {
+      console.error(`Película con ID ${id} no encontrada.`);
+    }
+    
+    return peli;
+  }
+
+  // Método que filtra películas según su título y sus tags
+  search(options: { title?: string; tag?: string }): Promise<Peli[]> {
+    return this.getAll().then((pelis) => {
+      let tmp = pelis;
+      if (options.title) {
+        tmp = tmp.filter((i) => i.title.includes(options.title));
+      }
+      if (options.tag) {
+        tmp = tmp.filter((i) => i.tags.includes(options.tag));
+      }
+      return tmp;
+    });
+  }
+
+  // Método que agrega una película a data y luego lo guarda en el json
+  add(peli: Peli): Promise<boolean> {
+    return this.getById(peli.id).then((peliExistente) => {
+      if (peliExistente) {
+        return false; // Si la película ya existe, devuelve false
+      }
+      this.data.push(peli); // Agrega la nueva película
+      return jsonfile.writeFile("./pelis.json", this.data).then(() => true); // Guarda y devuelve true
     });
   }
 }
+
 export { PelisCollection, Peli };
