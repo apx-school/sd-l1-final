f89efdcfa81d295d1cf9521c2b85fddc4915cf83
JeronimoCortez <jeronimofcortez3@gmail.com>
2025-02-25 13:39:31 -0300

    Desafio finalizado

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import * as minimist from "minimist";
+import minimist from "minimist";
+import { Peli, PelisCollection } from "./models";
 
 function parseaParams(argv) {
   const resultado = minimist(argv);
@@ -6,8 +7,40 @@ function parseaParams(argv) {
   return resultado;
 }
 
-function main() {
+async function main() {
   const params = parseaParams(process.argv.slice(2));
+  const pelisController = new PelisCollection();
+
+  if(params._[0] === "add"){
+    const peli: Peli = {
+      id: params.id,
+      title: params.title,
+      tags: params.tags || [],
+    }
+
+    const resultado = await pelisController.add(peli)
+    console.log(resultado ? "Peli agregada con exito" : "Error al cargar peli");
+    
+  } else if(params._[0] === "get"){
+    const id = Number(params._[1])
+    const peli = await pelisController.getById(id);
+    console.log(peli ? peli : "Película no encontrada.");
+  } else if(params._[0] === "search"){
+    const options = {
+      search: {
+        title: params.title,
+        tag: params.tag
+      }
+    }
+    const pelis: Peli[] = await pelisController.search(options);
+    console.log(pelis.length > 0 ? pelis : "No se encontraron resultados");
+  } else{
+    const pelis = await pelisController.getAll();
+    console.log(pelis);
+    
+  }
+
+  
 
   console.log(params);
 }
