4cc020816febb37c5865569c877eaa1e94aa9c5f
LucasDevGame <lucasgongora.dev@gmail.com>
2025-03-12 14:41:03 -0300

    Primer tramo de codigo agregado, hasta el medoto search en models.ts

--- src/models.ts
+++ src/models.ts
@@ -12,12 +12,69 @@ class Peli {
   tags: string[];
 }
 
+export type SearchOptions = {
+  title?: string;
+  tag?: string;
+};
+
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile.readFile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
-    });
-  }
+  dataPeliculas: Peli[] = [];
+  async getAll(): Promise<Peli[]> {
+    try {
+        const peliculas: Peli[] = await jsonfile.readFile("./pelis.json");
+        return peliculas;
+    } catch (error) {
+        console.error("Error al leer el archivo:", error);
+        return []; // Si hay un error, retorna un array vacío
+    }
 }
+  async add(peli: Peli): Promise<boolean> {
+    const promersaUno = this.getById(peli.id).then((peliExistente) => {
+    if (peliExistente) {
+      return false;
+    } else{
+      const data = {...peli};
+      const promesaDos = jsonfile.writeFile("./pelis.json", data);
+        return promesaDos.then(() => {
+          return true;
+      });  
+      }
+  });
+    return promersaUno;
+  }  
+  async getById(id: number): Promise<Peli | undefined> {
+    const pelis = await this.getAll(); // Obtener todas las películas
+    const peliEncontrada = pelis.find((peli) => peli.id == id);
+
+    if (!peliEncontrada) {
+        console.log("No existe esa película en los registros");
+    }
+    
+    return peliEncontrada;
+  }
+  async search(options: SearchOptions): Promise<Peli[]> {
+    this.getAll();
+    
+    const listaFiltrada = this.dataPeliculas.filter(function (p) {
+      let esteVa = false;
+       // lógica de tags
+      if (options.tag) {
+        const tagsDelTituloActual = p["tags"];
+        tagsDelTituloActual.forEach((tag) => {tag == options.tag ? esteVa = true : null});
+      }
+        // lógica de title
+      if (options.title) {
+        const tituloActual = p["title"];
+        tituloActual.forEach((title) => {title == options.title ? esteVa = true: null});
+      }
+      return esteVa;
+    });
+  
+    return listaFiltrada;
+  }
+  }
+  
+
 export { PelisCollection, Peli };
+
+
