3ae6a24d10d5f1e410d9f250a485a571190c1ab5
felis <felisicca@gmail.com>
2025-03-27 13:05:12 -0300

    Desafio Completado

--- src/models.test.ts
+++ src/models.test.ts
@@ -1,31 +1,22 @@
 import anyTest, { TestFn } from "ava";
 import { PelisCollection, Peli } from "./models";
 
+// Función para generar IDs aleatorios
 export const getRandomId = () => {
   const randomNumber = Math.floor(Math.random() * 100000);
   return 129856 + randomNumber;
 };
 
 const SESSION_ID = getRandomId();
+const TEST_ID = getRandomId();
+const TEST_TITLE = "title " + SESSION_ID + TEST_ID;
+const SECOND_TEST_ID = getRandomId();
+const SECOND_TEST_TITLE = "title " + SESSION_ID + SECOND_TEST_ID;
 
 const test = anyTest as TestFn<{
   instance: PelisCollection;
   all: Peli[];
 }>;
-
-const TEST_ID = getRandomId();
-const TEST_TITLE = "title " + SESSION_ID + TEST_ID;
-
-const SECOND_TEST_ID = getRandomId();
-const SECOND_TEST_TITLE = "title " + SESSION_ID + SECOND_TEST_ID;
-
-// # IMPORTANTE #
-
-// apenas te clones este repo
-// todos los test a continuación van a fallar
-
-// comentalos y descomentá uno a uno a medida
-// que vas avanzando en cada test
 
 test.serial("Corre ava", async (t) => {
   t.is("si", "si");
@@ -38,35 +29,44 @@ test.serial("Testeo el método getById", async (t) => {
     title: TEST_TITLE,
     tags: ["tt", "rr"],
   });
+  
   const all = await collection.getAll();
   const a = all[0];
+  
+  // Verificamos que getById devuelva el objeto correcto
   const b = await collection.getById(a.id);
-  t.is(a.title, b.title);
+  t.is(a.title, b?.title);
 });
 
 test.serial("Testeo el método search", async (t) => {
   const collection = new PelisCollection();
+  
+  // Agregamos dos películas a la colección
   await collection.add({
     id: TEST_ID,
     title: TEST_TITLE,
     tags: ["tt", "rr"],
   });
+  
   await collection.add({
     id: SECOND_TEST_ID,
     title: SECOND_TEST_TITLE,
     tags: ["yy", "uu"],
   });
-  const all = await collection.getAll();
-  const a = all[0];
-  // El search debe encontrar ambas pelis creadas a partir de la session
+
+  // Buscamos todas las pelis por la session_id en el title
   const b = await collection.search({ title: SESSION_ID.toString() });
   const ids = b.map((b) => b.id);
+  
+  // Verificamos que ambas películas sean encontradas
   t.deepEqual(ids, [TEST_ID, SECOND_TEST_ID]);
 
-  // El search debe encontrar solo la peli con el title (session) y el tag (yy)
+  // Buscamos una peli con un title y tag específicos
   const c = await collection.search({
     title: SECOND_TEST_ID.toString(),
     tag: "yy",
   });
+  
+  // Verificamos que la segunda película sea encontrada
   t.deepEqual(c[0].id, SECOND_TEST_ID);
 });
