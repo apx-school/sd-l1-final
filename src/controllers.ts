a2a96aabc801d5ee7d3d4ffc32a7e23ba001023c
Daniel Coria <hdcaml25@gmail.com>
2025-04-11 22:34:34 -0300

    Creados todos los metodos

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,6 +1,47 @@
 import { PelisCollection, Peli } from "./models";
 
+type Options = {
+  id?: number;
+  search?: {
+    title?: string;
+    tag?: string;
+  };
+};
+
 class PelisController {
-  constructor() {}
+  model: PelisCollection;
+  constructor() {
+    this.model = new PelisCollection();
+  }
+
+  get(options?: Options): Peli[] {
+    const resultado = [];
+    if (options.id) {
+      const conId = this.model.getById(options.id).then((result) => {
+        return result;
+      });
+      resultado.push(conId);
+    } else if (options.search) {
+      const conTitle = this.model.search(options.search).then((result) => {
+        return result;
+      });
+      resultado.push(conTitle);
+    } else {
+      const todas = this.model.getAll().then((result) => {
+        return result;
+      });
+      resultado.push(todas);
+    }
+
+    return resultado;
+  }
+  getOne(options:Options):Peli{
+    return this.get[0];
+  }
+  add(peli:Peli){
+    this.model.add(peli).then((result)=>{
+      return result
+    })
+  }
 }
 export { PelisController };
--- src/models.ts
+++ src/models.ts
@@ -12,12 +12,116 @@ class Peli {
   tags: string[];
 }
 
+type SearchOptions = { title?: string; tag?: string };
+
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile.readFile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
+  db: Peli[];
+  filePath: string;
+  constructor() {
+    this.db = [];
+    this.filePath = __dirname + "/pelis.json";
+  }
+  private async loadFromPath() {
+    try {
+      const data = jsonfile.readFile(__dirname + "/pelis.json");
+      return data as Peli[];
+    } catch (error) {
+      console.error("Error al leer pelis.json:", error);
       return [];
-    });
+    }
+  }
+
+  async getAll(): Promise<Peli[]> {
+    this.db = await this.loadFromPath();
+    return this.db;
+  }
+
+  async add(pelicula: Peli): Promise<Boolean> {
+    this.db = await this.loadFromPath();
+    try {
+      if (this.db.some((p) => p.id === pelicula.id)) {
+        console.log("La ID ingresada ya existe");
+        return false;
+      }
+      console.log(this.db);
+      this.db.push(pelicula);
+      await jsonfile.writeFile(this.filePath, this.db, { spaces: 2 });
+      console.log("La Pelicula se guardo con exito");
+      return true;
+    } catch (err) {
+      throw new Error("err" + err);
+      return false;
+    }
+  }
+  async getById(id:number):Promise<Peli>{
+    this.db = await this.loadFromPath();
+    try{
+      const resultado = this.db.filter((p)=>{
+        return p.id === id;
+      })
+      if(resultado.length != 1){
+        console.log("La id ingresada no existe")
+      }
+      else{
+        return resultado[0]
+      }
+    }catch(error){
+      throw new Error(error);
+    }
+  }
+
+  async search(options: SearchOptions): Promise<Peli[]> {
+    this.db = await this.loadFromPath();
+    try {
+      const listaFiltrada = this.db.filter(function (p) {
+        let esteVa = false;
+        if (options.title && options.tag) {
+          if (
+            options.title.toLowerCase() === p.title.toLowerCase() &&
+            p.tags.includes(options.tag)
+          ) {
+            esteVa = true;
+          }
+        } else if (options.title) {
+          if (options.title.toLowerCase() === p.title.toLowerCase()) {
+            esteVa = true;
+          }
+        } else if (options.tag) {
+          if (p.tags.includes(options.tag)) {
+            esteVa = true;
+          }
+        }
+        return esteVa;
+      });
+      return listaFiltrada;
+    } catch (err) {
+      throw new Error("Error al cargar el Json");
+    }
   }
 }
 export { PelisCollection, Peli };
+
+// const prueba = new PelisCollection();
+// prueba
+//   .add({ id: 9, title: "La Aventura", tags: ["action", "adventure"] })
+//   .then((resultado) => {
+//     console.log(resultado);
+//   });
+// prueba.getAll().then((resultado)=>{
+//   console.log(resultado);
+// })
+
+// prueba.search({ title: "La aventura",tag:"adventure"}).then((resultado) => {
+//   console.log("Resultado de search: ");
+//   console.log(resultado);
+// });
+// prueba.getById(10).then((resultado)=>{
+//   if(resultado === undefined){
+//     console.log([])
+//   }
+//   else{
+//     console.log("Resultado: ");
+//     console.log(resultado);
+//   }
+
+// })
