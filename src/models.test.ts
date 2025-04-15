659a7067fed0177532a041fdf4a70c419afaa5ea
carlosaac23 <carlosaac23@hotmail.com>
2025-04-15 17:43:40 -0500

    Update: Realizado el models.ts

--- src/models.test.ts
+++ src/models.test.ts
@@ -1,5 +1,5 @@
-import anyTest, { TestFn } from "ava";
-import { PelisCollection, Peli } from "./models";
+import anyTest, { TestFn } from 'ava';
+import { PelisCollection, Peli } from './models';
 
 export const getRandomId = () => {
   const randomNumber = Math.floor(Math.random() * 100000);
@@ -14,10 +14,10 @@ const test = anyTest as TestFn<{
 }>;
 
 const TEST_ID = getRandomId();
-const TEST_TITLE = "title " + SESSION_ID + TEST_ID;
+const TEST_TITLE = 'title ' + SESSION_ID + TEST_ID;
 
 const SECOND_TEST_ID = getRandomId();
-const SECOND_TEST_TITLE = "title " + SESSION_ID + SECOND_TEST_ID;
+const SECOND_TEST_TITLE = 'title ' + SESSION_ID + SECOND_TEST_ID;
 
 // # IMPORTANTE #
 
@@ -27,16 +27,16 @@ const SECOND_TEST_TITLE = "title " + SESSION_ID + SECOND_TEST_ID;
 // comentalos y descomentá uno a uno a medida
 // que vas avanzando en cada test
 
-test.serial("Corre ava", async (t) => {
-  t.is("si", "si");
+test.serial('Corre ava', async t => {
+  t.is('si', 'si');
 });
 
-test.serial("Testeo el método getById", async (t) => {
+test.serial('Testeo el método getById', async t => {
   const collection = new PelisCollection();
   await collection.add({
     id: TEST_ID,
     title: TEST_TITLE,
-    tags: ["tt", "rr"],
+    tags: ['tt', 'rr'],
   });
   const all = await collection.getAll();
   const a = all[0];
@@ -44,29 +44,29 @@ test.serial("Testeo el método getById", async (t) => {
   t.is(a.title, b.title);
 });
 
-test.serial("Testeo el método search", async (t) => {
+test.serial('Testeo el método search', async t => {
   const collection = new PelisCollection();
   await collection.add({
     id: TEST_ID,
     title: TEST_TITLE,
-    tags: ["tt", "rr"],
+    tags: ['tt', 'rr'],
   });
   await collection.add({
     id: SECOND_TEST_ID,
     title: SECOND_TEST_TITLE,
-    tags: ["yy", "uu"],
+    tags: ['yy', 'uu'],
   });
   const all = await collection.getAll();
   const a = all[0];
   // El search debe encontrar ambas pelis creadas a partir de la session
   const b = await collection.search({ title: SESSION_ID.toString() });
-  const ids = b.map((b) => b.id);
+  const ids = b.map(b => b.id);
   t.deepEqual(ids, [TEST_ID, SECOND_TEST_ID]);
 
   // El search debe encontrar solo la peli con el title (session) y el tag (yy)
   const c = await collection.search({
     title: SECOND_TEST_ID.toString(),
-    tag: "yy",
+    tag: 'yy',
   });
   t.deepEqual(c[0].id, SECOND_TEST_ID);
 });
--- src/models.ts
+++ src/models.ts
@@ -1,6 +1,6 @@
-import * as jsonfile from "jsonfile";
 // El siguiente import no se usa pero es necesario
-import "./pelis.json";
+import { readFile, writeFile } from 'fs/promises';
+import './pelis.json';
 // de esta forma Typescript se entera que tiene que incluir
 // el .json y pasarlo a la carpeta /dist
 // si no, solo usandolo desde la libreria jsonfile, no se dá cuenta
@@ -12,12 +12,55 @@ class Peli {
   tags: string[];
 }
 
+type SearchOptions = {
+  title?: string;
+  tag?: string;
+};
+
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile.readFile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
+  async getAll(): Promise<Peli[]> {
+    try {
+      const data = await readFile('./pelis.json', 'utf-8');
+      return JSON.parse(data);
+    } catch (error) {
+      console.error('Oops, algo ha salido mal al intentar leer el archivo:', error);
       return [];
-    });
+    }
+  }
+
+  async add(peli: Peli): Promise<boolean> {
+    const peliculas = await this.getAll();
+    const peliExistente = peliculas.find(pelicula => pelicula.id === peli.id);
+
+    try {
+      if (peliExistente) {
+        return false;
+      } else {
+        peliculas.push(peli);
+        await writeFile('./pelis.json', JSON.stringify(peliculas, null, 2));
+        return true;
+      }
+    } catch (error) {
+      console.error('Oops, algo ha salido mal al intentar escribir en el archivo:', error);
+      return false;
+    }
+  }
+
+  async getById(id: number): Promise<Peli | undefined> {
+    const peliculas = await this.getAll();
+    return peliculas.find(pelicula => pelicula.id === id);
+  }
+
+  async search(options: SearchOptions): Promise<Peli[]> {
+    const peliculas = await this.getAll();
+
+    if (options.title) {
+      return peliculas.filter(pelicula => pelicula.title.includes(options.title));
+    } else if (options.tag) {
+      return peliculas.filter(pelicula => pelicula.tags.includes(options.tag));
+    }
+
+    return [];
   }
 }
 export { PelisCollection, Peli };
