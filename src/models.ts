ec010d0bf8b49d2a3101dd30f26154c1b64b9374
Mathias <mathimonges040@gmail.com>
2025-04-21 14:32:17 -0300

    models resuelto

--- src/models.ts
+++ src/models.ts
@@ -1,5 +1,4 @@
-import * as jsonfile from "jsonfile";
-// El siguiente import no se usa pero es necesario
+import jsonfile from "jsonfile";
 import "./pelis.json";
 // de esta forma Typescript se entera que tiene que incluir
 // el .json y pasarlo a la carpeta /dist
@@ -12,12 +11,58 @@ class Peli {
   tags: string[];
 }
 
+
 class PelisCollection {
   getAll(): Promise<Peli[]> {
-    return jsonfile.readFile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+    return jsonfile.readFile("src/pelis.json");
+  };
+
+  getById(id: number): Promise<Peli> {
+    return this.getAll().then((lista) => {
+      const peli = lista.find((peli) => {
+        return peli.id === id;
+      });
+      return peli;
     });
+  };
+
+  async add(peli: Peli): Promise<boolean> {
+    const peliculaYaExiste = await this.getById(peli.id);
+    const listaPelis = await this.getAll();
+    if (!peliculaYaExiste) {
+      listaPelis.push(peli);
+      await jsonfile.writeFile("src/pelis.json", listaPelis);
+    } else return false;
+
+  }
+
+  async search(options: SearchOptions): Promise<Peli[]> {
+    const lista = await this.getAll();
+    const listaFiltrada = lista.filter((p) => {
+      let peliAceptada = false;
+      if (options.tag) {
+        const tagOptions = options.tag.trim().toLowerCase();
+        peliAceptada = p.tags.some((tag) => tag.trim().toLowerCase().includes(tagOptions));
+      }
+      if (options.title) {
+        const titleOptions = options.title.trim().toLowerCase();
+        if (p.title.trim().toLowerCase().includes(titleOptions)) {
+          peliAceptada = true;
+        }
+      }
+      return peliAceptada;
+    });
+    return listaFiltrada;
   }
 }
+type SearchOptions = { title?: string; tag?: string };
+
+async function main() {
+  const a = new PelisCollection();
+  const peli = await a.search({ tag: "a" })
+  console.log(peli);
+
+}
+
+main();
 export { PelisCollection, Peli };
